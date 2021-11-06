7fc7b52026d9756060eb7ea78181aeef2763624f
yinonov <yinon@hotmail.com>
2021-11-06 12:56:19 +0200

    importMap assistance

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,5 +1,8 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { importMapsPlugin } from '@web/dev-server-import-maps';
+import { importMap } from './docs/import-map.js';
 
+console.log(importMap);
 
 export default {
     open: false,
@@ -8,5 +11,10 @@ export default {
     appIndex: 'docs/index.html',
     // in a monorepo you need to set set the root dir to resolve modules
     rootDir: '.',
-    plugins: [esbuildPlugin({ ts: true, target: 'auto' })],
+    plugins: [
+        esbuildPlugin({ ts: true, target: 'auto' }),
+        importMapsPlugin({
+            inject: {importMap}
+        })
+    ],
 };
