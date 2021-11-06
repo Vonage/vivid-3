78db5ab0723b4ae2cb6041c36ba78e277a701e04
yinonov <yinon@hotmail.com>
2021-11-07 01:10:29 +0200

    external scripts

--- docs/import-map.js
+++
@@ -1,6 +0,0 @@
-export const importMap = {
-    imports: {
-        "@vonage/vivid/badge": "./src/components/badge/badge.ts",
-        "@vonage/vivid/icon": "./src/components/icon/icon.ts"
-    }
-};
