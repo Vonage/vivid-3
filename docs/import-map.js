9a4b8a87aa9c3dff4a8fba3397cd34d455d6b850
yinonov <yinon@hotmail.com>
2021-11-06 12:56:19 +0200

    importMap assistance

---
+++ docs/import-map.js
@@ -0,0 +1,6 @@
+export const importMap = {
+    imports: {
+        "@vonage/vivid/badge": "./src/components/badge/badge.ts",
+        "@vonage/vivid/icon": "./src/components/icon/icon.ts"
+    }
+};
