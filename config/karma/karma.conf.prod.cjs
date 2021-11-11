cf1af22a34e7894489ba5501f015ff2ed799fc6a
yonatankra <yonatan.kra@vonage.com>
2021-11-11 18:07:31 +0200

    chore(tests): add coverage report
    closes #5

--- config/karma/karma.conf.prod.cjs
+++ config/karma/karma.conf.prod.cjs
@@ -1,8 +1,9 @@
-// Karma configuration
-// Generated on Sun Nov 07 2021 16:41:08 GMT+0200 (GMT+02:00)
-const karmaConfig = require('../../karma.conf.cjs');
+const setKarmaBaseConfig = require('../../karma.conf.cjs');
 
 module.exports = function (config) {
-    karmaConfig.singleRun = true;
-    config.set(karmaConfig);
+    setKarmaBaseConfig(config);
+    const newConfig = {
+        singleRun: true
+    }
+    config.set(newConfig);
 }
--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,43 +1,57 @@
 const del = require("rollup-plugin-delete");
 const typescript = require("@rollup/plugin-typescript");
 const {nodeResolve} = require("@rollup/plugin-node-resolve");
+const istanbul = require('./scripts/rollup-plugin-istanbul.cjs');
 
-module.exports = {
-    basePath: process.cwd(),
-    frameworks: ['jasmine'],
-    files: [
-        {
-            pattern: 'src/**/*.spec.ts',
-            watched: false
-        }
-    ],
-    exclude: [],
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'Firefox', 'SafariNative'],
-    concurrency: Infinity,
-    preprocessors: {
-        'src/**/*.spec.ts': ["rollup"]
-    },
-    coverageReporter: {
-        dir: `coverage/`,
-        reporters: [
+module.exports = function (config) {
+
+    config.set({
+        basePath: process.cwd(),
+        frameworks: ['jasmine'],
+        files: [
             {
-                type: 'cobertura', subdir: 'report-cobertura', file: 'coverage.xml'
-            },
-            {
-                type: 'lcovonly', subdir: 'report-lcov', file: 'lcov.info'
+                pattern: 'src/**/*.spec.ts',
+                watched: false
             }
-        ]
-    },
-    rollupPreprocessor: {
-        plugins: [typescript({ tsconfig: './config/typescript/tsconfig.spec.json'}), nodeResolve()],
-        output: {
-            format: 'iife',
-            name: 'testFile',
-            sourcemap: 'inline',
+        ],
+        exclude: [],
+        reporters: config.coverage ? ['coverage', 'progress'] : ['progress'],
+        port: 9876,
+        colors: true,
+        autoWatch: true,
+        browsers: ['Chrome', 'ChromeHeadless', 'Firefox', 'SafariNative'],
+        concurrency: Infinity,
+        preprocessors: {
+            'src/**/*.spec.ts': ["rollup"],
         },
-    },
+        coverageReporter: {
+            dir: `coverage/`,
+            includeAllSources: true,
+            reporters: [
+                {
+                    type: 'cobertura', subdir: 'report-cobertura', file: 'coverage.xml'
+                },
+                {
+                    type: 'lcovonly', subdir: 'report-lcov', file: 'lcov.info'
+                },
+                {
+                    type: 'html'
+                }
+            ]
+        },
+        rollupPreprocessor: {
+            plugins: [
+                del({targets: 'dist/*'}),
+                typescript({tsconfig: './config/typescript/tsconfig.spec.json'}),
+                istanbul({
+                    exclude: ['src/**/*.spec.ts', 'node_modules/**/*']
+                }),
+                nodeResolve()],
+            output: {
+                format: 'iife',
+                name: 'testFile',
+                sourcemap: 'inline',
+            },
+        },
+    });
 };
