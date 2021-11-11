d6a6062b51c55c4f63008cf4c15f39ac936b4ca0
yinonov <yinon@hotmail.com>
2021-11-11 17:55:05 +0200

    Use rollup to import styles into `ts` files Fixes #15

--- types/style.d.ts
+++ types/style.d.ts
@@ -1,13 +1,3 @@
-// declare module '*.css' {
-//   const classes: { [key: string]: string };
-//   export default classes;
-// }
-
-// declare module '*.scss' {
-//   const classes: { [key: string]: string };
-//   export default classes;
-// }
-
 declare module '*.scss' {
   const classes: import('lit').CSSResult;
   export default classes;
