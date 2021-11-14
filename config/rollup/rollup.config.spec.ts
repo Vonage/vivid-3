952c609bd067ca8ddff055c059bc1a4a94dfa8f1
Yonatan Kra <yonatan.kra@vonage.com>
2021-11-14 17:21:20 +0200

    chore(tests): add coverage report (#26)
    * chore(tests): add coverage report
    
    closes #5
    
    * Change karma config files to `ts`
    
    * Cleanup
    
    * Make tests rollup better
    
    * Update packages and fix tsconfig warning
    
    * Revert change to tsconfig
    
    * Fix badge race condition
    
    The icon not always updated when the parent did in Safari
    
    * Remove obsolete ts ignore

--- config/rollup/rollup.config.spec.ts
+++ config/rollup/rollup.config.spec.ts
@@ -1,10 +1,26 @@
-import rollupBaseConfig from "./rollup.config.base.ts";
+import rollupBaseConfig from "./rollup.config.base";
+import typescript from "@rollup/plugin-typescript";
+import {RollupOptions} from "rollup";
+const istanbul = require('../../scripts/rollup-plugin-istanbul.cjs');
 
-export default {
+rollupBaseConfig.plugins.push(istanbul({
+    exclude: ['src/**/*.spec.ts', 'node_modules/**/*']
+}));
+
+rollupBaseConfig.plugins.splice(1,1, typescript({tsconfig: './config/typescript/tsconfig.spec.json'}),);
+
+const rollupConfig: RollupOptions = {
+    cache: false,
     output: {
-        format: 'iife', // Helps prevent naming collisions.
-        name: '<your_project>', // Required for 'iife' format.
-        sourcemap: 'inline', // Sensible for testing.
+        format: 'iife',
+        name: 'vivid',
+        sourcemap: 'inline',
+        dir: './dist/out-tsc'
     },
-    ...rollupBaseConfig
+    ...rollupBaseConfig,
+    watch: {
+        clearScreen: true
+    }
 };
+
+export default rollupConfig;
