ef1498ee94f38c99494c1bd0cd07ade248b2c3e2
yinonov <yinon@hotmail.com>
2021-11-17 23:29:13 +0200

    code blocks

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const codeblocks = require('@code-blocks/eleventy-plugin')
 
+// some renderers
+const math = require('@code-blocks/math')
+const music = require('@code-blocks/music')
+const prism = require('@code-blocks/prism')
 
 module.exports = function (eleventyConfig) {
 
@@ -12,6 +17,11 @@ module.exports = function (eleventyConfig) {
     "dist/components": "assets/modules/components"
   });
 
+  eleventyConfig.addPlugin(codeblocks([
+    math,
+    music,
+    prism
+  ]))
   // const style = {"styles": 'dist/core/theme/*.css'};
   // eleventyConfig.addPassthroughCopy(style);
   // eleventyConfig.addWatchTarget(style);
