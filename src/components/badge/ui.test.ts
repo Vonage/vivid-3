cefd4aa26622415131187fe122671d7a3f48e7a3
yinonov <yinon@hotmail.com>
2021-11-22 11:53:52 +0200

    eslint stable

--- src/components/badge/ui.test.ts
+++ src/components/badge/ui.test.ts
@@ -1,53 +1,50 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
 async function loadComponent({
-  page,
-  componentName,
-  styleUrl = "http://127.0.0.1:8080/dist/core/theme/light.css",
+	page,
+	componentName,
+	styleUrl = 'http://127.0.0.1:8080/dist/core/theme/light.css',
 }) {
-  await page.goto(`http://127.0.0.1:8080/scripts/visual-tests/index.html`);
+	await page.goto('http://127.0.0.1:8080/scripts/visual-tests/index.html');
 
-  await page.addScriptTag({
-    url: `http://127.0.0.1:8080/dist/components/${componentName}/${componentName}.js`,
-    type: "module",
-  });
+	await page.addScriptTag({
+		url: `http://127.0.0.1:8080/dist/components/${componentName}/${componentName}.js`,
+		type: 'module',
+	});
 
-  await page.addStyleTag({
-    url: styleUrl,
-  });
+	await page.addStyleTag({
+		url: styleUrl,
+	});
 }
 
 async function loadTemplate({ page, template }) {
-  await page.addScriptTag({
-    content: `
+	await page.addScriptTag({
+		content: `
             document.body.innerHTML = '<div id="wrapper">${template}</div>';
         `,
-  });
+	});
 }
 
-const componentName = "badge";
-test(`should have all connotations`, async function ({ page }) {
-  const connotations = [
-    "primary",
-    "cta",
-    "success",
-    "alert",
-    "warning",
-    "info",
-    "announcement",
-  ];
+const componentName = 'badge';
+test('should have all connotations', async ({ page }) => {
+	const connotations = [
+		'primary',
+		'cta',
+		'success',
+		'alert',
+		'warning',
+		'info',
+		'announcement',
+	];
 
-  const template = connotations.reduce((htmlString, connotationValue) => {
-    htmlString += `<div style="margin: 5px"><vwc-badge text="Badge" connotation="${connotationValue}"></vwc-badge></div>`;
-    return htmlString;
-  }, "");
+	const template = connotations.reduce((htmlString, connotationValue) => `${htmlString}<div style="margin: 5px"><vwc-badge text="Badge" connotation="${connotationValue}"></vwc-badge></div>`, '');
 
-  await loadComponent({ page, componentName });
-  await loadTemplate({ page, template });
+	await loadComponent({ page, componentName });
+	await loadTemplate({ page, template });
 
-  const testWrapper = await page.$("#wrapper");
+	const testWrapper = await page.$('#wrapper');
 
-  expect(await testWrapper.screenshot()).toMatchSnapshot(
-    "./snapshots/badge.png"
-  );
+	expect(await testWrapper.screenshot()).toMatchSnapshot(
+		'./snapshots/badge.png',
+	);
 });
