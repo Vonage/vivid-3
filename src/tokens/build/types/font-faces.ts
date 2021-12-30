a8936966339ed111bc459d71e12d4cb28369462b
yinonov <yinon@hotmail.com>
2021-12-30 14:29:31 +0200

    build(sass): connotation mixin support

---
+++ src/tokens/build/types/font-faces.ts
@@ -0,0 +1,28 @@
+/*
+  Do not edit directly
+  Generated on Thu, 23 Dec 2021 07:54:44 GMT
+*/
+
+export enum VVDFontFace {
+	Body1 = 'body-1',
+	Body1Bold = 'body-1-bold',
+	Body1Code = 'body-1-code',
+	Body1Link = 'body-1-link',
+	Body2 = 'body-2',
+	Body2Bold = 'body-2-bold',
+	Body2Code = 'body-2-code',
+	Body2Link = 'body-2-link',
+	Button = 'button',
+	ButtonDense = 'button-dense',
+	ButtonEnlarge = 'button-enlarge',
+	Caption = 'caption',
+	CaptionBold = 'caption-bold',
+	CaptionCode = 'caption-code',
+	CaptionLink = 'caption-link',
+	Headline1 = 'headline-1',
+	Headline2 = 'headline-2',
+	Subtitle1 = 'subtitle-1',
+	Subtitle2 = 'subtitle-2',
+	Title1 = 'title-1',
+	Title2 = 'title-2'
+}
