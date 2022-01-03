706d05774c5c099805a0adcbdedc7ea864bf4582
yonatankra <yonatan.kra@vonage.com>
2022-01-03 14:57:31 +0200

    Change output path to dist

--- apps/11ty/.eleventy.js
+++ apps/11ty/.eleventy.js
@@ -2,9 +2,11 @@ const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const codeblockdemo = require('eleventy-plugin-code-block-demo')
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
+const OUTPUT_DIR = 'dist/vvd-docs';
 
 module.exports = function (eleventyConfig) {
 
+  console.log(eleventyConfig);
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(codeblockdemo);
   eleventyConfig.addPlugin(syntaxHighlight);
@@ -15,12 +17,18 @@ module.exports = function (eleventyConfig) {
     "11ty/assets": "assets"
   });
 
+  eleventyConfig.setBrowserSyncConfig({
+    server: {
+      baseDir: OUTPUT_DIR
+    }
+  })
 
   return {
     dir: {
       input: "./11ty",
       includes: "_includes",
       layouts: "_layouts",
+      output: OUTPUT_DIR,
     }
   }
 };
