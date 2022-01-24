2ba0df8676f8abf34e06680fb8a9cbbec4200be7
yinonov <yinon@hotmail.com>
2022-01-24 09:40:14 +0200

    jsdoc fix

--- libs/components/src/lib/common/design-system/index.ts
+++ libs/components/src/lib/common/design-system/index.ts
@@ -2,7 +2,8 @@ import { DesignSystem } from '@microsoft/fast-foundation';
 
 // common components dependency
 /**
- * @param element
+ * @param {HTMLElement} element - The element to get or create a design system for.
+ * @returns {DesignSystem} - Represents a configurable design system. An API gateway to design system features.
  */
 export function provideVividDesignSystem(element?: HTMLElement): DesignSystem {
 	return DesignSystem.getOrCreate(element).withPrefix('vwc');
