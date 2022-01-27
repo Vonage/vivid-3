659943f5cc3b218bbda7bf03b6c5eec82d43bf3f
yonatankra <yonatan.kra@vonage.com>
2022-01-27 13:51:26 +0200

    Fix e2e local run

--- scripts/visual-tests/run.tests.in.container.sh
+++ scripts/visual-tests/run.tests.in.container.sh
@@ -1,6 +1,8 @@
-if [ $1 = "build" ]
-then
-  ./scripts/visual-tests/build.image.sh
-else
+case $1 in
+"build" )
+  ./scripts/visual-tests/build.image.sh ;;
+"local" )
+  ./scripts/visual-tests/run.tests.sh ;;
+*)
   docker run -it --name vivid-visual-tests --env update=$1 --rm -v $PWD:/usr/src/app vivid-visual-tests-img
-fi
+esac
