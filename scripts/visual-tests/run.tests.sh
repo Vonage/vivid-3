207bda88ba95cb9e1699f432fbe62e01ab41dbf4
yonatankra <yonatan.kra@vonage.com>
2022-02-27 13:52:22 +0200

    Add watch mode for components e2e
    `npx nx run components:e2e:watch`

--- scripts/visual-tests/run.tests.sh
+++ scripts/visual-tests/run.tests.sh
@@ -4,12 +4,12 @@ arg=$([ -n "$task" ] && echo "$task" || echo "5")
 echo $arg
 case $arg in
 "update")
-  echo "Testing snapshots"
+  echo "Updating snapshots"
   npx http-server -s &
   npx playwright test --update-snapshots -c ./libs/components/playwright.config.ts
   ;;
 *)
-  echo "Updating snapshots"
+  echo "Testing snapshots"
   npx http-server -s &
   npx playwright test -c ./libs/components/playwright.config.ts
   ;;
