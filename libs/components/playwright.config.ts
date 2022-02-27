b544a6c2467c2e7f4e7ffcbfd57ad6850da8af47
yonatankra <yonatan.kra@vonage.com>
2022-02-27 07:44:43 +0200

    Lint the playwright config file

--- libs/components/playwright.config.ts
+++ libs/components/playwright.config.ts
@@ -5,29 +5,29 @@ interface PlaywrightTestConfigWithRegression extends PlaywrightTestConfig {
 }
 
 const config: PlaywrightTestConfigWithRegression = {
-    testMatch: 'src/**/*.test.ts',
-    projects: [
-        {
-            name: 'Chrome Stable',
-            use: {
-                browserName: 'chromium',
-                channel: 'chrome',
-            },
-        },
-        {
-            name: 'Desktop Safari',
-            use: {
-                browserName: 'webkit',
-                viewport: { width: 1200, height: 750 },
-            }
-        },
-        {
-            name: 'Desktop Firefox',
-            use: {
-                browserName: 'firefox',
-                viewport: { width: 800, height: 600 },
-            }
-        },
-    ]
+	testMatch: 'src/**/*.test.ts',
+	projects: [
+		{
+			name: 'Chrome Stable',
+			use: {
+				browserName: 'chromium',
+				channel: 'chrome',
+			},
+		},
+		{
+			name: 'Desktop Safari',
+			use: {
+				browserName: 'webkit',
+				viewport: { width: 1200, height: 750 },
+			}
+		},
+		{
+			name: 'Desktop Firefox',
+			use: {
+				browserName: 'firefox',
+				viewport: { width: 800, height: 600 },
+			}
+		},
+	]
 };
 export default config;
