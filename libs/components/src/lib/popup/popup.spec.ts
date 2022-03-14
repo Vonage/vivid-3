a1ee9513e4ffbd8e025ddba79eeace9600bea85e
Rina <rina.oksman@vonage.com>
2022-03-14 13:09:04 +0200

    add tests

--- libs/components/src/lib/popup/popup.spec.ts
+++ libs/components/src/lib/popup/popup.spec.ts
@@ -1,4 +1,4 @@
-import { elementUpdated, fixture } from '@vivid-nx/shared';
+import { elementUpdated, fixture, getControlElement } from '@vivid-nx/shared';
 import { Corner, Position } from '../enums';
 import type { Button } from '../button/button';
 import { Popup } from './popup';
@@ -66,10 +66,40 @@ describe('vwc-popup', () => {
 			expect(element.open).toEqual(false);
 		});
 	});
-	/**
-	 *
-	 */
-	async function setPopupAndAnchor(){
+
+	describe('render classes', () => {
+		it('should render arrow class', async () => {
+			element.arrow = true;
+			await elementUpdated(element);
+			expect(element.shadowRoot?.querySelector(".arrow")).not.toBeNull();
+		});
+
+		it('should render dismiss class', async () => {
+			element.dismissible = true;
+			await elementUpdated(element);
+			expect(element.shadowRoot?.querySelector(".dismissible")).not.toBeNull();
+		});
+	});
+
+	describe('alternate', () => {
+		it('should set to alternate', async () => {
+			expect(getControlElement(element).getAttribute("part")).toEqual('');
+			element.alternate = true;
+			await elementUpdated(element);
+			expect(getControlElement(element).getAttribute("part")).toEqual('vvd-theme-alternate');
+		});
+	});
+
+	describe('accessibility', () => {
+		it('should set aroa-hidden', async () => {
+			expect(getControlElement(element).getAttribute("aria-hidden")).toEqual('true');
+			element.open = true;
+			await elementUpdated(element);
+			expect(getControlElement(element).getAttribute("aria-hidden")).toEqual('false');
+		});
+	});
+
+	async function setPopupAndAnchor() {
 		const anchorEl = await fixture('<vwc-button id="anchor"></vwc-button>') as Button;
 		await elementUpdated(anchorEl);
 		return anchorEl;
