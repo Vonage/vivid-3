7278775b054e01bf1f623d6177299df721a5f2c7
yonatankra <yonatan.kra@vonage.com>
2022-03-15 05:06:03 +0200

    Extend text-anchor and not fast breadcrumb

--- libs/components/src/lib/breadcrumb-item/breadcrumb-item.ts
+++ libs/components/src/lib/breadcrumb-item/breadcrumb-item.ts
@@ -1,22 +1,13 @@
-import { BreadcrumbItem as FastBreadcrumbItem } from '@microsoft/fast-foundation';
 import {attr} from '@microsoft/fast-element';
+import {TextAnchor} from '../text-anchor/text-anchor';
 
 /**
  * Base class for breadcrumb-item
  *
  * @public
  */
-export class BreadcrumbItem extends FastBreadcrumbItem {
-	/**
-	 * Indicates the breadcrumb-item's text.
-	 *
-	 * @public
-	 * @remarks
-	 * HTML Attribute: text
-	 */
-	@attr text = '';
-
-	@attr({ mode: 'boolean' }) override separator = false;
+export class BreadcrumbItem extends TextAnchor {
+	@attr({ mode: 'boolean' }) separator = false;
 
 	constructor() {
 		super();
