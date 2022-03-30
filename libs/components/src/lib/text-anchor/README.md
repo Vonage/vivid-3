943dde7f3c6e5f9b18ea5f46dca4d9ed2df01789
yinon <yinon@hotmail.com>
2022-03-30 10:22:01 +0300

    refactor(patterns): affix icon trailing condition (#250)
    * chore(text-anchor): feature underlying vivid anchor
    
    * update
    
    * remove doc
    
    * refactor(patterns): affix icon trailing condition
    
    * icon-trailing owned by consuming component
    
    * stylelint fix
    
    * Change files
    
    * missing context
    
    * remove when condition
    
    * removed commented code
    
    * Test disabled state
    
    Co-authored-by: yonatankra <yonatan.kra@vonage.com>

--- libs/components/src/lib/text-anchor/README.md
+++ libs/components/src/lib/text-anchor/README.md
@@ -3,11 +3,3 @@
 underlying component to be used as inlined text anchor element
 
 TODO document for maintainers usage
-
-```js
-<script type="module">import '@vonage/vivid/text-anchor';</script>
-```
-
-```html preview
-<vwc-text-anchor text="show"></vwc-text-anchor>
-```
--- libs/components/src/shared/patterns/affix.ts
+++ libs/components/src/shared/patterns/affix.ts
@@ -1,7 +1,6 @@
 import { attr, html } from '@microsoft/fast-element';
 import type { ViewTemplate } from '@microsoft/fast-element';
 import type { ElementDefinitionContext } from '@microsoft/fast-foundation';
-import { classNames } from '@microsoft/fast-web-utilities';
 import { Icon } from '../../lib/icon/icon';
 
 /**
@@ -10,7 +9,7 @@ import { Icon } from '../../lib/icon/icon';
 	*
 	* @public
 	*/
-export class Prefix {
+export class AffixIcon {
 	/**
 		* A decorative icon the custom element should have.
 		*
@@ -26,7 +25,7 @@ export class Prefix {
 	*
 	* @public
 	*/
-export class PrefixOrSuffix extends Prefix {
+export class AffixIconWithTrailing extends AffixIcon {
 	/**
 		* Indicates the icon affix alignment.
 		*
@@ -42,38 +41,15 @@ export class PrefixOrSuffix extends Prefix {
 
 /**
 	* The template for the prefixed element.
-	* For use with {@link Prefix}
+	* For use with {@link AffixIcon}
 	*
 	* @param context
 	* @public
 	*/
-export const prefixTemplate: (context: ElementDefinitionContext) => ViewTemplate<Prefix> = (context: ElementDefinitionContext) => {
+export const affixIconTemplate: (context: ElementDefinitionContext) => ViewTemplate<AffixIcon> = (context: ElementDefinitionContext) => {
 	const iconTag = context.tagFor(Icon);
 
 	const iconTemplate = html`<span class="icon"><${iconTag} :type="${(x) => x.icon}"></${iconTag}></span>`;
 
 	return html`${(x) => (x.icon ? iconTemplate : '')}`;
 };
-
-/**
-	* The template for the affixed (prefixed or suffixed) element.
-	* For use with {@link PrefixOrSuffix}
-	*
-	* @param context
-	* @public
-	*/
-export const prefixOrSuffixTemplate: (context: ElementDefinitionContext) => ViewTemplate<PrefixOrSuffix> =
-	(context: ElementDefinitionContext) => {
-		const iconTag = context.tagFor(Icon);
-
-		const classes = ({
-			iconTrailing,
-		}: PrefixOrSuffix) => classNames(
-			'icon',
-			['icon-trailing', iconTrailing],
-		);
-
-		const iconTemplate = html`<span class="${classes}"><${iconTag} :type="${(x) => x.icon}"></${iconTag}></span>`;
-
-		return html`${(x) => (x.icon ? iconTemplate : '')}`;
-	};
