4034881a6d21de8df159c455932d38b2a954085f
yonatankra <yonatan.kra@vonage.com>
2022-04-07 15:45:43 +0300

    Fix some styleint

--- libs/components/src/lib/banner/ui.test.ts
+++ libs/components/src/lib/banner/ui.test.ts
@@ -1,36 +1,36 @@
 import * as path from 'path';
 import { expect, Page, test } from '@playwright/test';
 import {
-  extractHTMLBlocksFromReadme,
-  loadComponent,
-  loadTemplate,
+	extractHTMLBlocksFromReadme,
+	loadComponent,
+	loadTemplate,
 } from '../../visual-tests/visual-tests-utils';
 
 const components = ['banner'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
-  const template = extractHTMLBlocksFromReadme(
-    path.join(__dirname, 'README.md')
-  ).reduce(
-    (htmlString: string, block: string) =>
-      `${htmlString} <div style="margin: 5px;">${block}</div>`,
-    ''
-  );
+	const template = extractHTMLBlocksFromReadme(
+		path.join(__dirname, 'README.md')
+	).reduce(
+		(htmlString: string, block: string) =>
+			`${htmlString} <div style="margin: 5px;">${block}</div>`,
+		''
+	);
 
-  await loadComponent({
-    page,
-    components,
-  });
-  await loadTemplate({
-    page,
-    template,
-  });
+	await loadComponent({
+		page,
+		components,
+	});
+	await loadTemplate({
+		page,
+		template,
+	});
 
-  const testWrapper = await page.$('#wrapper');
+	const testWrapper = await page.$('#wrapper');
 
-  await page.waitForLoadState('networkidle');
+	await page.waitForLoadState('networkidle');
 
-  expect(await testWrapper?.screenshot()).toMatchSnapshot(
-    './snapshots/banner.png'
-  );
+	expect(await testWrapper?.screenshot()).toMatchSnapshot(
+		'./snapshots/banner.png'
+	);
 });
