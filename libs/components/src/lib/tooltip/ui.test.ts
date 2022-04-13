331f4e0b884e7a7919e3268c3ac5d421faf840e4
Rina <rina.oksman@vonage.com>
2022-04-13 16:46:20 +0300

    update tests

--- libs/components/src/lib/tooltip/ui.test.ts
+++ libs/components/src/lib/tooltip/ui.test.ts
@@ -1,7 +1,5 @@
-import * as path from 'path';
 import { expect, Page, test } from '@playwright/test';
 import {
-	extractHTMLBlocksFromReadme,
 	loadComponents,
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils';
@@ -9,13 +7,31 @@ import {
 const components = ['tooltip','button'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
-	const template = extractHTMLBlocksFromReadme(
-		path.join(__dirname, 'README.md')
-	).reduce(
-		(htmlString: string, block: string) =>
-			`${htmlString} <div style="margin: 5px;">${block}</div>`,
-		''
-	);
+	const template = `
+<style>
+  .wrapper{
+    width: 100%;
+    height: 220px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  vwc-tooltip#tooltip{
+    --tooltip-inline-size:100px;
+  }
+</style>
+<div class="wrapper">
+  <vwc-button id="anchor" aria-describedby="tooltip" appearance='outlined' label='This is an anchor'></vwc-button>
+  <vwc-tooltip id="tooltip" anchor="anchor" open text="right" corner="right">
+  </vwc-tooltip>
+  <vwc-tooltip id="tooltip" anchor="anchor" open text="left" corner="left">
+  </vwc-tooltip>
+  <vwc-tooltip id="tooltip" anchor="anchor" open text="top" corner="top">
+  </vwc-tooltip>
+  <vwc-tooltip id="tooltip" anchor="anchor" open text="bottom" corner="bottom">
+  </vwc-tooltip>
+</div>
+	`;
 
 	await loadComponents({
 		page,
