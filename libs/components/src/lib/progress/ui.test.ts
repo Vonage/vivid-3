79d5c4f7872bbe5b71a1b6498b60d7d198786646
yonatankra <yonatan.kra@vonage.com>
2022-04-13 10:08:29 +0300

    Ui tests

--- libs/components/src/lib/progress/ui.test.ts
+++ libs/components/src/lib/progress/ui.test.ts
@@ -1,36 +1,54 @@
 import * as path from 'path';
-import { expect, Page, test } from '@playwright/test';
+import {expect, Page, test} from '@playwright/test';
 import {
-  extractHTMLBlocksFromReadme,
-  loadComponents,
-  loadTemplate,
+	extractHTMLBlocksFromReadme,
+	loadComponents,
+	loadTemplate,
 } from '../../visual-tests/visual-tests-utils';
 
 const components = ['progress'];
 
-test('should show the component', async ({ page }: { page: Page }) => {
-  const template = extractHTMLBlocksFromReadme(
-    path.join(__dirname, 'README.md')
-  ).reduce(
-    (htmlString: string, block: string) =>
-      `${htmlString} <div style="margin: 5px;">${block}</div>`,
-    ''
-  );
+test('should show the component', async ({page}: { page: Page }) => {
+	const template = `
+    <vwc-progress min="0" max="100" value="25" connotation="primary"></vwc-progress>
+    <br/>
+    <br/>
+    <vwc-progress min="0" max="100" value="25" connotation="cta"></vwc-progress>
+    <br/>
+    <br/>
+    <vwc-progress min="0" max="100" value="25" connotation="success"></vwc-progress>
+    <br/>
+    <br/>
+    <vwc-progress min="0" max="100" value="25" connotation="alert"></vwc-progress>
+    <br/>
+    <br/>
+    <vwc-progress min="0" max="100" value="25" connotation="pacific"></vwc-progress>
+    <br/>
+    <br/>
+    <vwc-progress min="0" max="50" value="25" paused></vwc-progress>
+    <br/>
+    <br/>
+    <vwc-progress min="0" max="50" value="indeterminate" paused></vwc-progress>
+    <br/>
+    <br/>
+    <vwc-progress min="0" max="50" value="25" reverse></vwc-progress>
+  `;
 
-  await loadComponents({
-    page,
-    components,
-  });
-  await loadTemplate({
-    page,
-    template,
-  });
+	await loadComponents({
+		page,
+		components,
+	});
+	await loadTemplate({
+		page,
+		template,
+	});
 
-  const testWrapper = await page.$('#wrapper');
+	const testWrapper = await page.$('#wrapper');
 
-  await page.waitForLoadState('networkidle');
+	await page.waitForLoadState('networkidle');
 
-  expect(await testWrapper?.screenshot()).toMatchSnapshot(
-    './snapshots/progress.png'
-  );
+	expect(await testWrapper?.screenshot())
+		.toMatchSnapshot(
+			'./snapshots/progress.png'
+		);
 });
