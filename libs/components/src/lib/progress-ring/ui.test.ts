f9ce84aa4a7e13452af51c8c2e0bd8c61541133a
yonatankra <yonatan.kra@vonage.com>
2022-04-15 10:55:05 +0300

    Update ui tests

--- libs/components/src/lib/progress-ring/ui.test.ts
+++ libs/components/src/lib/progress-ring/ui.test.ts
@@ -1,36 +1,35 @@
 import * as path from 'path';
 import { expect, Page, test } from '@playwright/test';
 import {
-  extractHTMLBlocksFromReadme,
-  loadComponents,
-  loadTemplate,
+	extractHTMLBlocksFromReadme,
+	loadComponents,
+	loadTemplate,
 } from '../../visual-tests/visual-tests-utils';
 
 const components = ['progress-ring'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
-  const template = extractHTMLBlocksFromReadme(
-    path.join(__dirname, 'README.md')
-  ).reduce(
-    (htmlString: string, block: string) =>
-      `${htmlString} <div style="margin: 5px;">${block}</div>`,
-    ''
-  );
+	const template = `
+	  <vwc-progress-ring min="0" max="100" value="50"></vwc-progress-ring>
+	  <vwc-progress-ring min="0" max="100" value="33" size="base-large"></vwc-progress-ring>
+	  <vwc-progress-ring min="0" max="100" value="66" size="base-small"></vwc-progress-ring>
+	  <vwc-progress-ring min="0" max="100" value="77" size="base-small" paused></vwc-progress-ring>
+	`;
 
-  await loadComponents({
-    page,
-    components,
-  });
-  await loadTemplate({
-    page,
-    template,
-  });
+	await loadComponents({
+		page,
+		components,
+	});
+	await loadTemplate({
+		page,
+		template,
+	});
 
-  const testWrapper = await page.$('#wrapper');
+	const testWrapper = await page.$('#wrapper');
 
-  await page.waitForLoadState('networkidle');
+	await page.waitForLoadState('networkidle');
 
-  expect(await testWrapper?.screenshot()).toMatchSnapshot(
-    './snapshots/progress-ring.png'
-  );
+	expect(await testWrapper?.screenshot()).toMatchSnapshot(
+		'./snapshots/progress-ring.png'
+	);
 });
