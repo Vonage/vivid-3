9c5bca49f05b303f1009955f0e293b6b026e6c64
Rina <rina.oksman@vonage.com>
2022-04-18 18:00:06 +0300

    add tests

--- libs/components/src/lib/accordion/accordion.spec.ts
+++ libs/components/src/lib/accordion/accordion.spec.ts
@@ -1,4 +1,5 @@
-import { fixture } from '@vivid-nx/shared';
+import { elementUpdated, fixture } from '@vivid-nx/shared';
+import type { AccordionItem } from '../accordion-item/accordion-item';
 import { Accordion } from './accordion';
 import '.';
 
@@ -6,16 +7,43 @@ const COMPONENT_TAG = 'vwc-accordion';
 
 describe('vwc-accordion', () => {
 	let element: Accordion;
+	let accordionItem1: AccordionItem;
+	let accordionItem2: AccordionItem;
 
 	beforeEach(async () => {
 		element = (await fixture(
-			`<${COMPONENT_TAG}></${COMPONENT_TAG}>`
+			`<${COMPONENT_TAG}>
+				<vwc-accordion-item id="item1"></vwc-accordion-item>
+				<vwc-accordion-item id="item2"></vwc-accordion-item>
+			</${COMPONENT_TAG}>`
 		)) as Accordion;
+		await elementUpdated(element);
+
+		accordionItem1 = element.querySelector('#item1') as AccordionItem;
+		accordionItem2 = element.querySelector('#item2') as AccordionItem;
 	});
 
 	describe('basic', () => {
 		it('should be initialized as a vwc-accordion', async () => {
 			expect(element).toBeInstanceOf(Accordion);
+			expect(element.multi).toBeFalsy();
+		});
+	});
+
+	describe('accordion visibility', () => {
+		it('should only allow one expansion panel open at a time', async () => {
+			console.log(accordionItem1.open);
+			console.log(accordionItem2.open);
+
+			expect(accordionItem1.open).toBeFalsy();
+			expect(accordionItem2.open).toBeFalsy();
+
+			accordionItem1.open = true;
+			accordionItem2.open = true;
+			await elementUpdated(element);
+
+			expect(accordionItem1.open).toBeFalsy();
+			expect(accordionItem2.open).toBeTruthy();
 		});
 	});
 });
