6695b63b2041db0daf111a541d8757dc48df9b55
yinonov <yinon@hotmail.com>
2022-04-26 21:32:42 +0300

    back

--- site/server/main.ts
+++ site/server/main.ts
@@ -1,29 +1,8 @@
-import { Application, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-const app = new Application();
-
-// First we try to serve static files from the _site folder. If that fails, we
-// fall through to the router below.
-app.use(async (ctx, next) => {
-  try {
-    await ctx.send({
-      root: `${Deno.cwd()}/_site`,
-      index: "index.html",
-    });
-  } catch {
-    next();
-  }
+console.log("Listening on http://localhost:8000");
+serve((_req) => {
+  return new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
 });
-
-const router = new Router();
-
-// The /api/time endpoint returns the current time in ISO format.
-router.get("/api/time", (ctx) => {
-  ctx.response.body = { time: new Date().toISOString() };
-});
-
-// After creating the router, we can add it to the app.
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-await app.listen({ port: 8000 });
