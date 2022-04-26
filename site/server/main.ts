d1e4bb500146768d77c7e2a1af1abed93537a36e
yinonov <yinon@hotmail.com>
2022-04-26 21:43:18 +0300

    fixed

--- site/server/main.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-
-console.log("Listening on http://localhost:8000");
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-});
