6830e885ababa700d55a04df67c4864a1e07fa5d
Yonatan Kra <yonatan.kra@vonage.com>
2022-04-28 09:20:29 +0300

    feat(progress-ring): add progress-ring component (#320)

---
+++ libs/components/src/lib/progress-ring/progress-ring.ts
@@ -0,0 +1,18 @@
+import {BaseProgress} from '@microsoft/fast-foundation';
+import {attr} from '@microsoft/fast-element';
+import type {Connotation} from '../enums';
+
+export type ProgressRingConnotation =
+	Connotation.Primary |
+	Connotation.Success |
+	Connotation.Alert |
+	Connotation.CTA;
+
+/**
+ * Base class for progress-ring
+ *
+ * @public
+ */
+export class ProgressRing extends BaseProgress {
+	@attr() connotation?: ProgressRingConnotation;
+}
