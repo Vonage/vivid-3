bc7960e8422d7c4f7b568d5544be28613e4a0925
yinonov <yinon@hotmail.com>
2022-05-01 13:19:07 +0300

    fix calendar ui

--- libs/components/src/lib/calendar/ui.test.ts
+++ libs/components/src/lib/calendar/ui.test.ts
@@ -1,8 +1,6 @@
-import * as path from 'path';
-import { expect, test } from '@playwright/test'; 
+import { expect, test } from '@playwright/test';
 import type { Page } from '@playwright/test';
 import {
-	extractHTMLBlocksFromReadme,
 	loadComponents,
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.ts';
@@ -12,7 +10,7 @@ const components = ['calendar'];
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template =`
     <vwc-calendar datetime="2022-01-01"></vwc-calendar>
-    <vwc-calendar hour12 locales="he-IL" start-day="sunday" style="direction: rtl"></vwc-calendar>
+    <vwc-calendar datetime="2022-01-01" hour12 locales="he-IL" start-day="sunday" style="direction: rtl"></vwc-calendar>
 	`;
 
 	await loadComponents({
