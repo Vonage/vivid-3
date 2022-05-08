b7c7c5b9530dedad7348bf3b056fd76a900e3e54
yonatankra <yonatan.kra@vonage.com>
2022-05-08 21:44:02 +0300

    add file to root

---
+++ beachball.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+		access: 'public',
+		branch: 'main'
+		prereleasePrefix: 'next',
+		publish: false,
+		tag: 'next',
+		disallowedChangeTypes: ["major", "minor", "patch"],
+		gitTags: false
+}
