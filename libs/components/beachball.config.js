3b7d3e692983325a7bd0893bc3ce30004158fa99
yonatankra <yonatan.kra@vonage.com>
2022-05-08 21:47:40 +0300

    Change is now prerelease

--- libs/components/beachball.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-		access: 'public',
-		branch: 'main',
-		prereleasePrefix: 'next',
-		publish: false,
-		tag: 'next',
-		disallowedChangeTypes: ["major", "minor", "patch"],
-		gitTags: false
-}
