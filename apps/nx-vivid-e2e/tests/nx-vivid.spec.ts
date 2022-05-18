a0dc62a0ab362b653d788aa771cecc15202c6645
yonatankra <yonatan.kra@vonage.com>
2022-05-18 13:33:46 +0300

    Setting up the e2e tests

--- apps/nx-vivid-e2e/tests/nx-vivid.spec.ts
+++ apps/nx-vivid-e2e/tests/nx-vivid.spec.ts
@@ -7,50 +7,41 @@ import {
 } from '@nrwl/nx-plugin/testing';
 
 describe('nx-vivid e2e', () => {
-  // Setting up individual workspaces per
-  // test can cause e2e runs to take a long time.
-  // For this reason, we recommend each suite only
-  // consumes 1 workspace. The tests should each operate
-  // on a unique project in the workspace, such that they
-  // are not dependant on one another.
   beforeAll(() => {
     ensureNxProject('@vonage/nx-vivid', 'dist/libs/nx-vivid');
   });
 
   afterAll(() => {
-    // `nx reset` kills the daemon, and performs
-    // some work which can help clean up e2e leftovers
     runNxCommandAsync('reset');
   });
-
-  it('should create nx-vivid', async () => {
-    const project = uniq('nx-vivid');
-    await runNxCommandAsync(`generate @vonage/nx-vivid:nx-vivid ${project}`);
-    const result = await runNxCommandAsync(`build ${project}`);
-    expect(result.stdout).toContain('Executor ran');
-  }, 120000);
 
   describe('--directory', () => {
     it('should create src in the specified directory', async () => {
       const project = uniq('nx-vivid');
       await runNxCommandAsync(
-        `generate @vonage/nx-vivid:nx-vivid ${project} --directory subdir`
+        `generate @vonage/nx-vivid:component ${project}`
       );
       expect(() =>
-        checkFilesExist(`libs/subdir/${project}/src/index.ts`)
+        checkFilesExist(`libs/components/src/lib/${project}/index.ts`)
       ).not.toThrow();
-    }, 120000);
-  });
-
-  describe('--tags', () => {
-    it('should add tags to the project', async () => {
-      const projectName = uniq('nx-vivid');
-      ensureNxProject('@vonage/nx-vivid', 'dist/libs/nx-vivid');
-      await runNxCommandAsync(
-        `generate @vonage/nx-vivid:nx-vivid ${projectName} --tags e2etag,e2ePackage`
-      );
-      const project = readJson(`libs/${projectName}/project.json`);
-      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+      expect(() =>
+        checkFilesExist(`libs/components/src/lib/${project}/ui.test.ts`)
+      ).not.toThrow();
+      expect(() =>
+        checkFilesExist(`libs/components/src/lib/${project}/README.md`)
+      ).not.toThrow();
+      expect(() =>
+        checkFilesExist(`libs/components/src/lib/${project}/${project}.ts`)
+      ).not.toThrow();
+      expect(() =>
+        checkFilesExist(`libs/components/src/lib/${project}/${project}.template.ts`)
+      ).not.toThrow();
+      expect(() =>
+        checkFilesExist(`libs/components/src/lib/${project}/${project}.spec.ts`)
+      ).not.toThrow();
+      expect(() =>
+        checkFilesExist(`libs/components/src/lib/${project}/${project}.scss`)
+      ).not.toThrow();
     }, 120000);
   });
 });
