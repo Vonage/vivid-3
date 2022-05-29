436d839a963309fa4af8f9c073d92a2c7efd44e6
Rina <rina.oksman@vonage.com>
2022-05-29 17:19:45 +0300

    update ui tests

--- libs/components/src/lib/top-app-bar/ui.test.ts
+++ libs/components/src/lib/top-app-bar/ui.test.ts
@@ -7,7 +7,7 @@ import {
   loadTemplate,
 } from '../../visual-tests/visual-tests-utils.ts';
 
-const components = ['top-app-bar'];
+const components = ['top-app-bar', 'button', 'text', 'side-drawer', 'sidenav-item' ];
 
 test('should show the component', async ({ page }: { page: Page }) => {
   const template = extractHTMLBlocksFromReadme(
