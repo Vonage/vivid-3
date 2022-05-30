0df3e0527d6d1deebc19af4359d0e1a6c61e0f39
Rina <rina.oksman@vonage.com>
2022-05-30 16:37:59 +0300

    add tests

--- libs/components/src/lib/top-app-bar/top-app-bar.spec.ts
+++ libs/components/src/lib/top-app-bar/top-app-bar.spec.ts
@@ -1,4 +1,4 @@
-import { fixture } from '@vivid-nx/shared';
+import { elementUpdated, fixture, getControlElement } from '@vivid-nx/shared';
 import { TopAppBar } from './top-app-bar';
 import '.';
 
@@ -16,6 +16,57 @@ describe('vwc-top-app-bar', () => {
   describe('basic', () => {
     it('should be initialized as a vwc-top-app-bar', async () => {
       expect(element).toBeInstanceOf(TopAppBar);
+			expect(element.alternate).toBeFalsy();
+      expect(element.shadow).toBeFalsy();
+			expect(element.fixed).toBeFalsy();
+      expect(element.heading).toBeUndefined();
     });
+  });
+
+  describe('fixed', () => {
+		it('should set "fixed" to true and add "fixed" class', async () => {
+			const control = getControlElement(element);
+			let hasClassFixed = control.classList.contains('fixed');
+			element.fixed = true;
+			await elementUpdated(element);
+			expect(hasClassFixed).toEqual(false);
+
+			hasClassFixed = control.classList.contains('fixed');
+			expect(hasClassFixed).toEqual(true);
+		});
+	});
+
+	describe('alternate', () => {
+		it('should set "alternate" to true and add "alternate" class', async () => {
+			const control = getControlElement(element);
+			let hasClassAlternate = control.classList.contains('alternate');
+			element.alternate = true;
+			await elementUpdated(element);
+			expect(hasClassAlternate).toEqual(false);
+
+			hasClassAlternate = control.classList.contains('alternate');
+			expect(hasClassAlternate).toEqual(true);
+		});
+	});
+
+	describe('shadow', () => {
+		it('should set "shadow" to true and add "shadow" class', async () => {
+			const control = getControlElement(element);
+			let hasClassShadow = control.classList.contains('shadow');
+			element.shadow = true;
+			await elementUpdated(element);
+			expect(hasClassShadow).toEqual(false);
+
+			hasClassShadow = control.classList.contains('shadow');
+			expect(hasClassShadow).toEqual(true);
+		});
+	});
+
+	it('should render heading if heading is set', async function () {
+    element.heading = 'top app bar';
+    await elementUpdated(element);
+
+    const headerContent = element.shadowRoot?.querySelector('.header-content');
+    expect(headerContent).toBeTruthy();
   });
 });
