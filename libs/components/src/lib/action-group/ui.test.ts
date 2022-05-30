e298a93b7bcd63068bafae7babf34a20751df648
Rachel BT <rachel.tannenbaum@vonage.com>
2022-05-30 15:43:20 +0300

    update ui test

--- libs/components/src/lib/action-group/ui.test.ts
+++ libs/components/src/lib/action-group/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.ts';
 
-const components = ['action-group'];
+const components = ['action-group', 'button', 'text-field'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
