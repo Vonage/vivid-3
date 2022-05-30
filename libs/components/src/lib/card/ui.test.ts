bea6f0cc72385adf8c904c754476c30b3d87ea42
Rachel BT <rachel.tannenbaum@vonage.com>
2022-05-30 16:34:51 +0300

    add button to text file

--- libs/components/src/lib/card/ui.test.ts
+++ libs/components/src/lib/card/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['card'];
+const components = ['card', 'button'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
