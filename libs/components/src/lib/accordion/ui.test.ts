0e5abeec10b36954e07823f8cfa1681a29d81cee
yinon <yinon@hotmail.com>
2022-06-19 15:29:05 +0300

    ci(components): viewport size to improve ui tests performance (#468)
    * ci(components): improve ui tests performance by viewport size
    
    * update height
    
    * calendar
    
    * ts to js
    
    * updates
    
    * update snapshots
    
    * snapshots
    
    * Change files
    
    * snapshots
    
    * update
    
    * update
    
    * update
    
    * snap

--- libs/components/src/lib/accordion/ui.test.ts
+++ libs/components/src/lib/accordion/ui.test.ts
@@ -1,11 +1,11 @@
 import * as path from 'path';
-import { expect, test } from '@playwright/test'; 
+import { expect, test } from '@playwright/test';
 import type { Page } from '@playwright/test';
 import {
 	extractHTMLBlocksFromReadme,
 	loadComponents,
 	loadTemplate,
-} from '../../visual-tests/visual-tests-utils.ts';
+} from '../../visual-tests/visual-tests-utils.js';
 
 const components = ['accordion','accordion-item'];
 
@@ -17,6 +17,8 @@ test('should show the component', async ({ page }: { page: Page }) => {
 			`${htmlString} <div style="margin: 5px;">${block}</div>`,
 		''
 	);
+
+	page.setViewportSize({ width: 500, height: 720 });
 
 	await loadComponents({
 		page,
