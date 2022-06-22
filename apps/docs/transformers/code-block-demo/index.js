498122fc63b72d09099b26a130ec1817cf9fa998
yinonov <yinon@hotmail.com>
2022-06-22 14:09:33 +0300

    update

--- apps/docs/transformers/code-block-demo/index.js
+++ apps/docs/transformers/code-block-demo/index.js
@@ -70,7 +70,7 @@ const getHtml = (demoData) => {
   <vwc-elevation dp="0">
     <div class="${CBD_BASE}">
       <div class="iframe-container">
-        <iframe class="${CBD_DEMO}" src="${iframeSrc}" onload=onloadIframe(this) loading="lazy" aria-label="code block preview iframe" role="presentation"></iframe>
+        <iframe class="${CBD_DEMO}" src="${iframeSrc}" onload=onloadIframe(this) loading="lazy" aria-label="code block preview iframe"></iframe>
         <vwc-fab class="${CBD_FAB}" aria-label="Show source code" icon="curly-brackets-line" aria-expanded="false" aria-controls="${codeBlockId}" onclick="codeBlockButtonClick(this)"></vwc-fab>
       </div>
       <details class="${CBD_DETAILS}" ontoggle="detailsToggle()">
