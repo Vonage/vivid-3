56e353d66660a3ddf29924a599e56aa48ecb3b26
Rina <rina.oksman@vonage.com>
2022-06-23 12:52:02 +0300

    lint

--- libs/components/src/lib/header/ui.test.ts
+++ libs/components/src/lib/header/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.ts';
 
-const components = ['header', 'button', 'text', 'side-drawer', 'sidenav-item', 'layout' ];
+const components = ['header', 'button', 'text', 'side-drawer', 'sidenav-item', 'layout'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
@@ -48,13 +48,24 @@ test('should add elevation to fixed header when scrolled', async ({ page }: { pa
   
 	  <vwc-text font-face="body-1">
 		<p>
-		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in. Fusce id pulvinar massa. In est erat, gravida sed velit id, tempus tempus metus. Proin mollis auctor orci. Curabitur vestibulum elementum imperdiet. Mauris ac nisl vel nisi auctor sodales. Vestibulum vel rutrum leo, a convallis tellus. Aliquam vel ultricies elit, eget malesuada orci. Praesent ut blandit nisl. Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui. Suspendisse potenti. Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in. Fusce id pulvinar massa. 
+		  In est erat, gravida sed velit id, tempus tempus metus. Proin mollis auctor orci. 
+		  Curabitur vestibulum elementum imperdiet. Mauris ac nisl vel nisi auctor sodales. 
+		  Vestibulum vel rutrum leo, a convallis tellus. Aliquam vel ultricies elit, eget malesuada orci. Praesent ut blandit nisl.
+		   Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui. Suspendisse potenti. Ut et massa arcu.
+		    Pellentesque pellentesque id tortor at ornare.
 		</p>
 	  </vwc-text>
   
 	  <vwc-text font-face="body-1">
 		<p>
-		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in. Fusce id pulvinar massa. In est erat, gravida sed velit id, tempus tempus metus. Proin mollis auctor orci. Curabitur vestibulum elementum imperdiet. Mauris ac nisl vel nisi auctor sodales. Vestibulum vel rutrum leo, a convallis tellus. Aliquam vel ultricies elit, eget malesuada orci. Praesent ut blandit nisl. Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui. Suspendisse potenti. Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in.
+		   Fusce id pulvinar massa. In est erat, gravida sed velit id, tempus tempus metus.
+		    Proin mollis auctor orci. Curabitur vestibulum elementum imperdiet. 
+			Mauris ac nisl vel nisi auctor sodales. Vestibulum vel rutrum leo, a convallis tellus.
+			 Aliquam vel ultricies elit, eget malesuada orci. Praesent ut blandit nisl.
+			  Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui. Suspendisse potenti.
+			   Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
 		</p>
 	  </vwc-text>
 	</vwc-layout>
