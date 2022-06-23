9ed4871128ef70ef22763ba6309ca5a7f2e6f9a2
yinon <yinon@hotmail.com>
2022-06-23 17:40:42 +0300

    feat(components): migrate checkbox (#458)
    * feat(components): migrate checkbox
    
    * progress
    
    * simplify
    
    * progress
    
    * functioning
    
    * tests
    
    * stylelint
    
    * more tests
    
    * dependency failure
    
    * include patched methods
    
    * checkbox snapshots
    
    * smaller image
    
    * Extract form association test utils
    
    * lint
    
    * missed elementUpdated
    
    * duplicate test removed
    
    * Test fixing
    
    * Refactor
    
    Extract form association related utils to its own file
    
    * Remove `console.log` from side drawer
    
    * Prevent codecov errors
    
    * Fix TS coverage fluk
    
    * Fix TS coverage clash
    
    * Change files
    
    * refactor test
    
    * Update libs/components/src/lib/checkbox/checkbox.spec.ts
    
    Co-authored-by: Yonatan Kra <yonatan.kra@vonage.com>
    
    * Update libs/components/src/lib/checkbox/checkbox.spec.ts
    
    Co-authored-by: Yonatan Kra <yonatan.kra@vonage.com>
    
    Co-authored-by: yonatankra <yonatan.kra@vonage.com>

---
+++ libs/components/src/lib/checkbox/checkbox.template.ts
@@ -0,0 +1,59 @@
+import { html, when } from '@microsoft/fast-element';
+import type { ViewTemplate } from '@microsoft/fast-element';
+import type {
+	CheckboxOptions,
+	FoundationElementTemplate,
+} from '@microsoft/fast-foundation';
+import { classNames } from '@microsoft/fast-web-utilities';
+import { focusTemplateFactory } from '../../shared/patterns/focus';
+import { Icon } from '../icon/icon';
+import type { Checkbox } from './checkbox';
+
+const getClasses = ({
+	readOnly, checked, disabled, indeterminate
+}: Checkbox) =>
+	classNames(
+		'base',
+		['readonly', Boolean(readOnly)],
+		['checked', Boolean(checked)],
+		['disabled', Boolean(disabled)],
+		['indeterminate', Boolean(indeterminate)],
+	);
+
+
+/**
+ * The template for the {@link @microsoft/fast-foundation#Checkbox} component.
+ *
+ * @param context
+ * @param _
+ * @param definition
+ * @public
+ */
+export const CheckboxTemplate: FoundationElementTemplate<
+ViewTemplate<Checkbox>,
+CheckboxOptions
+> = (context) => {
+	const focusTemplate = focusTemplateFactory(context);
+	const iconTag = context.tagFor(Icon);
+
+	return html`<span
+  role="checkbox"
+  aria-checked="${x => x.checked}"
+  aria-required="${x => x.required}"
+  aria-disabled="${x => x.disabled}"
+  aria-readonly="${x => x.readOnly}"
+  tabindex="${x => (x.disabled ? null : 0)}"
+  @keypress="${(x, c) => x.keypressHandler(c.event as KeyboardEvent)}"
+  @click="${(x) => x.clickHandler()}"
+  class="${getClasses}"
+  >
+    <div class="control">
+      <${iconTag} class="indicator checkmark" type="check-solid"></${iconTag}>
+      <${iconTag} class="indicator minus" type="minus-solid"></${iconTag}>
+      ${() => focusTemplate}
+    </div>
+
+    ${when(x => x.label, html<Checkbox>`<label>${x => x.label}</label>`)}
+
+  </span>`;
+};
