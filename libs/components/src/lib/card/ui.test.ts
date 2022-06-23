d411e3363c233187f29a21cb5bc0b7e3e7323f8f
yinon <yinon@hotmail.com>
2022-06-23 20:05:37 +0300

    chore(components): refactor card to block flow (#485)

--- libs/components/src/lib/card/ui.test.ts
+++ libs/components/src/lib/card/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['card', 'button', 'layout'];
+const components = ['card', 'button', 'layout', 'text'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
@@ -18,7 +18,7 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		''
 	);
 
-	page.setViewportSize({ width: 360, height: 720 });
+	page.setViewportSize({ width: 420, height: 720 });
 
 	await loadComponents({
 		page,
