36e00540840b9b6724304a4e4e2b99dec30f0553
yinon <yinon@hotmail.com>
2022-06-26 12:38:15 +0300

    chore(docs): cross browsers compatibility (#486)
    * chore(components): refactor card to block flow
    
    * Change files
    
    * snapshots
    
    * control to base
    
    * chore(docs): cross browsers compatibility
    
    * style
    
    * update
    
    Co-authored-by: rachelbt <rachelbt@users.noreply.github.com>

--- apps/docs/transformers/code-block-demo/index.js
+++ apps/docs/transformers/code-block-demo/index.js
@@ -9,7 +9,7 @@ const ELEVENTY_HTML_CODE_BLOCK_SELECTOR = 'pre.preview > code';
 const IFRAME_STYLE = '<link rel="stylesheet" href="/assets/styles/iframe.css">';
 const FONTS = '<link rel="stylesheet" href="/assets/styles/fonts/spezia.css">';
 
-const CBD_BASE = 'cbd-base';
+const CBD_CONTAINER = 'cbd-container';
 const CBD_DEMO = 'cbd-demo';
 const CBD_DETAILS = 'cbd-details';
 const CBD_BUTTON_SHOW = 'cbd-button-show';
@@ -37,7 +37,7 @@ const generateCodeBlockDemo = function (blockData) {
 
   const dom = new JSDOM(`<body>${getHtml(demoData)}</body>`);
 
-  return dom.window.document.querySelector('vwc-elevation');
+  return dom.window.document.querySelector(`.${CBD_CONTAINER}`);
 };
 
 module.exports = function (content, outputPath) {
@@ -67,9 +67,9 @@ const getHtml = (demoData) => {
   const iframeSrc = getIframe(frameData);
 
   return `
-    <vwc-elevation dp="0">
-      <div class="${CBD_BASE}">
-      <iframe class="${CBD_DEMO}" src="${iframeSrc}" onload=onloadIframe(this) loading="lazy"></iframe>
+    <vwc-card elevation="0" class="${CBD_CONTAINER}">
+      <div slot="main">
+        <iframe class="${CBD_DEMO}" src="${iframeSrc}" onload=onloadIframe(this) loading="lazy"></iframe>
         <details class="${CBD_DETAILS}">
             <summary>
                 <vwc-button label="source" icon="chevron-down-line" icon-trailing class="${CBD_BUTTON_SHOW}" aria-expanded="false" aria-controls="${codeBlockId}">
@@ -80,7 +80,7 @@ const getHtml = (demoData) => {
             </div>
         </details>
       </div>
-    </vwc-elevation>`;
+    </vwc-card>`;
 }
 
 const getIframe = (frameData) => {
