971ff78f2e5da21d5f875dc3358de4410bc7456c
Yonatan Kra <yonatan.kra@vonage.com>
2022-06-27 14:07:57 +0300

    chore(workspace): add "no console" rule (#494)

--- libs/nx-vivid/src/executors/build/executor.ts
+++ libs/nx-vivid/src/executors/build/executor.ts
@@ -1,7 +1,6 @@
 import { BuildExecutorSchema } from './schema';
 
 export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build', options);
   return {
     success: true,
   };
