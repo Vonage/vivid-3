3735609b0a07f5f9d6841fc78e295db0d8fc6a60
Yonatan Kra <yonatan.kra@vonage.com>
2022-06-28 14:38:53 +0300

    fix(text-field): make sure validation is not eager (#454)

--- libs/components/src/lib/text-field/ui.test.ts
+++ libs/components/src/lib/text-field/ui.test.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
-import { expect, test } from '@playwright/test';
-import type { Page } from '@playwright/test';
+import {expect, test} from '@playwright/test';
+import type {Page} from '@playwright/test';
 import {
 	extractHTMLBlocksFromReadme,
 	loadComponents,
@@ -9,16 +9,17 @@ import {
 
 const components = ['text-field'];
 
-test('should show the component', async ({ page }: { page: Page }) => {
+test('should show the component', async ({page}: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
 		path.join(new URL('.', import.meta.url).pathname, 'README.md')
-	).reduce(
-		(htmlString: string, block: string) =>
-			`${htmlString} <div style="margin: 5px;">${block}</div>`,
-		''
-	);
+	)
+		.reduce(
+			(htmlString: string, block: string) =>
+				`${htmlString} <div style="margin: 5px;">${block}</div>`,
+			''
+		);
 
-	page.setViewportSize({ width: 300, height: 720 });
+	await page.setViewportSize({ width: 300, height: 720 });
 
 	await loadComponents({
 		page,
@@ -33,7 +34,41 @@ test('should show the component', async ({ page }: { page: Page }) => {
 
 	await page.waitForLoadState('networkidle');
 
-	expect(await testWrapper?.screenshot()).toMatchSnapshot(
-		'./snapshots/text-field.png'
-	);
+	expect(await testWrapper?.screenshot())
+		.toMatchSnapshot(
+			'./snapshots/text-field.png'
+		);
+});
+
+test('should invalidate the component', async ({page, browserName}: { page: Page, browserName: string }) => {
+	const selector = browserName === 'chromium' ? 'input[name="invalid-text-field"]' : '#invalid-text-field';
+
+	const template = `<vwc-text-field id="invalid-text-field" 
+																		label="invalid" 
+																		pattern="123" 
+																		value="5" 
+																		name="invalid-text-field"></vwc-text-field>`;
+
+	await loadComponents({
+		page,
+		components,
+	});
+	await loadTemplate({
+		page,
+		template,
+	});
+
+	const testWrapper = await page.$('#wrapper');
+
+	const invalidTextField = await page.locator(selector);
+	await invalidTextField.type('55');
+	await invalidTextField.evaluate(e => {
+		e.blur();
+	});
+
+	await page.waitForLoadState('networkidle');
+	expect(await testWrapper?.screenshot())
+		.toMatchSnapshot(
+			'./snapshots/text-field-invalidation.png'
+		);
 });
