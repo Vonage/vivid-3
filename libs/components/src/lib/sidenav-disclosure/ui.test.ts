6413bddfeb01e64a6b46221288ce2473ea571479
Rina <rina.oksman@vonage.com>
2022-06-29 17:28:26 +0300

    update ui tests

---
+++ libs/components/src/lib/sidenav-disclosure/ui.test.ts
@@ -0,0 +1,34 @@
+import * as path from 'path';
+import { expect, test } from '@playwright/test';
+import type { Page } from '@playwright/test';
+import {
+	extractHTMLBlocksFromReadme,
+	loadComponents,
+	loadTemplate,
+} from '../../visual-tests/visual-tests-utils.js';
+
+const components = ['sidenav-disclosure', 'sidenav-item'];
+
+test('should show the component', async ({ page }: { page: Page }) => {
+	const template = extractHTMLBlocksFromReadme(path.join(new URL('.', import.meta.url).pathname, 'README.md'))
+		.reduce((htmlString: string, block: string) => `${htmlString} <div style="margin: 5px;">${block}</div>`,'');
+
+	page.setViewportSize({ width: 200, height: 720 });
+
+	await loadComponents({
+		page,
+		components,
+	});
+	await loadTemplate({
+		page,
+		template,
+	});
+
+	const testWrapper = await page.$('#wrapper');
+
+	await page.waitForLoadState('networkidle');
+
+	expect(await testWrapper?.screenshot()).toMatchSnapshot(
+		'./snapshots/sidenav-disclosure.png'
+	);
+});
