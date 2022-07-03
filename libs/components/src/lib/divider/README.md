c6fdd79990a766a23313b9056f7bc9a3b4137ca4
Rachel BT <rachel.tannenbaum@vonage.com>
2022-07-03 12:41:57 +0300

    tests

--- libs/components/src/lib/divider/README.md
+++ libs/components/src/lib/divider/README.md
@@ -19,7 +19,7 @@
 </div>
 ```
 
-### roll
+### role
 The divider should default to having a role of separator, just as a typical horizontal rule would.  
 A role of `presentation` implies that the divider is a visual treatment only so the contrast requirement does not apply in that case.
 
@@ -32,10 +32,10 @@ A role of `presentation` implies that the divider is a visual treatment only so 
 <vwc-action-group appearance="fieldset">
   <vwc-button icon="reply-line"></vwc-button>
   <vwc-button icon="transfer-line"></vwc-button>
-  <vwc-divider orientation="vertical" roll="presentation"></vwc-divider>
+  <vwc-divider orientation="vertical"></vwc-divider>
   <vwc-button icon="compose-line"></vwc-button>
   <vwc-button icon="crop-line"></vwc-button>
-  <vwc-divider orientation="vertical" roll="presentation"></vwc-divider>
+  <vwc-divider orientation="vertical"></vwc-divider>
   <vwc-button icon="copy-2-line"></vwc-button>
   <vwc-button icon="save-line"></vwc-button>
 </vwc-action-group>
--- libs/components/src/lib/divider/divider.spec.ts
+++ libs/components/src/lib/divider/divider.spec.ts
@@ -1,21 +1,42 @@
-import { fixture } from '@vivid-nx/shared';
+import {elementUpdated, fixture, getBaseElement} from '@vivid-nx/shared';
 import { Divider } from './divider';
 import '.';
 
 const COMPONENT_TAG = 'vwc-divider';
 
 describe('vwc-divider', () => {
-  let element: Divider;
+	let element: Divider;
 
-  beforeEach(async () => {
-    element = (await fixture(
-      `<${COMPONENT_TAG}></${COMPONENT_TAG}>`
-    )) as Divider;
-  });
+	beforeEach(async () => {
+		element = (await fixture(
+			`<${COMPONENT_TAG}></${COMPONENT_TAG}>`
+		)) as Divider;
+	});
 
-  describe('basic', () => {
-    it('should be initialized as a vwc-divider', async () => {
-      expect(element).toBeInstanceOf(Divider);
-    });
-  });
+	describe('basic', () => {
+		it('should be initialized as a vwc-divider', async () => {
+			expect(element).toBeInstanceOf(Divider);
+		});
+	});
+
+	describe('orientation', function () {
+		it('should set the orientation class', async function() {
+			const control = element.shadowRoot?.querySelector('.base');
+			const orientation = 'vertical';
+			(element as any).orientation = orientation;
+			await elementUpdated(element);
+
+			expect(control?.classList.contains(`${orientation}`)).toBeTruthy();
+		});
+	});
+
+	describe('role', function () {
+		it('should set the role attribute on base', async function() {
+			element.toggleAttribute('roll', true);
+			await elementUpdated(element);
+
+			expect(getBaseElement(element).hasAttribute('role')).toEqual(true);
+		});
+	});
+
 });
