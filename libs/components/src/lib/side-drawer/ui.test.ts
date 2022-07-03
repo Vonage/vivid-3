e7db6d9e0722a42285eb2f761e6ad4cbc1c09e6f
yinonov <yinon@hotmail.com>
2022-07-03 22:50:14 +0300

    small size test

--- libs/components/src/lib/side-drawer/ui.test.ts
+++ libs/components/src/lib/side-drawer/ui.test.ts
@@ -5,49 +5,32 @@ import {
 	loadTemplate
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['side-drawer', 'text', 'fab', 'sidenav-item', 'layout'];
+const components = ['side-drawer', 'text', 'layout'];
 test('should show the component', async ({ page }: { page: Page }) => {
-	const template = `<style>
-    vwc-side-drawer#sideDrawer{
-        --side-drawer-background-color: var(--vvd-color-neutral-10);
-        --side-drawer-inline-size: 200px;
-    }
-</style>
-<vwc-side-drawer id="sideDrawer" open>
-	<div slot="header">
-        <vwc-text tight font-face="subtitle-1">VIVID</vwc-text>
-    </div>
-	<div>
-		<vwc-sidenav-item href="#" text="1st level item" icon="home-line"></vwc-sidenav-item>
-        <p><vwc-text font-face="body-2-bold">SECTION TITLE</vwc-text></p>
-        <vwc-sidenav-item href="#" text="1st level item" icon="chat-line"></vwc-sidenav-item>
-        <vwc-sidenav-item href="#" text="1st level item" icon="chat-line"></vwc-sidenav-item>
-	</div>
-	<vwc-layout slot="app-content" column-basis="block" gutters="medium">
-		<vwc-button id="button" shape="pill" icon='menu-solid'></vwc-button>
-		<vwc-text font-face="body-1">
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in. Fusce id pulvinar massa.
-			In est erat, gravida sed velit id, tempus tempus metus. Proin mollis auctor orci. Curabitur vestibulum elementum imperdiet.
-			Mauris ac nisl vel nisi auctor sodales. Vestibulum vel rutrum leo, a convallis tellus.
-			Aliquam vel ultricies elit, eget malesuada orci.
-			Praesent ut blandit nisl. Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui.
-			Suspendisse potenti. Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
-		</p>
-		</vwc-text>
+	const template = `
+    <vwc-side-drawer open>
+      <div slot="header">
+        Header
+      </div>
 
-		<vwc-text font-face="body-1">
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in.
-			 Fusce id pulvinar massa. In est erat, gravida sed velit id, tempus tempus metus. Proin mollis auctor orci.
-			  Curabitur vestibulum elementum imperdiet. Mauris ac nisl vel nisi auctor sodales.
-			   Vestibulum vel rutrum leo, a convallis tellus. Aliquam vel ultricies elit, eget malesuada orci.
-			    Praesent ut blandit nisl. Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui.
-				 Suspendisse potenti. Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
-		</p>
-		</vwc-text>
-	</vwc-layout>
-</vwc-side-drawer>`;
+      <vwc-text>
+        Body
+      </vwc-text>
+
+      <vwc-layout slot="app-content" column-basis="block" gutters="small">
+        <vwc-text>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in. Fusce id pulvinar massa.
+            In est erat, gravida sed velit id, tempus tempus metus. Proin mollis auctor orci. Curabitur vestibulum elementum imperdiet.
+            Mauris ac nisl vel nisi auctor sodales. Vestibulum vel rutrum leo, a convallis tellus.
+            Aliquam vel ultricies elit, eget malesuada orci.
+            Praesent ut blandit nisl. Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui.
+            Suspendisse potenti. Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
+          </p>
+        </vwc-text>
+      </vwc-layout>
+    </vwc-side-drawer>
+  `;
 
 	page.setViewportSize({ width: 1200, height: 720 });
 
@@ -70,45 +53,31 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		);
 });
 
-test('should show the component 2', async ({ page }: { page: Page }) => {
-	const template = `<style>
-    vwc-side-drawer#sideDrawer{
-        --side-drawer-background-color: var(--vvd-color-neutral-10);
-        --side-drawer-inline-size: 200px;
-    }
-</style>
-<vwc-side-drawer id="sideDrawer" open position="end" alternate modal>
-	<div>
-		<vwc-sidenav-item href="#" text="1st level item" icon="home-line"></vwc-sidenav-item>
-        <p><vwc-text font-face="body-2-bold">SECTION TITLE</vwc-text></p>
-        <vwc-sidenav-item href="#" text="1st level item" icon="chat-line"></vwc-sidenav-item>
-        <vwc-sidenav-item href="#" text="1st level item" icon="chat-line"></vwc-sidenav-item>
-	</div>
-	<vwc-layout slot="app-content" column-basis="block" gutters="medium">
-		<vwc-button id="button" shape="pill" icon='menu-solid'></vwc-button>
-		<vwc-text font-face="body-1">
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in.
-			 Fusce id pulvinar massa. In est erat, gravida sed velit id, tempus tempus metus. Proin mollis auctor orci.
-			  Curabitur vestibulum elementum imperdiet. Mauris ac nisl vel nisi auctor sodales.
-			   Vestibulum vel rutrum leo, a convallis tellus. Aliquam vel ultricies elit, eget malesuada orci.
-			    Praesent ut blandit nisl. Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui.
-				 Suspendisse potenti. Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
-		</p>
-		</vwc-text>
+test('should show the component modal', async ({ page }: { page: Page }) => {
+	const template = `
+    <vwc-side-drawer open modal alternate>
+      <div slot="header">
+        Header
+      </div>
 
-		<vwc-text font-face="body-1">
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in.
-			 Fusce id pulvinar massa. In est erat, gravida sed velit id, tempus tempus metus.
-			  Proin mollis auctor orci. Curabitur vestibulum elementum imperdiet. Mauris ac nisl vel nisi auctor sodales.
-			   Vestibulum vel rutrum leo, a convallis tellus. Aliquam vel ultricies elit, eget malesuada orci.
-			    Praesent ut blandit nisl. Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui.
-				 Suspendisse potenti. Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
-		</p>
-		</vwc-text>
-	</vwc-layout>
-</vwc-side-drawer>`;
+      <vwc-text>
+        Body
+      </vwc-text>
+
+      <vwc-layout slot="app-content" column-basis="block" gutters="small">
+        <vwc-text>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis ante est, ac porta sapien rutrum in. Fusce id pulvinar massa.
+            In est erat, gravida sed velit id, tempus tempus metus. Proin mollis auctor orci. Curabitur vestibulum elementum imperdiet.
+            Mauris ac nisl vel nisi auctor sodales. Vestibulum vel rutrum leo, a convallis tellus.
+            Aliquam vel ultricies elit, eget malesuada orci.
+            Praesent ut blandit nisl. Morbi ut ligula faucibus ante pellentesque condimentum sit amet ac dui.
+            Suspendisse potenti. Ut et massa arcu. Pellentesque pellentesque id tortor at ornare.
+          </p>
+        </vwc-text>
+      </vwc-layout>
+    </vwc-side-drawer>
+  `;
 
 	await loadComponents({
 		page,
