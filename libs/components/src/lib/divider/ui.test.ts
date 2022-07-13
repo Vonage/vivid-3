9738a510a9d23b7c92150d4eb70795df21ad799b
rachelbt <rachelbt@users.noreply.github.com>
2022-07-13 14:01:15 +0300

    Update libs/components/src/lib/divider/ui.test.ts
    Co-authored-by: Rina Oksman <rina.oksman@vonage.com>

--- libs/components/src/lib/divider/ui.test.ts
+++ libs/components/src/lib/divider/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['divider', 'button', 'action-group', 'text', 'card'];
+const components = ['divider', 'button', 'action-group', 'text', 'card', 'layout'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
