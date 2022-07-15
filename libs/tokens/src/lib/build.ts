f107b2880a7b1c74351e404b0fd9a0a7d7ff2466
yinonov <yinon@hotmail.com>
2022-07-15 15:28:55 +0300

    cwd

--- libs/tokens/src/lib/build.ts
+++ libs/tokens/src/lib/build.ts
@@ -1,14 +1,14 @@
 const notAliasFilter = require('./filters/not-alias');
 const StyleDictionary = require('style-dictionary')
 	.registerFilter(notAliasFilter)
-	.extend('libs/tokens/src/lib/config.json');
+	.extend('config.json');
 
 const themes = ['light'];
 themes.forEach(brand => {
   StyleDictionary.extend({
     // include: [`tokens/default/**/*.json`],
 		source: [
-			`libs/tokens/src/lib/tokens-from-figma/mappings/schemes/theme-${brand}-main.tokens.json`
+			`tokens-from-figma/mappings/schemes/theme-${brand}-main.tokens.json`
 		],
   }).buildAllPlatforms();
 });
---
+++ libs/tokens/src/lib/dist/libs/tokens/js/lib/tokens.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 15 Jul 2022 12:28:52 GMT
+ */
+
+export const ColorCta100 : string;
+export const ColorCta200 : string;
+export const ColorCta500 : string;
+export const ColorCta600 : string;
+export const ColorCta700 : string;
+export const ColorCta800 : string;
+export const ColorCta050 : string;
+export const ColorSurface0dp : string;
+export const Shadow0dp : { color: string, offsetX: string, offsetY: string, blur: string, spread: string };
