471b4cb7b7fa6180a192847246fa2823ed2559f8
yinonov <yinon@hotmail.com>
2022-07-18 22:00:06 +0300

    experiementing

--- libs/tokens/src/lib/build.ts
+++ libs/tokens/src/lib/build.ts
@@ -1,21 +1,39 @@
 const notAliasFilter = require('./filters/not-alias');
 const StyleDictionary = require('style-dictionary')
+
 	.registerFilter(notAliasFilter)
 	.extend('config.json');
 
+const { transform } = StyleDictionary;
+const { transformer: colorToRGB } = transform['color/rgb'];
+const { transformer: sizeToPx } = transform['size/px'];
 
+StyleDictionary.registerTransform({
+		type: `value`,
+		transitive: true,
+		name: `shadow/shorthand`,
+		matcher: ({ attributes: { category }}) => category == 'shadow',
+		transformer: ({ value }) =>
+			value.map((
+				{ color, offsetX, offsetY, spread }
+			) => `drop-shadow(${colorToRGB(color)} ${sizeToPx({value:offsetX})})`, '').join(' ')
+			// const { color, offsetX, offsetY, spread } = value;
+			// token.value will be resolved and transformed at this point
+			// return 'yoy1o'
+
+	})
 const getStyleDictionaryConfig = (theme: string): any => ({
 	source: [
 		`tokens-from-figma/mappings/schemes/theme-${theme}-main.tokens.json`
 	],
 	platforms: {
 		web: {
-			transformGroup: "web",
+			transforms: ["attribute/cti", "name/cti/kebab", "size/px", "color/rgb", "shadow/shorthand"],
 			prefix: "vvd",
 			buildPath: `../../../../dist/libs/tokens/scss/themes/${theme}/`,
 			files: [{
 				destination: "_main.scss",
-				format: "scss/map-flat",
+				format: "css/variables",
 				filter: "isNotAlias"
 			}]
 		}
