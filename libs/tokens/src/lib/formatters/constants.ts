066d5de3244e5cd6425def59fb8f879b318a6723
yinonov <yinon@hotmail.com>
2022-07-27 23:16:52 +0300

    with header

--- libs/tokens/src/lib/formatters/constants.ts
+++ libs/tokens/src/lib/formatters/constants.ts
@@ -1,7 +1,9 @@
+const { formatHelpers: { fileHeader } } = require('style-dictionary');
+
 module.exports = {
 	name: 'scss/constants',
-	formatter: function ({ dictionary }) {
-		return dictionary.allProperties.map(function (prop) {
+	formatter: function ({ dictionary, file }) {
+		return fileHeader({file}) + dictionary.allProperties.map(function (prop) {
 			return `$${prop.name}: --${prop.name};`
 		}).join('\n');
 	}
