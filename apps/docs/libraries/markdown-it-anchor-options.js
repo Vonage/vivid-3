816d71229b865f17123dd331f5041aa14d533309
Rachel BT <rachel.tannenbaum@vonage.com>
2022-07-28 15:30:03 +0300

    style h1

--- apps/docs/libraries/markdown-it-anchor-options.js
+++ apps/docs/libraries/markdown-it-anchor-options.js
@@ -38,33 +38,3 @@ module.exports = {
     linkAfterHeader(slug, opts, state, idx + 1);
   },
 };
-
-module.exports = {
-    level: [1],
-    slugify: (str) =>
-        slugify(str, {
-            lower: true,
-            strict: true,
-            remove: /["]/g,
-        }),
-    tabIndex: false,
-    permalink(slug, opts, state, idx) {
-        state.tokens.splice(
-            idx,
-            0,
-            Object.assign(new state.Token("div_open", "div", 1), {
-                // Add class "header-wrapper [h1 or h2 or h3]"
-                attrs: [["class", `component-heading`]],
-                block: true,
-            })
-        );
-
-        state.tokens.splice(
-            idx + 4,
-            0,
-            Object.assign(new state.Token("div_close", "div", -1), {
-                block: true,
-            })
-        );
-    },
-};
