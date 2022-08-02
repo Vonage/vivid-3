3aea41b4b606fd1bab921d14c38a18ae5e951d3e
yinonov <yinon@hotmail.com>
2022-08-02 10:06:34 +0300

    if not array

--- libs/tokens/src/lib/transforms/shadow-shorthand.spec.ts
+++ libs/tokens/src/lib/transforms/shadow-shorthand.spec.ts
@@ -1,46 +1,49 @@
 export {}
 const { transformer, matcher } = require('./shadow-shorthand');
 
+const token = {
+	type: 'boxShadow',
+	attributes: {
+		category: 'shadow'
+	},
+	value: [
+		{
+			x: '0',
+			y: '2px',
+			blur: '1px',
+			spread: '0',
+			color: 'rgba(rgb(0, 0, 0), 0.25)',
+			type: 'dropShadow'
+		},
+		{
+			x: '0',
+			y: '1px',
+			blur: '2px',
+			spread: '0',
+			color: 'rgba(rgb(0, 0, 0), 0.25)',
+			type: 'innerShadow'
+		},
+		{
+			x: '0',
+			y: '1px',
+			blur: '4px',
+			spread: '0',
+			color: 'rgba(rgb(0, 0, 0), 0.25)',
+			type: 'innerShadow'
+		}
+	]
+};
+
+const expectedParsedEffects = 'drop-shadow(0px 2px 1px rgb(0, 0, 0)) drop-shadow(0px 1px 2px rgb(0, 0, 0)) drop-shadow(0px 1px 4px rgb(0, 0, 0))';
 
 describe('basic', () => {
-	const token = {
-		type: 'boxShadow',
-		attributes: {
-			category: 'shadow'
-		},
-		value: [
-			{
-				x: '0',
-				y: '2px',
-				blur: '1px',
-				spread: '0',
-				color: 'rgba(rgb(0, 0, 0), 0.25)',
-				type: 'dropShadow'
-			},
-			{
-				x: '0',
-				y: '1px',
-				blur: '2px',
-				spread: '0',
-				color: 'rgba(rgb(0, 0, 0), 0.25)',
-				type: 'innerShadow'
-			},
-			{
-				x: '0',
-				y: '1px',
-				blur: '4px',
-				spread: '0',
-				color: 'rgba(rgb(0, 0, 0), 0.25)',
-				type: 'innerShadow'
-			}
-		]
-	};
-
 	it('should transform array of drop shadows to single token value', () => {
-		expect(transformer(token))
-			.toEqual(
-				'drop-shadow(0px 2px 1px rgb(0, 0, 0)) drop-shadow(0px 1px 2px rgb(0, 0, 0)) drop-shadow(0px 1px 4px rgb(0, 0, 0))'
-			);
+		expect(transformer(token)).toEqual(expectedParsedEffects);
+	});
+
+	it('should ignore already parsed value', () => {
+		expect(transformer({ value: expectedParsedEffects }))
+			.toEqual(expectedParsedEffects);
 	});
 
 	it('should match if category and type comply to a shadow type', () => {
