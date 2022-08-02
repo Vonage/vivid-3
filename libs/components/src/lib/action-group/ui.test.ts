c0c10479fa3cecde161b68ba2aaa5796340ab407
Yonatan Kra <yonatan.kra@vonage.com>
2022-08-02 16:31:38 +0300

    chore(workspace): exempt issues with labels from being stale (#586)
    * Exempt issues with labels from being stale
    
    Question,Enhancement,work-in-progress,Backlog
    
    * Stabilize the ui tests
    
    Add the "no-build" flavor, update snapshots

--- libs/components/src/lib/action-group/ui.test.ts
+++ libs/components/src/lib/action-group/ui.test.ts
@@ -18,7 +18,7 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		''
 	);
 
-	page.setViewportSize({ width: 380, height: 720 });
+	await page.setViewportSize({ width: 380, height: 720 });
 
 	await loadComponents({
 		page,
