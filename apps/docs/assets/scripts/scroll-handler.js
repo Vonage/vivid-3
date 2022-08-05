bf16e12dee6e96b67c980c3fd89be721aada1200
yinonov <yinon@hotmail.com>
2022-08-05 12:20:13 +0300

    push

--- apps/docs/assets/scripts/scroll-handler.js
+++ apps/docs/assets/scripts/scroll-handler.js
@@ -5,11 +5,18 @@ const updateHeaderElevationShadow = (isShadowed) => {
 	sideHeader.elevationShadow = isShadowed;
 }
 
-const onWindowScroll = () => {
-	updateHeaderElevationShadow(window.scrollY > 0);
+const onWindowScroll = ({ target }) => {
+	const isScrolled = target == document ? window.scrollY > 0 : target.scrollTop > 0;
+	updateHeaderElevationShadow(isScrolled);
 }
 
 (() => {
 	// hook window scroll
 	window.addEventListener('scroll', onWindowScroll);
+
+	customElements.whenDefined('vwc-side-drawer').then(() => {
+		const sideDrawer = document.querySelector('#sidedrawer');
+		const aside = sideDrawer.shadowRoot.querySelector('aside');
+		aside.addEventListener('scroll', onWindowScroll)
+	});
 })();
