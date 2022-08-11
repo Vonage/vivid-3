ffac6792470719431478782e321f6316669d60b3
yinonov <yinon@hotmail.com>
2022-08-11 15:31:14 +0300

    update

--- apps/docs/assets/scripts/main.js
+++ apps/docs/assets/scripts/main.js
@@ -1,33 +1,33 @@
 const toggleCodePanel = (event) => {
-	const button = event.target;
-	const details = button.closest(".cbd-details");
-	details.open = !details.open;
-	button.setAttribute('aria-expanded', details.open.toString());
+  const button = event.target;
+  const details = button.closest(".cbd-details");
+  details.open = !details.open;
+  button.setAttribute('aria-expanded', details.open.toString());
 };
 
 const codeBlockButtonClick = (button) => {
-	const details = button.closest('vwc-action-group').nextElementSibling;
-	details.open = !details.open;
-	button.ariaExpanded = details.open;
+  const details = button.closest('vwc-action-group').nextElementSibling;
+  details.open = !details.open;
+  button.ariaExpanded = details.open;
 };
 
 const codeCopyButtonClick = (button) => {
-	const details = button.closest('vwc-action-group').nextElementSibling;
-	const { textContent } = details;
-	navigator.clipboard.writeText(textContent.trim());
+  const details = button.closest('vwc-action-group').nextElementSibling;
+  const { textContent } = details;
+  navigator.clipboard.writeText(textContent.trim());
 };
 
 const onloadIframe = (iFrame) => {
-	const toggle = document.querySelector('dark-mode-toggle');
+  const toggle = document.querySelector('dark-mode-toggle');
 
-	iFrame.style.height = iFrame.contentWindow.document.documentElement.clientHeight + 4 + "px";
-	setCurrentIframeTheme(toggle, iFrame);
-	toggle.addEventListener('colorschemechange', () => {
-		setCurrentIframeTheme(toggle, iFrame);
-	});
+  iFrame.style.height = iFrame.contentWindow.document.documentElement.clientHeight + 4 + "px";
+  setCurrentIframeTheme(toggle, iFrame);
+  toggle.addEventListener('colorschemechange', () => {
+    setCurrentIframeTheme(toggle, iFrame);
+  });
 };
 
 const setCurrentIframeTheme = (toggle, iFrame) => {
-	const theme = toggle.mode === 'dark' ? '<link rel="stylesheet" href="/assets/styles/themes/dark.css" media="all">' : '<link rel="stylesheet" href="/assets/styles/themes/light.css" media="all">';
-	iFrame.contentWindow.document.head?.insertAdjacentHTML("beforeend", theme);
+  const theme = toggle.mode === 'dark' ? '<link rel="stylesheet" href="/assets/styles/themes/dark.css" media="all">' : '<link rel="stylesheet" href="/assets/styles/themes/light.css" media="all">';
+  iFrame.contentWindow.document.head?.insertAdjacentHTML("beforeend", theme);
 }
