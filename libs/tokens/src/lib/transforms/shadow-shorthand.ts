6effdd98c9339e91f68cfcc63583a0fd72d424ed
yinonov <yinon@hotmail.com>
2022-08-24 01:41:19 +0300

    fixed

--- libs/tokens/src/lib/transforms/shadow-shorthand.ts
+++ libs/tokens/src/lib/transforms/shadow-shorthand.ts
@@ -1,11 +1,22 @@
 const { transform } = require('style-dictionary');
+const Color = require('tinycolor2');
 
-const { transformer: colorToRGB } = transform['color/rgb'];
 const { transformer: sizeToPx } = transform['size/px'];
 
 const parseShadowEffects = value =>
-	value.map(({ x, y, blur, color }) =>
-		`drop-shadow(${sizeToPx({ value: x })} ${sizeToPx({ value: y })} ${sizeToPx({ value: blur })} ${colorToRGB(color)})`)
+	value.map(
+		({ x, y, blur, color }) => {
+			if (color.startsWith('rgba')) {
+				const start = color.indexOf('(') + 1;
+				const end = color.indexOf(')') + 1;
+				const rgb = Color(color.slice(start, end));
+				const alpha = Number(color.slice(end + 1, -1));
+				rgb.setAlpha(alpha);
+				color = rgb.toRgbString();
+			}
+			return `drop-shadow(${sizeToPx({ value: x })} ${sizeToPx({ value: y })} ${sizeToPx({ value: blur })} ${color})`
+		}
+	)
 		.join(' ')
 
 module.exports = {
