39024ca8123702cfd874ea6f309868f7b38b21b5
Rina <rina.oksman@vonage.com>
2022-08-24 13:23:38 +0300

    add side drawer scroll

--- apps/docs/assets/scripts/scroll-handler.js
+++ apps/docs/assets/scripts/scroll-handler.js
@@ -8,6 +8,20 @@ const updateHeaderElevationShadow = (isShadowed) => {
 const onWindowScroll = ({ target }) => {
 	const isScrolled = target == document ? window.scrollY > 0 : target.scrollTop > 0;
 	updateHeaderElevationShadow(isScrolled);
+	// save sideDrawer's scroll in localStorage
+	if (target.scrollTop) {
+		localStorage.setItem("scroll", target.scrollTop);
+	}
+}
+
+const setScrollFromLocalStorage = (target) => {
+	// set sideDrawer's scroll from localStorage
+	let scrollTop = localStorage.getItem("scroll");
+	if (scrollTop != null) {
+		setTimeout(() => {
+			target.scrollTop = scrollTop;
+		}, 1);
+	}
 }
 
 (() => {
@@ -17,6 +31,7 @@ const onWindowScroll = ({ target }) => {
 	customElements.whenDefined('vwc-side-drawer').then(() => {
 		const sideDrawer = document.querySelector('#sidedrawer');
 		const aside = sideDrawer.shadowRoot.querySelector('aside');
+		setScrollFromLocalStorage(aside);
 		aside.addEventListener('scroll', onWindowScroll)
 	});
 })();
