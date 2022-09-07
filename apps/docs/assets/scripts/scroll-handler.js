9c52d965c6894b65d9782cfc7a30677ed39a9003
Rina <rina.oksman@vonage.com>
2022-09-07 11:54:42 +0300

    scrollIntoView

--- apps/docs/assets/scripts/scroll-handler.js
+++ apps/docs/assets/scripts/scroll-handler.js
@@ -8,20 +8,6 @@ const updateHeaderElevationShadow = (isShadowed) => {
 const onWindowScroll = ({ target }) => {
 	const isScrolled = target == document ? window.scrollY > 0 : target.scrollTop > 0;
 	updateHeaderElevationShadow(isScrolled);
-	// save sideDrawer's scroll in localStorage
-	if (target.scrollTop) {
-		localStorage.setItem("scroll", target.scrollTop);
-	}
-}
-
-const setScrollFromLocalStorage = (target) => {
-	// set sideDrawer's scroll from localStorage
-	let scrollTop = localStorage.getItem("scroll");
-	if (scrollTop != null) {
-		setTimeout(() => {
-			target.scrollTop = scrollTop;
-		}, 1);
-	}
 }
 
 (() => {
@@ -31,7 +17,6 @@ const setScrollFromLocalStorage = (target) => {
 	customElements.whenDefined('vwc-side-drawer').then(() => {
 		const sideDrawer = document.querySelector('#sidedrawer');
 		const aside = sideDrawer.shadowRoot.querySelector('aside');
-		setScrollFromLocalStorage(aside);
 		aside.addEventListener('scroll', onWindowScroll)
 	});
 })();
