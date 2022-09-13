5d56cc9c76f13491df963a1bd43daba66fcd055c
yinonov <yinon@hotmail.com>
2022-09-13 11:08:40 +0300

    grammer

--- libs/components/src/lib/menu/index.ts
+++ libs/components/src/lib/menu/index.ts
@@ -1,6 +1,6 @@
 import '../popup';
 // by convention, menu-item isn't required to be imported
-// in menu as it is not used directly rather by self authoring.
+// in menu as it is not used directly rather by authoring.
 // but, due to the race condition and way menu needs children to
 // connect before setting/checking their props/attributes, it is required
 import '../menu-item';
