84288bdbbda622dd601650c5bb11a327a9e6391e
yinon <yinon@hotmail.com>
2022-09-22 17:20:27 +0300

    refactor(components): menu item migrate to fast (#636)
    * refactor(components): menu migrate to fast
    
    * progress
    
    * works
    
    * progressed
    
    * mixed menu
    
    * Change files
    
    * Update libs/components/src/lib/menu-item/index.ts
    
    * typo
    
    * exports
    
    * info
    
    * progress
    
    * focus issue
    
    * ok
    
    * need it
    
    * grammer
    
    * update
    
    * done
    
    * snaps
    
    * resolve comments
    
    * ui tests
    
    * snapshots
    
    * stylelint
    
    * add coverage
    
    * icon unit test
    
    * increase coverage
    
    * simple
    
    * resolve comments
    
    * text test
    
    * done
    
    * update
    
    * 1 line item
    
    * snaps
    
    * max min
    
    * stylelint
    
    * typo
    
    * @testing-library/dom
    
    * maybe now
    
    * remove

---
+++ libs/components/src/lib/menu-item/index.ts
@@ -0,0 +1,16 @@
+import '../icon';
+
+import type { MenuItemOptions } from '@microsoft/fast-foundation';
+import { designSystem } from '../../shared/design-system';
+import styles from './menu-item.scss';
+
+import { MenuItem } from './menu-item';
+import { MenuItemTemplate as template } from './menu-item.template';
+
+export const vividMenuItem = MenuItem.compose<MenuItemOptions>({
+	baseName: 'menu-item',
+	template: template as any,
+	styles
+});
+
+designSystem.register(vividMenuItem());
