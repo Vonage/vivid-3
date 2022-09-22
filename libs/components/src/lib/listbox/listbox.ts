f0df9732d392ceb95c116a58c6fbcfedadeafb6d
yinonov <yinon@hotmail.com>
2022-09-22 23:37:21 +0300

    ok?

--- libs/components/src/lib/listbox/listbox.ts
+++ libs/components/src/lib/listbox/listbox.ts
@@ -1,6 +1,6 @@
 import { attr } from '@microsoft/fast-element';
-import type { Appearance } from '../enums';
 import { ListboxElement as FoundationListboxElement } from '@microsoft/fast-foundation';
+import type { Appearance } from '../enums';
 
 /**
  * Types of listbox appearance.
@@ -8,7 +8,7 @@ import { ListboxElement as FoundationListboxElement } from '@microsoft/fast-foun
  * @public
  */
 export type LisboxAppearance = Extract<Appearance,
-    Appearance.Outlined | Appearance.Ghost>;
+Appearance.Outlined | Appearance.Ghost>;
 
 /**
  * Base class for listbox
@@ -16,33 +16,33 @@ export type LisboxAppearance = Extract<Appearance,
  * @public
  */
 export class Listbox extends FoundationListboxElement {
-    /**
-     * The appearance the listbox should have.
-     *
-     * @public
-     * @remarks
-     * HTML Attribute: appearance
-     */
-    @attr appearance?: LisboxAppearance;
+	/**
+	 * The appearance the listbox should have.
+	 *
+	 * @public
+	 * @remarks
+	 * HTML Attribute: appearance
+	 */
+	@attr appearance?: LisboxAppearance;
 
-    override slottedOptionsChanged(prev: Element[] | undefined, next: Element[]) {
-        super.slottedOptionsChanged(prev, next);
-        this.#disableSlottedChildren();
-    }
-    
-    override attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
-        super.attributeChangedCallback(name, oldValue, newValue);
-        switch (name) {
-            case 'disabled': {
-                this.#disableSlottedChildren();
-                break;
-            }
-        }
-    }
+	override slottedOptionsChanged(prev: Element[] | undefined, next: Element[]) {
+		super.slottedOptionsChanged(prev, next);
+		this.#disableSlottedChildren();
+	}
 
-    #disableSlottedChildren(): void {
-        this.options.forEach(optionElement => {
-            optionElement.disabled = this.disabled;
-        });
-    }
+	override attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
+		super.attributeChangedCallback(name, oldValue, newValue);
+		switch (name) {
+			case 'disabled': {
+				this.#disableSlottedChildren();
+				break;
+			}
+		}
+	}
+
+	#disableSlottedChildren(): void {
+		this.options.forEach(optionElement => {
+			optionElement.disabled = this.disabled;
+		});
+	}
 }
