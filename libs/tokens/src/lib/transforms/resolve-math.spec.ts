4273a32fa47e0a318ad2c826736620effc7a4c00
yinonov <yinon@hotmail.com>
2022-10-12 16:34:47 +0300

    better tests

--- libs/tokens/src/lib/transforms/resolve-math.spec.ts
+++ libs/tokens/src/lib/transforms/resolve-math.spec.ts
@@ -1,13 +1,22 @@
 import { resolveMath } from './resolve-math';
 
-const { transformer } = resolveMath;
+const { transformer, matcher } = resolveMath;
 
 const token = {
-	value: 4 * 10
+	value: "4 * 3.72"
 };
 
 describe('basic', () => {
 	it('should evaluate math expression', () => {
-		expect(transformer(token)).toEqual('40');
+		expect(transformer(token)).toEqual('15');
+	});
+
+	it('should throw', () => {
+		expect(transformer({ })).toEqual("undefined");
+	});
+
+	it('should match if is token', () => {
+		expect(matcher(undefined)).toBeFalsy();
+		expect(matcher(token)).toBeTruthy();
 	});
 });
