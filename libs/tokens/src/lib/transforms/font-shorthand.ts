6d84b3d5772caf38e9909ef8c36d4c6d639d5f56
yinonov <yinon@hotmail.com>
2022-10-12 12:34:32 +0300

    partially fixed

--- libs/tokens/src/lib/transforms/font-shorthand.ts
+++ libs/tokens/src/lib/transforms/font-shorthand.ts
@@ -1,7 +1,17 @@
 const isObject = (value) => typeof value === 'object' && !Array.isArray(value) && value !== null;
 
-const parseFontProps = ({ fontFamily, fontWeight, lineHeight, fontSize, fontStretch }) =>
-	`${fontWeight} ${fontStretch} ${fontSize}/${lineHeight} ${fontFamily}`;
+// due to figma api typography limitations,
+// we patch font weight to output font weight
+// as a number and stretch keyword.
+// once figma api supports variable fonts, we can refactor this
+const fontWeightMap = new Map([
+	['Wide Medium', '500 condensed'],
+	['Regular', '400 ultra-condensed'],
+	['SemiBold', '600 ultra-condensed']
+]);
+
+const parseFontProps = ({ fontFamily, fontWeight, lineHeight, fontSize }) =>
+	`${fontWeightMap.get(fontWeight)} ${fontSize}/${lineHeight} ${fontFamily}`;
 
 module.exports = {
 	type: `value`,
