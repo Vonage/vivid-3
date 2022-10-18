1161ee02f9768f041b4a7d3408c6e1d0ad375018
yinonov <yinon@hotmail.com>
2022-10-19 01:34:31 +0300

    nicer

--- libs/tokens/src/lib/configurations/size.ts
+++ libs/tokens/src/lib/configurations/size.ts
@@ -1,35 +1,36 @@
 import { prefix, buildPath } from './common/config';
 
-const isBase = token => token.attributes.type === "base";
-const isDerivative = token => !["base", "unit", "unitMultiplier"].includes(token.attributes.type);
+const isBase = token => token.attributes.type === 'base';
 
 export const sizeConfig = {
 	source: [
-		`../../../../node_modules/@vonage/vivid-figma-tokens/data/sizing/base.tokens.json`,
-		`../../../../node_modules/@vonage/vivid-figma-tokens/data/sizing/desktop.tokens.json`
+		'../../../../node_modules/@vonage/vivid-figma-tokens/data/sizing/desktop.tokens.json'
+	],
+	include: [
+		'../../../../node_modules/@vonage/vivid-figma-tokens/data/sizing/base.tokens.json',
 	],
 	platforms: {
 		css: {
-			transforms: ["attribute/cti", "name/cti/kebab", "resolveMath"],
+			transforms: ['attribute/cti', 'name/cti/kebab', 'resolveMath'],
 			prefix,
 			buildPath,
 			files: [{
-				destination: `size/_base.mixin.scss`,
-				format: "suffixPxCssVariables",
+				destination: 'size/_base.mixin.scss',
+				format: 'suffixPxCssVariables',
 				options: {
-					selector: "@mixin variables"
+					selector: '@mixin variables'
 				},
 				filter: isBase,
 			}]
 		},
 		scss: {
-			transforms: ["attribute/cti", "name/cti/kebab", "referenceSizingBase", "resolveMath"],
+			transforms: ['attribute/cti', 'name/cti/kebab', 'referenceSizingBase', 'resolveMath'],
 			prefix,
 			buildPath,
 			files: [{
 				destination: 'size/_variables.scss',
-				format: "sizingScssVariables",
-				filter: isDerivative,
+				format: 'sizingScssVariables',
+				filter: 'sourceOnly',
 			}],
 		}
 	}
