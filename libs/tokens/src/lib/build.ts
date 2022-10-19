a25d5f3520efeeb496d864e3664d8238fe861e46
yinon <yinon@hotmail.com>
2022-10-19 09:21:43 +0300

    feat(tokens): integrate sizing tokens (#735)
    * feat(tokens): add support for sizing tokens
    
    * fixed
    
    * progress
    
    * quite close
    
    * 90%
    
    * almost done
    
    * done
    
    * clean up
    
    * types
    
    * to esm
    
    * simply eval
    
    * refactor tests
    
    * version
    
    * done
    
    * done
    
    * nicer

--- libs/tokens/src/lib/build.ts
+++ libs/tokens/src/lib/build.ts
@@ -1,19 +1,33 @@
+
 import StyleDictionary from 'style-dictionary';
+
 import { sourceOnly } from './filters/source-only';
+
 import { shadowShorthand } from './transforms/shadow-shorthand';
 import { fontShorthand } from './transforms/font-shorthand';
+import { referenceSizingBase } from './transforms/reference-sizing-base';
+import { resolveMath } from './transforms/resolve-math';
+
 import { scssConstants } from './formatters/scss-constants';
+import { sizingScssVariables } from './formatters/sizing-scss-variables';
+import { suffixPxCssVariables } from './formatters/suffix-px-css-variables';
+
 import { scssConstantsConfig } from './configurations/scss-constants';
 import { getThemeConfig } from './configurations/theme';
 import { getTypographyConfig } from './configurations/typography';
+import { sizeConfig } from './configurations/size';
 
 import themes from '@vonage/vivid-figma-tokens/data/$themes.json';
 
 StyleDictionary
 .registerTransform(shadowShorthand)
 .registerTransform(fontShorthand)
+.registerTransform(resolveMath)
 .registerFilter(sourceOnly)
 .registerFormat(scssConstants)
+.registerFormat(sizingScssVariables)
+.registerFormat(suffixPxCssVariables)
+.registerTransform(referenceSizingBase);
 
 
 
@@ -32,3 +46,5 @@ themes.forEach(({ name }) =>
 		.extend(getTypographyConfig(viewport)).buildPlatform('web')
 );
 
+StyleDictionary
+	.extend(sizeConfig).buildAllPlatforms();
