0d05a0977c274ba542712449dba2ee6a30345a2c
Olaf <olaf-k@users.noreply.github.com>
2022-10-24 17:12:04 +0200

    review fixes

--- libs/components/src/lib/radio-group/radio-group.spec.ts
+++ libs/components/src/lib/radio-group/radio-group.spec.ts
@@ -31,12 +31,20 @@ describe('vwc-radio-group', () => {
 	});
 
 	describe('basic', () => {
-		it('should be initialized as a vwc-radio-group', async () => {
+		it('should be initialized as a vwc-radio-group with proper default values', async () => {
 			expect(element).toBeInstanceOf(RadioGroup);
 			expect(element.readOnly).toBeFalsy();
 			expect(element.disabled).toBeFalsy();
 			expect(element.label).toBeUndefined();
 			expect(element.orientation).toEqual('horizontal');
+			expect(element.getAttribute('value')).toBeNull();
+		});
+
+		it('should set the radio buttons with proper default values', async () => {
+			expect(radios.every(r => r.checked)).toBeFalsy();
+			expect(radios[0].getAttribute('tabindex')).toBe('0');
+			expect(radios[1].getAttribute('tabindex')).toBe('-1');
+			expect(radios[2].getAttribute('tabindex')).toBe('-1');
 		});
 	});
 
@@ -45,7 +53,6 @@ describe('vwc-radio-group', () => {
 			element.label = 'testlabel';
 			await elementUpdated(element);
 			const label = element.shadowRoot?.querySelector('label');
-			expect(label).not.toBeNull();
 			expect(label?.textContent).toBe(element.label);
 		});
 	});
@@ -53,9 +60,11 @@ describe('vwc-radio-group', () => {
 	describe('orientation', () => {
 		it('should update the positioning region when changing orientation', async () => {
 			const positioningRegion = element.shadowRoot?.querySelector('.positioning-region');
-			expect(positioningRegion?.classList).toContain('horizontal');
+			const positioningRegionDefaultClassList = Array.from(positioningRegion?.classList as DOMTokenList);
 			element.setAttribute('orientation', 'vertical');
 			await elementUpdated(element);
+			
+			expect(positioningRegionDefaultClassList).toContain('horizontal');
 			expect(positioningRegion?.classList).toContain('vertical');
 		});
 	});
@@ -72,6 +81,7 @@ describe('vwc-radio-group', () => {
 		it('should select the radio button with the same value', async () => {
 			element.setAttribute('value', '1');
 			await elementUpdated(element);
+			
 			expect(radios[1].checked && !radios[0].checked && !radios[2].checked).toBeTruthy();
 			expect(radios[0].getAttribute('tabindex')).toBe('-1');
 			expect(radios[1].getAttribute('tabindex')).toBe('0');
@@ -79,8 +89,6 @@ describe('vwc-radio-group', () => {
 		});
 
 		it('should receive the value of the radio that was clicked', async () => {
-			expect(element.getAttribute('value')).toBeNull();
-			expect(radios.every(r => r.checked)).toBeFalsy();
 			getBaseElement(radios[1]).dispatchEvent(new MouseEvent('click'));
 			await elementUpdated(element);
 			expect(element.getAttribute('value')).toEqual('1');
@@ -89,9 +97,9 @@ describe('vwc-radio-group', () => {
 	
 	describe('click', () => {
 		it('should update when a radio is clicked', async () => {
-			expect(radios[2].getAttribute('tabindex')).toBe('-1');
 			getBaseElement(radios[2]).click();
 			await elementUpdated(element);
+
 			expect(radios[2].getAttribute('tabindex')).toBe('0');
 			expect(element.value).toBe('2');
 		});
@@ -99,12 +107,17 @@ describe('vwc-radio-group', () => {
 
 	describe('keyboard', () => {
 		async function keyboardCheck(radioToCheck: number, key: string) {
-			expect(radios[radioToCheck].checked).toBeFalsy();
-			expect(element.value).toBeUndefined();
+			const radio = radios[radioToCheck];
+			const radioCheckedBefore = radio.checked;
+			const radioGroupValueBefore = element.value;
+			
 			radios[0].focus();
 			radios[0].dispatchEvent(new KeyboardEvent('keydown', { key: key, bubbles: true }));
 			await elementUpdated(element);
-			expect(radios[radioToCheck].checked).toBeTruthy();
+			
+			expect(radioCheckedBefore).toBeFalsy();
+			expect(radioGroupValueBefore).toBeUndefined();
+			expect(radio.checked).toBeTruthy();
 			expect(element.value).toBe(radioToCheck.toString());
 		}
 
@@ -116,11 +129,11 @@ describe('vwc-radio-group', () => {
 			await keyboardCheck(0, 'Enter');
 		});
 
-		it('should loop', async () => {
+		it('should loop over radio buttons', async () => {
 			getBaseElement(radios[2]).click();
-			expect(element.value).toBe('2');
 			radios[2].dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight', bubbles: true }));
 			await elementUpdated(element);
+
 			expect(radios[0].checked).toBeTruthy();
 			expect(element.value).toBe('0');
 		});
@@ -129,8 +142,10 @@ describe('vwc-radio-group', () => {
 	describe('focus', () => {
 		it('should move focus outside the group when a radio is blurred', async () => {
 			radios[0].focus();
-			expect(document.activeElement).toBe(radios[0]);
+			const activeElementBeforeBlur = document.activeElement;
 			radios[0].blur();
+			
+			expect(activeElementBeforeBlur).toBe(radios[0]);
 			expect(document.activeElement).toBe(document.body);
 		});
 	});
