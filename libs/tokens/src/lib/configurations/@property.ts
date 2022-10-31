0160cd56b21f50c2d26da8f65847f05345f7aa5b
yinon <yinon@hotmail.com>
2022-10-31 12:03:04 +0200

    refactor(tokens): locking down baseline size (#768)

---
+++ libs/tokens/src/lib/configurations/@property.ts
@@ -0,0 +1,28 @@
+
+const SD = require('style-dictionary');
+
+import { prefix, buildPath } from '../common';
+import cssAtRuleProperty from '../formatters/@property';
+
+SD.registerFormat(cssAtRuleProperty);
+
+export default {
+	source: [
+		'../../../../node_modules/@vonage/vivid-figma-tokens/data/globals/**/*.tokens.json'
+	],
+	platforms: {
+		scss: {
+			transforms: ["name/cti/kebab"],
+			prefix,
+			buildPath,
+			files: [{
+				destination: '@properties.scss',
+				format: "scss/@property",
+				filter: token => token['@property'],
+				options: {
+					selector: '@mixin properties'
+				}
+			}]
+		}
+	}
+};
