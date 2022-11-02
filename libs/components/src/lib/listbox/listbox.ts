e6f9e13c96a2b8f8ccb7b992ce9e98fa57220061
Rina <rina.oksman@vonage.com>
2022-11-02 16:21:47 +0200

    lint

--- libs/components/src/lib/listbox/listbox.ts
+++ libs/components/src/lib/listbox/listbox.ts
@@ -7,8 +7,7 @@ import type { Appearance } from '../enums';
  *
  * @public
  */
-export type LisboxAppearance = Extract<Appearance,
-	Appearance.Outlined | Appearance.Ghost>;
+export type LisboxAppearance = Extract<Appearance, Appearance.Outlined | Appearance.Ghost>;
 
 /**
  * Base class for listbox
