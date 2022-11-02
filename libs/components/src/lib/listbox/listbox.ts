4f0a2dea9b5a139f12de0b5eeb05205f944a08fc
Rina <rina.oksman@vonage.com>
2022-11-02 16:18:27 +0200

    if instead of switch

--- libs/components/src/lib/listbox/listbox.ts
+++ libs/components/src/lib/listbox/listbox.ts
@@ -8,7 +8,7 @@ import type { Appearance } from '../enums';
  * @public
  */
 export type LisboxAppearance = Extract<Appearance,
-Appearance.Outlined | Appearance.Ghost>;
+	Appearance.Outlined | Appearance.Ghost>;
 
 /**
  * Base class for listbox
@@ -32,11 +32,8 @@ export class Listbox extends FoundationListboxElement {
 
 	override attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
 		super.attributeChangedCallback(name, oldValue, newValue);
-		switch (name) {
-			case 'disabled': {
-				this.#disableSlottedChildren();
-				break;
-			}
+		if (name === 'disabled') {
+			this.#disableSlottedChildren();
 		}
 	}
 
