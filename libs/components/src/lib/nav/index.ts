c6dba69f79d0ba9763240d1a5a2657eee4a3763d
yinonov <yinon@hotmail.com>
2022-11-10 18:30:36 +0200

    [Development check up]: things that need attention Fixes #805

--- libs/components/src/lib/nav/index.ts
+++ libs/components/src/lib/nav/index.ts
@@ -1,5 +1,6 @@
 import type { FoundationElementDefinition } from '@microsoft/fast-foundation';
 import { designSystem } from '../../shared/design-system';
+import styles from './nav.scss';
 
 import { Nav } from './nav';
 import { NavTemplate as template } from './nav.template';
@@ -8,6 +9,7 @@ export const vividNav =
 	Nav.compose<FoundationElementDefinition>({
 		baseName: 'nav',
 		template: template as any,
+		styles
 	});
 
 designSystem.register(vividNav());
