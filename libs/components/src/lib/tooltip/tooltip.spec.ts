fc0d71dd7222eb350cf125a40d739bc03eafc4c7
Rina <rina.oksman@vonage.com>
2022-11-15 17:40:09 +0200

    update tests

--- libs/components/src/lib/tooltip/tooltip.spec.ts
+++ libs/components/src/lib/tooltip/tooltip.spec.ts
@@ -2,6 +2,7 @@ import { ADD_TEMPLATE_TO_FIXTURE, elementUpdated, fixture } from '@vivid-nx/shar
 import type { Button } from '../button/button';
 import { Tooltip } from './tooltip';
 import '.';
+import { fireEvent } from '@testing-library/dom';
 
 const COMPONENT_TAG = 'vwc-tooltip';
 
@@ -37,11 +38,11 @@ describe('vwc-tooltip', () => {
 
 	describe('show', () => {
 		it('should set "open" to true', async () => {
-			await setAnchor();
+			const anchor = await setAnchor();
 			element.anchor = 'anchor';
 			await elementUpdated(element);
 
-			element.open = true;
+			fireEvent(anchor, new MouseEvent('mouseover'));
 			await elementUpdated(element);
 
 			expect(element.open)
@@ -51,7 +52,12 @@ describe('vwc-tooltip', () => {
 
 	describe('hide', () => {
 		it('should set "open" to false', async () => {
-			element.open = false;
+			const anchor = await setAnchor();
+			element.anchor = 'anchor';
+			element.open = true;
+			await elementUpdated(element);
+
+			fireEvent(anchor, new MouseEvent('mouseout'));
 			await elementUpdated(element);
 
 			expect(element.open)
