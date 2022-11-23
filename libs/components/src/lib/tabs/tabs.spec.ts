7aa31446d33364df0a32b10a0844320c6e37d1d1
Rina <rina.oksman@vonage.com>
2022-11-23 15:55:49 +0200

    add test

--- libs/components/src/lib/tabs/tabs.spec.ts
+++ libs/components/src/lib/tabs/tabs.spec.ts
@@ -1,4 +1,4 @@
-import { fixture } from '@vivid-nx/shared';
+import { elementUpdated, fixture, getBaseElement } from '@vivid-nx/shared';
 import { Tabs } from './tabs';
 import '.';
 
@@ -15,6 +15,17 @@ describe('vwc-tabs', () => {
 		it('should be initialized as a vwc-tabs', async () => {
 			expect(element).toBeInstanceOf(Tabs);
 			expect(element.orientation).toEqual("horizontal");
+			expect(element.activeid).toBeUndefined();
+		});
+	});
+
+	describe('orientation', () => {
+		it('should set orientation property', async () => {
+			const orientation = 'vertical';
+			expect(getBaseElement(element).classList.contains(`orientation-${orientation}`)).toBeFalsy();
+			element.orientation = orientation as any;
+			await elementUpdated(element);
+			expect(getBaseElement(element).classList.contains(`orientation-${orientation}`)).toBeTruthy();
 		});
 	});
 });
