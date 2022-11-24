ea5dd60a08acd394c1d310f2a4b82fa24c8427c7
yinonov <yinon@hotmail.com>
2022-11-24 16:43:53 +0200

    feat(components): enable prefix override

--- libs/components/src/shared/design-system/index.ts
+++ libs/components/src/shared/design-system/index.ts
@@ -1,12 +1,16 @@
 import { DesignSystem } from '@microsoft/fast-foundation';
 
+let prefix = 'vwc';
+
+export const overridePrefix = (customPrefix: string) => prefix = customPrefix;
+
 // common components dependency
 /**
  * @param {HTMLElement} element - The element to get or create a design system for.
  * @returns {DesignSystem} - Represents a configurable design system. An API gateway to design system features.
  */
 export function provideVividDesignSystem(element?: HTMLElement): DesignSystem {
-	return DesignSystem.getOrCreate(element).withPrefix('vwc');
+	return DesignSystem.getOrCreate(element).withPrefix(prefix);
 }
 
 export const designSystem = provideVividDesignSystem();
