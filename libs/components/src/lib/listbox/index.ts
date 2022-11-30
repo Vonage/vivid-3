bf53a2b015d2b1f4f202a3f699228d81a153212e
yinonov <yinon@hotmail.com>
2022-11-30 14:41:31 +0200

    mixins issue

--- libs/components/src/lib/listbox/index.ts
+++ libs/components/src/lib/listbox/index.ts
@@ -8,14 +8,14 @@ import styles from './listbox.scss';
 
 const prefix = getPrefix(import.meta.url);
 
-export const vividListbox = Listbox.compose<FoundationElementDefinition>({
+const vividListbox = Listbox.compose<FoundationElementDefinition>({
 	baseName: 'listbox',
 	template: template as any,
 	styles
 });
 
 (async () => {
-	// by convention, listbox-option isn't required to be imported
+	// by convention, option isn't required to be imported
 	// in listbox as it is not used directly in its template rather by user's authoring.
 	// but, due to the race condition and way listbox needs children to
 	// connect before setting/checking their props/attributes, it is required
--- libs/components/src/lib/option/index.ts
+++ libs/components/src/lib/option/index.ts
@@ -1,9 +1,9 @@
 import type { FoundationElementDefinition } from '@microsoft/fast-foundation';
-import { designSystem, getPrefix } from '../../shared/design-system';
-import { loadComponentsModules } from '../../shared/utils';
-import { ListboxOption } from './listbox-option';
-import { ListboxOptionTemplate as template } from './listbox-option.template';
-import styles from './listbox-option.scss';
+import { designSystem, getPrefix } from '../shared/design-system';
+import { loadComponentsModules } from '../shared/utils';
+import { ListboxOption } from './option';
+import { ListboxOptionTemplate as template } from './option.template';
+import styles from './option.scss';
 
 const prefix = getPrefix(import.meta.url);
 
