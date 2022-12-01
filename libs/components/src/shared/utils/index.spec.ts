70f2012685bb5ccd36b1b650358a28a37b483019
Yonatan Kra <yonatan.kra@vonage.com>
2022-12-01 15:20:19 +0200

    Passing test

--- libs/components/src/shared/utils/index.spec.ts
+++ libs/components/src/shared/utils/index.spec.ts
@@ -1,14 +1,30 @@
-import { loadComponentsModules } from '.';
+import '.';
 
 describe('loadComponentsModules', () => {
+	let whenDefinedMock: any;
+	beforeEach(() => {
+		whenDefinedMock = jest.spyOn(customElements, 'whenDefined').mockReturnValue(Promise.resolve({whenDefinedCalled: true} as unknown as CustomElementConstructor));
+	});
+
+	afterEach(() => {
+		whenDefinedMock.mockRestore();
+	});
+
 	it('should load components modules', async () => {
 		const components = ['button'];
 		const prefix = 'vivid';
+		const {loadComponentsModules} = jest.requireActual('.');
 
-		await loadComponentsModules(components, prefix);
+		let importCalledWithPrefix = false;
+		jest.mock('../button/index.js?prefix=vivid', async () => {
+			importCalledWithPrefix = true;
+			return import('../../lib/button/index');
+		  }, {virtual: true});
 
-		components.forEach((component) => {
-			expect(customElements.get(`${prefix}-${component}`)).toBeDefined();
-		});
+		const [{whenDefinedCalled}] = await loadComponentsModules(components, prefix);
+
+		expect(importCalledWithPrefix).toBeTruthy();
+		expect(customElements.whenDefined).toHaveBeenCalledWith('vivid-button');
+		expect(whenDefinedCalled).toEqual(true);
 	});
 });
