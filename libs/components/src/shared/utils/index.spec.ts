5c92210b437f0eb021e9c170713d26727b6b90f9
Yonatan Kra <yonatan.kra@vonage.com>
2022-12-01 15:53:14 +0200

    Refactor

--- libs/components/src/shared/utils/index.spec.ts
+++ libs/components/src/shared/utils/index.spec.ts
@@ -1,38 +1,41 @@
 import '.';
 
 describe('loadComponentsModules', () => {
+	function mockCallWithPrefix(prefix: string) {
+		jest.mock(`../${components[0]}/index.js?prefix=${prefix}`, async () => {
+			return import(`../../lib/${components[0]}/index`);
+		}, { virtual: true });
+	}
+
+	function getNonMockedLoadComponentsModules(): { loadComponentsModules: any; } {
+		return jest.requireActual('.');
+	}
+
+	const components = ['button'];
+	const prefix = 'vivid';
 	let whenDefinedMock: any;
 	const whenDefinedResolveValue = {};
+	const { loadComponentsModules } = getNonMockedLoadComponentsModules();
 
 	beforeEach(() => {
 		whenDefinedMock = jest.spyOn(customElements, 'whenDefined')
 			.mockReturnValue(Promise.resolve(whenDefinedResolveValue as CustomElementConstructor));
+		mockCallWithPrefix(prefix);
 	});
 
 	afterEach(() => {
 		whenDefinedMock.mockRestore();
 	});
 
-	it('should load components modules', async () => {
-		const components = ['button'];
-		const prefix = 'vivid';
-		const { loadComponentsModules } = getNonMockedLoadComponentsModules();
-
-		mockCallWithPrefix();
-
-		const [whenDefinedRActualesolveValue] = await loadComponentsModules(components, prefix);
+	it('should import "../${component}/index.js?prefic=${prefix}', async () => {
+		await loadComponentsModules(components, prefix);
 
 		expect(customElements.whenDefined).toHaveBeenCalledWith('vivid-button');
+	});
+
+	it('should wait for whenDefined of all components', async () => {
+		const [whenDefinedRActualesolveValue] = await loadComponentsModules(components, prefix);
+
 		expect(whenDefinedResolveValue).toEqual(whenDefinedRActualesolveValue);
 	});
 });
-
-function mockCallWithPrefix() {
-	jest.mock('../button/index.js?prefix=vivid', async () => {
-		return import('../../lib/button/index');
-	}, { virtual: true });
-}
-
-function getNonMockedLoadComponentsModules(): { loadComponentsModules: any; } {
-	return jest.requireActual('.');
-}
