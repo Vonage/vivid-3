2841ea8243e638b0aed3becc8741694b10ec58b7
yinonov <yinon@hotmail.com>
2022-12-07 18:46:41 +0200

    format

--- libs/components/src/shared/design-system/index.ts
+++ libs/components/src/shared/design-system/index.ts
@@ -2,7 +2,7 @@ import { DesignSystem } from '@microsoft/fast-foundation';
 
 const defaultPrefix = 'vwc';
 
-// !important: remove export. only use register() to register components
+// !important: remove export. only use registerFactorial()
 export const getPrefix = (url: string) => new URL(url).searchParams.get('prefix') || 'vwc';
 
 // common components dependency
@@ -11,6 +11,6 @@ export const getPrefix = (url: string) => new URL(url).searchParams.get('prefix'
  * @param prefix
  * @returns {DesignSystem} - Represents a configurable design system. An API gateway to design system features.
  */
-export const designSystem = DesignSystem.getOrCreate(); // !important: remove export. only use register() to register components
+export const designSystem = DesignSystem.getOrCreate(); // !important: remove export. only use registerFactorial()
 
 export const registerFactorial = (...args: any[]) => (prefix = defaultPrefix) => designSystem.withPrefix(prefix).register(...args);
