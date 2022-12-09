0bff4c424a3cf6258c4f9337e33c018e83a275b3
yinonov <yinon@hotmail.com>
2022-12-09 10:53:22 +0200

    forgot deps

--- libs/components/src/lib/switch/definition.ts
+++ libs/components/src/lib/switch/definition.ts
@@ -1,5 +1,7 @@
 import type { FoundationElementDefinition } from '@microsoft/fast-foundation';
 import { registerFactorial } from '../../shared/design-system';
+import { iconElements } from '../icon/definition';
+import { focusElements } from '../focus/definition';
 import styles from './switch.scss';
 import { Switch } from './switch';
 import { SwitchTemplate as template } from './switch.template';
@@ -19,7 +21,7 @@ export const switchCE = Switch.compose<FoundationElementDefinition>({
 	}
 })();
 
-export const switchElements = [switchCE];
+export const switchElements = [switchCE, ...iconElements, ...focusElements];
 
 /**
  * Registers the switch elements with the design system.
