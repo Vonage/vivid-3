0143328e35891e1d552a381bf54d19ccd15a843d
Rina <rina.oksman@vonage.com>
2022-12-13 17:41:36 +0200

    add unit test

--- libs/components/src/lib/combobox/combobox.spec.ts
+++ libs/components/src/lib/combobox/combobox.spec.ts
@@ -16,6 +16,12 @@ describe('vwc-combobox', () => {
 	describe('basic', () => {
 		it('should be initialized as a vwc-combobox', async () => {
 			expect(element).toBeInstanceOf(Combobox);
+			expect(element.open).toEqual(false);
+			expect(element.placement).toBeUndefined();
+			expect(element.disabled).toEqual(false);
+			expect(element.value).toBeUndefined();
+			expect(element.placeholder).toBeUndefined();
+			expect(element.autocomplete).toBeUndefined();
 		});
 	});
 });
