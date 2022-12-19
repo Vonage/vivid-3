bd7efa4b31d586e9a051ee44843d6339b564510a
Rina <rina.oksman@vonage.com>
2022-12-19 15:12:30 +0200

    fix

--- libs/components/src/lib/popup/index.ts
+++ libs/components/src/lib/popup/index.ts
@@ -1,13 +1,3 @@
-import '../elevation';
-import '../button';
-import '@oddbird/popover-polyfill';
-
-import type { FoundationElementDefinition } from '@microsoft/fast-foundation';
-import { designSystem, getPrefix } from '../../shared/design-system';
-import { loadComponentsModules } from '../../shared/utils';
-import { Popup } from './popup';
-import styles from './popup.scss';
-import { popupTemplate as template } from './popup.template';
-
+import { registerPopup } from './definition';
 
 registerPopup();
