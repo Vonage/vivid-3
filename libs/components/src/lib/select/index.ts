4877d85b570e1153f45b50c5a0d68412fa1c4250
Rina <rina.oksman@vonage.com>
2022-12-19 12:14:44 +0200

    add definiton file

--- libs/components/src/lib/select/index.ts
+++ libs/components/src/lib/select/index.ts
@@ -1,23 +1,4 @@
-import '../popup';
-import '../icon';
-// import '../listbox'; //! not using the component
-// by convention, listbox-option isn't required to be imported
-// in combobox as it is not used directly in template rather by authoring.
-// but, due to the race condition and way combobox needs children to
-// connect before setting/checking their props/attributes, it is required
-import '../listbox-option';
+import { registerSelect } from './definition';
 
-import type { FoundationElementDefinition } from '@microsoft/fast-foundation';
-import { designSystem } from '../../shared/design-system';
-import styles from './select.scss';
 
-import { Select } from './select';
-import { SelectTemplate as template } from './select.template';
-
-export const vividSelect = Select.compose<FoundationElementDefinition>({
-	baseName: 'select',
-	template: template as any,
-	styles,
-});
-
-designSystem.register(vividSelect());
+registerSelect();
