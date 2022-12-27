fd60970fb62a66dddb3137075d53067f8eb5eb6b
yinonov <yinon.oved@vonage.com>
2022-12-27 15:47:16 +0200

    fixed janky transition

--- libs/components/src/lib/tabs/tabs.ts
+++ libs/components/src/lib/tabs/tabs.ts
@@ -1,10 +1,47 @@
 import { Tabs as FoundationTabs } from '@microsoft/fast-foundation';
 
+const TABS_ACTIVE_INDICATOR_INLINE_SIZE = "--_tabs-active-indicator-inline-size";
+
 /**
  * Base class for tabs
  *
  * @public
  */
 export class Tabs extends FoundationTabs {
+	override connectedCallback() {
+		super.connectedCallback();
+		this.activeIndicatorRef.addEventListener("transitionend", this.handleActiveIndicatorTransitionEnd.bind(this));
+	}
 
+	override disconnectedCallback() {
+		super.disconnectedCallback();
+		this.activeIndicatorRef.removeEventListener("transitionend", this.handleActiveIndicatorTransitionEnd);
+	}
+
+	override orientationChanged(): void {
+		super.orientationChanged();
+		this.patchIndicatorStyleTransition();
+	}
+
+	override activeidChanged(oldValue: string, newValue: string): void {
+		super.activeidChanged(oldValue, newValue);
+		this.patchIndicatorStyleTransition();
+	}
+
+	override tabsChanged(): void {
+		super.tabsChanged();
+		this.patchIndicatorStyleTransition();
+	}
+
+	override tabpanelsChanged(): void {
+		super.tabpanelsChanged();
+		this.patchIndicatorStyleTransition();
+	}
+
+	private patchIndicatorStyleTransition() {
+		const width = this.activetab?.getClientRects()[0]?.width;
+		if (!width) return;
+
+    this.activeIndicatorRef.style.setProperty(TABS_ACTIVE_INDICATOR_INLINE_SIZE, `${width}px`);
+  }
 }
