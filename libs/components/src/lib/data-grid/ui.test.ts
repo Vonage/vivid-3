0303ee696e2b2591a3b573207cf6bc1ef8a0a251
Yonatan Kra <yonatan.kra@vonage.com>
2023-01-10 15:32:05 +0200

    UI tests

--- libs/components/src/lib/data-grid/ui.test.ts
+++ libs/components/src/lib/data-grid/ui.test.ts
@@ -1,22 +1,17 @@
-import * as path from 'path';
 import { expect, test } from '@playwright/test';
 import type { Page } from '@playwright/test';
 import {
-	extractHTMLBlocksFromReadme,
 	loadComponents,
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
 
 const components = ['data-grid'];
 
-test('should show the component', async ({ page }: { page: Page }) => {
-	const template = extractHTMLBlocksFromReadme(
-		path.join(new URL('.', import.meta.url).pathname, 'README.md')
-	).reduce(
-		(htmlString: string, block: string) =>
-			`${htmlString} <div style="margin: 5px;">${block}</div>`,
-		''
-	);
+test.only('should show the component', async ({ page }: { page: Page }) => {
+
+	const template = `<div style="margin: 5px;">
+			<vwc-data-grid></vwc-data-grid>
+	</div>`;
 
 	await loadComponents({
 		page,
@@ -30,6 +25,18 @@ test('should show the component', async ({ page }: { page: Page }) => {
 	const testWrapper = await page.$('#wrapper');
 
 	await page.waitForLoadState('networkidle');
+
+	await page.addScriptTag({content: `
+	const grid = document.querySelector('vwc-data-grid');
+	grid.columnDefinitions = [
+		{columnDataKey: 'data1', title: 'Data 1'},
+		{columnDataKey: 'data2', title: 'Data 2'},
+	];
+	grid.rowsData = [
+		{data1: 'data11', data2: 'data12'},
+		{data1: 'data21', data2: 'data22'},
+	];
+	`});
 
 	expect(await testWrapper?.screenshot()).toMatchSnapshot(
 		'./snapshots/data-grid.png'
