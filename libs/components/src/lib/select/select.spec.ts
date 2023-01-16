0d3da9908b3443ee627c108e475f2e9bed35743f
Yonatan Kra <yonatan.kra@vonage.com>
2023-01-16 14:03:02 +0200

    Test events

--- libs/components/src/lib/select/select.spec.ts
+++ libs/components/src/lib/select/select.spec.ts
@@ -5,6 +5,8 @@ import '.';
 const COMPONENT_TAG = 'vwc-select';
 
 describe('vwc-select', () => {
+	let originalScrollIntoView: any;
+
 	let element: Select;
 
 	global.ResizeObserver = jest.fn()
@@ -13,6 +15,15 @@ describe('vwc-select', () => {
 			unobserve: jest.fn(),
 			disconnect: jest.fn()
 		}));
+
+	beforeAll(() => {
+		originalScrollIntoView = HTMLElement.prototype.scrollIntoView;
+		HTMLElement.prototype.scrollIntoView = jest.fn();
+	});
+	
+	afterAll(() => {
+		HTMLElement.prototype.scrollIntoView = originalScrollIntoView;
+	});
 
 	beforeEach(async () => {
 		element = (await fixture(
@@ -29,7 +40,30 @@ describe('vwc-select', () => {
 			expect(element.appearance).toEqual(undefined);
 			expect(element.label).toEqual(undefined);
 			expect(element.multiple).toBeUndefined();
+			expect(element.selectedIndex).toEqual(-1);
 
+		});
+	});
+
+	describe('selectedIndex', () => {
+		beforeEach(async () => {
+			element.innerHTML = `
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				`;
+			await elementUpdated(element);
+		});
+		
+		it('should set selectedIndex to 0 when first option is selected', async () => {
+			await elementUpdated(element);
+			expect(element.selectedIndex).toEqual(0);
+		});
+
+		it('should change selection when changed', async () => {
+			element.selectedIndex = 2;
+			await elementUpdated(element);
+			expect(element.selectedOptions).toEqual([element.querySelector('option:nth-child(3)')]);
 		});
 	});
 
@@ -127,7 +161,7 @@ describe('vwc-select', () => {
 		});
 	});
 
-
+	// TODO::check validation tests
 	describe('validation', function () {
 		function setValidityToError(errorMessage = 'error') {
 			element.setValidity({badInput: true}, errorMessage);
@@ -154,4 +188,120 @@ describe('vwc-select', () => {
 			expect(element.errorValidationMessage).toEqual(errorMessage);
 		});
 	});
+
+	describe('focusout', function () {
+		it('should leave popup closed if closed', async () => {
+			element.open = false;
+			element.dispatchEvent(new Event('focusout'));
+			expect(element.open).toBeFalsy();
+		});
+
+		it('should close popup if open', async () => {
+			element.open = true;
+			element.dispatchEvent(new Event('focusout'));
+			expect(element.open).toBeFalsy();
+		});
+
+		it('should emit input event when value changes', async () => {
+			element.innerHTML = `
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+			`;
+			const spy = jest.fn();
+			element.addEventListener('input', spy);
+			element.open = true;
+			await elementUpdated(element);
+			element.selectedIndex = 2;
+			element.dispatchEvent(new Event('focusout'));
+			expect(spy).toHaveBeenCalled();
+		});
+
+		it('should leave popup open if relatedTarget is same as element', async () => {
+			element.open = true;
+			element.dispatchEvent(new FocusEvent('focusout', {relatedTarget: element}));
+			expect(element.open).toBeTruthy();
+		});
+	});
+
+	describe('keydown', function () {
+
+		it('should toggle selection if spacebar pressed in single selection mode', async () => {
+			element.open = true;
+			
+			element.dispatchEvent(new KeyboardEvent('keydown', {key: ' '}));
+			const elementStatusAfterSpacebar = element.open;
+			
+			element.dispatchEvent(new KeyboardEvent('keydown', {key: ' '}));
+
+			expect(elementStatusAfterSpacebar).toBeFalsy();
+			expect(element.open).toBeTruthy();
+		});
+
+		it('should toggle selection if spacebar pressed in single selection mode', async () => {
+			element.open = true;
+			
+			element.dispatchEvent(new KeyboardEvent('keydown', {key: 'Enter'}));
+			const elementStatusAfterSpacebar = element.open;
+			
+			element.dispatchEvent(new KeyboardEvent('keydown', {key: 'Enter'}));
+
+			expect(elementStatusAfterSpacebar).toBeFalsy();
+			expect(element.open).toBeTruthy();
+		});
+
+		it('should close selection if escape key pressed', async () => {			
+			element.open = true;
+			await elementUpdated(element);
+			element.dispatchEvent(new KeyboardEvent('keydown', {key: 'Escape'}));
+			expect(element.open).toBeFalsy();	
+		});
+
+		it('should close selection if tab key pressed', async () => {			
+			element.open = true;
+			await elementUpdated(element);
+			element.dispatchEvent(new KeyboardEvent('keydown', {key: 'Tab'}));
+			expect(element.open).toBeFalsy();	
+		});
+
+		it('should emit input and change events if value changed', async () => {
+			element.innerHTML = `
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+			`;
+			const inputSpy = jest.fn();
+			const changeSpy = jest.fn();
+			element.addEventListener('input', inputSpy);
+			element.addEventListener('change', changeSpy);
+			element.open = true;
+			await elementUpdated(element);
+			
+			element.selectedIndex = 2;
+			element.dispatchEvent(new KeyboardEvent('keydown', {key: 'Enter'}));
+			
+			expect(inputSpy).toHaveBeenCalled();
+			expect(changeSpy).toHaveBeenCalled();
+		});
+	});
+
+	describe('click', function () {
+		it('should prevent focusin from firing before click event', async () => {
+			element.innerHTML = `
+				<option value="1" id="id1">1</option>
+				<option value="2" id="id2>2</option>
+				<option value="3">3</option>
+			`;
+			await elementUpdated(element);
+			(MouseEvent as any).prototype.offsetX = 0;
+
+			element.dispatchEvent(new MouseEvent('mousedown'));
+			const shouldSkipFocusAfterOneMouseDown = (element as any).shouldSkipFocus;
+			element.dispatchEvent(new Event('focusin'));
+			const shouldSkipFocusAfterFocusIn = (element as any).shouldSkipFocus;
+			
+			expect(shouldSkipFocusAfterOneMouseDown).toBeTruthy();	
+			expect(shouldSkipFocusAfterFocusIn).toBeFalsy();	
+		});
+	});	
 });
