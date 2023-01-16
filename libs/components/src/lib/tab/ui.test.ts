a36daaf1e94e02272af9a4301c1c64600247aadc
Rina <rina.oksman@vonage.com>
2023-01-16 13:58:38 +0200

    tab-panel

--- libs/components/src/lib/tab/ui.test.ts
+++ libs/components/src/lib/tab/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['tab', 'tabs'];
+const components = ['tab', 'tabs', 'tab-panel'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
