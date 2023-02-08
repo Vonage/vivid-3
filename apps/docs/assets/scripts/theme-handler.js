bb28313a68baeb72bc18bb489d1d575316261877
yinonov <yinon.oved@vonage.com>
2023-02-08 23:16:11 +0200

    refactor(docs): refactor theme selection to use menu

--- apps/docs/assets/scripts/theme-handler.js
+++ apps/docs/assets/scripts/theme-handler.js
@@ -1,43 +1,43 @@
 function getTheme() {
-    return localStorage.getItem('theme') || 'auto';
+	return localStorage.getItem('theme') || 'auto';
 }
 
 function isDark() {
-    if (theme === 'auto') {
-        return window.matchMedia('(prefers-color-scheme: dark)').matches;
-    }
-    return theme === 'dark';
+	if (theme === 'auto') {
+		return window.matchMedia('(prefers-color-scheme: dark)').matches;
+	}
+	return theme === 'dark';
 }
 
 function setTheme(newTheme) {
-    theme = newTheme;
+	theme = newTheme;
 
-    localStorage.setItem('theme', theme);
-    button.icon = isDark() ? 'dark-mode-solid' : 'light-mode-line';
+	localStorage.setItem('theme', theme);
+	menuButton.icon = isDark() ? 'dark-mode-solid' : 'light-mode-line';
 
-    const themeStyle = isDark() ?
-        '<link rel="stylesheet" href="/assets/styles/tokens/theme-dark.css" media="all">' : '<link rel="stylesheet" href="/assets/styles/tokens/theme-light.css" media="all">';
-    document.head?.insertAdjacentHTML("beforeend", themeStyle);
+	const themeStyle = isDark() ?
+		'<link rel="stylesheet" href="/assets/styles/tokens/theme-dark.css" media="all">' : '<link rel="stylesheet" href="/assets/styles/tokens/theme-light.css" media="all">';
+	document.head?.insertAdjacentHTML("beforeend", themeStyle);
 
-    popup.open = false;
+	popup.open = false;
 }
 
 function toggleMenu() {
-    popup.open = !popup.open;
-    if(popup.open){
-        document.getElementById(`option-${theme}`).selected = true;
-    }
+	popup.open = !popup.open;
+	if (popup.open) {
+		document.getElementById(`option-${theme}`).selected = true;
+	}
 }
 
 let theme = getTheme();
 
-const button = document.querySelector('vwc-button#dark-mode-toggle');
-button.addEventListener('click', toggleMenu);
+const menuButton = document.querySelector('#dark-mode-menu-button');
+menuButton.addEventListener('click', toggleMenu);
 
-const listbox = document.querySelector('vwc-listbox#dark-mode-listbox');
-listbox.addEventListener('click', event => setTheme(event.target.value));
+const menu = document.querySelector('#dark-mode-menu');
+menu.addEventListener('click', event => setTheme(event.target.value));
 
-const popup = document.querySelector('vwc-popup#dark-mode-popup');
+const popup = document.querySelector('#dark-mode-menu');
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => setTheme(theme));
 
