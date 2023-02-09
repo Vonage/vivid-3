04e410fad9beb662fc67f459248cc5d40184c377
yinonov <yinon.oved@vonage.com>
2023-02-09 11:47:56 +0200

    fixed

--- apps/docs/assets/scripts/theme-handler.js
+++ apps/docs/assets/scripts/theme-handler.js
@@ -24,21 +24,29 @@ function setTheme(theme) {
 		return;
 	}
 	console.log('setTheme', theme)
-	// localStorage.setItem(THEME, theme);
-	// menuButton.icon = isDark(theme) ? 'dark-mode-solid' : 'light-mode-line';
+	localStorage.setItem(THEME, theme);
+	const isDarkTheme = isDark(theme);
+	menuButton.icon = isDarkTheme ? 'dark-mode-solid' : 'light-mode-line';
+	// menuButton.ariaLabel = isDarkTheme ? 'Dark mode' : 'Light mode';
 
-	// const themeStyle = isDark(theme) ?
-	// 	'<link rel="stylesheet" href="/assets/styles/tokens/theme-dark.css" media="all">' : '<link rel="stylesheet" href="/assets/styles/tokens/theme-light.css" media="all">';
-	// document.head?.insertAdjacentHTML("beforeend", themeStyle);
+	const themeLink = document.head.querySelector('#themeLink');
+	themeLink.href = `/assets/styles/tokens/theme-${isDarkTheme ? 'dark' : 'light'}.css`;
 
-	// menu.open = false;
+	menu.open = false;
 }
 
-// menuButton.addEventListener('click', () => menu.open = !menu.open);
-// menu.addEventListener('change', event => setTheme(event.target.getAttribute('data-value')));
+menuButton.addEventListener('click', () => {
+	menu.open = !menu.open;
+	menuButton.ariaExpanded = menu.open;
+});
+menu.addEventListener('change', ({ target }) => {
+	if (target.checked) {
+		setTheme(target.getAttribute('data-value'));
+	}
+});
 
-// const theme = getTheme();
-// document.querySelector(`[data-value="${getTheme()}"]`).checked = true;
-// setTheme(theme);
+const theme = getTheme();
+document.querySelector(`[data-value="${theme}"]`).checked = true;
+setTheme(theme);
 
 // window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => setTheme(theme));
