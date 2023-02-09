1e275d7dc7b868c1d35db4cadac28ff29259974e
yinonov <yinon.oved@vonage.com>
2023-02-09 10:04:25 +0200

    need to update fast

--- apps/docs/assets/scripts/theme-handler.js
+++ apps/docs/assets/scripts/theme-handler.js
@@ -1,44 +1,44 @@
-function getTheme() {
-	return localStorage.getItem('theme') || 'auto';
-}
+"use strict";
 
-function isDark() {
-	if (theme === 'auto') {
-		return window.matchMedia('(prefers-color-scheme: dark)').matches;
-	}
-	return theme === 'dark';
-}
-
-function setTheme(newTheme) {
-	theme = newTheme;
-
-	localStorage.setItem('theme', theme);
-	menuButton.icon = isDark() ? 'dark-mode-solid' : 'light-mode-line';
-
-	const themeStyle = isDark() ?
-		'<link rel="stylesheet" href="/assets/styles/tokens/theme-dark.css" media="all">' : '<link rel="stylesheet" href="/assets/styles/tokens/theme-light.css" media="all">';
-	document.head?.insertAdjacentHTML("beforeend", themeStyle);
-
-	popup.open = false;
-}
-
-function toggleMenu() {
-	popup.open = !popup.open;
-	if (popup.open) {
-		document.getElementById(`option-${theme}`).selected = true;
-	}
-}
-
-let theme = getTheme();
-
-const menuButton = document.querySelector('#dark-mode-menu-button');
-menuButton.addEventListener('click', toggleMenu);
+const THEME = 'theme';
+const LIGHT = 'light';
+const DARK = 'dark';
+const AUTO = 'auto';
 
 const menu = document.querySelector('#dark-mode-menu');
-menu.addEventListener('click', event => setTheme(event.target.value));
+const menuButton = document.querySelector('#dark-mode-menu-button');
 
-const popup = document.querySelector('#dark-mode-menu');
+function getTheme() {
+	return localStorage.getItem(THEME) || AUTO;
+}
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => setTheme(theme));
+function isDark(theme) {
+	if (theme === AUTO) {
+		return window.matchMedia('(prefers-color-scheme: dark)').matches;
+	}
+	return theme === DARK;
+}
 
-setTheme(theme);
+function setTheme(theme) {
+	if (![AUTO, DARK, LIGHT].includes(theme)) {
+		return;
+	}
+	console.log('setTheme', theme)
+	// localStorage.setItem(THEME, theme);
+	// menuButton.icon = isDark(theme) ? 'dark-mode-solid' : 'light-mode-line';
+
+	// const themeStyle = isDark(theme) ?
+	// 	'<link rel="stylesheet" href="/assets/styles/tokens/theme-dark.css" media="all">' : '<link rel="stylesheet" href="/assets/styles/tokens/theme-light.css" media="all">';
+	// document.head?.insertAdjacentHTML("beforeend", themeStyle);
+
+	// menu.open = false;
+}
+
+// menuButton.addEventListener('click', () => menu.open = !menu.open);
+// menu.addEventListener('change', event => setTheme(event.target.getAttribute('data-value')));
+
+// const theme = getTheme();
+// document.querySelector(`[data-value="${getTheme()}"]`).checked = true;
+// setTheme(theme);
+
+// window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => setTheme(theme));
