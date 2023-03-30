a5bdd1f600f0777bc4789fb12b7ab8a50ff91e8c
olaf-k <olaf-k@users.noreply.github.com>
2023-03-30 10:31:55 +0200

    Update ui.test.ts

--- libs/components/src/lib/button/ui.test.ts
+++ libs/components/src/lib/button/ui.test.ts
@@ -9,8 +9,62 @@ import {
 
 const components = ['button'];
 test('should show the component', async ({ page }: { page: Page }) => {
-	const template = extractHTMLBlocksFromReadme(path.join(new URL('.', import.meta.url).pathname, 'README.md'))
-		.reduce((htmlString: string, block: string) => `${htmlString} <div style="margin: 5px;">${block}</div>`, '');
+	const template = `
+	<div style="margin: 5px;"><vwc-button appearance='filled' label='A default button'></vwc-button></div>
+	<div style="margin: 5px;">
+		<vwc-button label='ghost' appearance='ghost'></vwc-button>
+		<vwc-button label='filled' appearance='filled'></vwc-button>
+		<vwc-button label='outlined' appearance='outlined'></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button appearance="filled" icon='message-sent-line' aria-label="Send Message"></vwc-button>
+		<vwc-button appearance="filled" icon='message-sent-line' aria-label="Send Message" shape="pill"></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button appearance="filled" label='icon' icon='check-line'></vwc-button>
+		<vwc-button appearance="filled" label='icon-trailing' icon='check-line' icon-trailing></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button appearance='filled' label='rounded' shape='rounded'></vwc-button>
+		<vwc-button appearance='filled' label='pill' shape='pill'></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button appearance='filled' label='condensed' size='condensed'></vwc-button>
+		<vwc-button appearance='filled' label='normal' size='normal'></vwc-button>
+		<vwc-button appearance='filled' label='expanded' size='expanded'></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button stacked appearance='filled' label='Stacked'></vwc-button>
+		<vwc-button stacked appearance='filled' icon='message-sent-line' label='With Icon'></vwc-button>
+		<vwc-button stacked appearance='filled' icon='message-sent-line' icon-trailing label='Icon Trailing'></vwc-button>
+		<vwc-button stacked appearance='filled' icon='message-sent-line'></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button appearance="ghost" label='accent' connotation='accent'></vwc-button>
+		<vwc-button appearance="ghost" label='cta' connotation='cta'></vwc-button>
+		<vwc-button appearance="ghost" label='success' connotation='success'></vwc-button>
+		<vwc-button appearance="ghost" label='alert' connotation='alert'></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button appearance="filled" label='accent' connotation='accent'></vwc-button>
+		<vwc-button appearance="filled" label='cta' connotation='cta'></vwc-button>
+		<vwc-button appearance="filled" label='success' connotation='success'></vwc-button>
+		<vwc-button appearance="filled" label='alert' connotation='alert'></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button appearance="outlined" label='accent' connotation='accent'></vwc-button>
+		<vwc-button appearance="outlined" label='cta' connotation='cta'></vwc-button>
+		<vwc-button appearance="outlined" label='success' connotation='success'></vwc-button>
+		<vwc-button appearance="outlined" label='alert' connotation='alert'></vwc-button>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-button appearance='ghost' label='ghost' disabled></vwc-button>
+		<vwc-button appearance='filled' label='filled' disabled></vwc-button>
+		<vwc-button appearance='outlined' label='outlined' disabled></vwc-button>
+	</div>
+	<div style="margin: 5px;"><vwc-button connotation='cta' shape='pill' icon='microphone-solid' aria-label="Mute"></vwc-button></div>
+	<div style="margin: 5px;"><vwc-button style='display: block;' label="I'm full width" shape='pill' appearance='filled'></vwc-button></div>
+	`;
 
 	page.setViewportSize({ width: 500, height: 720 });
 
