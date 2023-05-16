baa1a0556435c8c0009e2efcb0ca9559a0b1bb1c
olaf-k <olaf-k@users.noreply.github.com>
2023-05-16 09:47:25 +0200

    ui tests

--- libs/components/src/lib/toggletip/ui.test.ts
+++ libs/components/src/lib/toggletip/ui.test.ts
@@ -1,8 +1,6 @@
-import * as path from 'path';
 import { expect, test } from '@playwright/test';
 import type { Page } from '@playwright/test';
 import {
-	extractHTMLBlocksFromReadme,
 	loadComponents,
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
@@ -10,13 +8,41 @@ import {
 const components = ['toggletip'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
-	const template = extractHTMLBlocksFromReadme(
-		path.join(new URL('.', import.meta.url).pathname, 'README.md')
-	).reduce(
-		(htmlString: string, block: string) =>
-			`${htmlString} <div style="margin: 5px;">${block}</div>`,
-		''
-	);
+	const template = `
+	<div style="padding: 20px 100px;">
+		<vwc-button id="button1" icon="help-solid" shape="pill"></vwc-button>
+		<vwc-toggletip open anchor="button1">My anchor is an ID</vwc-toggletip>
+	</div>
+
+	<div style="padding: 20px 100px;">
+		<vwc-button id="button3" icon="help-solid" shape="pill"></vwc-button>
+		<vwc-toggletip open anchor="button3" open>I'm open by default</vwc-toggletip>
+	</div>
+
+	<div style="padding: 20px 100px;">
+		<vwc-button id="button4" icon="help-solid" shape="pill"></vwc-button>
+		<vwc-toggletip open anchor="button4" headline="This is the headline">This is the content</vwc-toggletip>
+	</div>
+
+	<div style="padding: 50px 100px;">
+		<vwc-button id="button6" icon="help-solid" shape="pill"></vwc-button>
+		<vwc-toggletip open anchor="button6" placement="top">top</vwc-toggletip>
+		<vwc-toggletip open anchor="button6" placement="right">right</vwc-toggletip>
+		<vwc-toggletip open anchor="button6" placement="bottom">bottom</vwc-toggletip>
+		<vwc-toggletip open anchor="button6" placement="left">left</vwc-toggletip>
+	</div>
+
+	<div style="padding: 50px 100px;">
+		<vwc-button id="button7" icon="help-solid" shape="pill"></vwc-button>
+		<vwc-toggletip open anchor="button7">
+			This is a toggletip with action items
+			<vwc-button appearance='outlined' label='Action' shape='pill' slot="action-items"></vwc-button>
+			<vwc-button appearance='filled' label='Action' shape='pill' slot="action-items"></vwc-button>
+		</vwc-toggletip>
+	</div>
+	`;
+
+	page.setViewportSize({ width: 500, height: 720 });
 
 	await loadComponents({
 		page,
