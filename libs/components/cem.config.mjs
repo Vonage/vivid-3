08d5cd673e0153ef804a6068126862d44201af72
acelletti <37002976+acelletti@users.noreply.github.com>
2023-05-18 11:26:24 +0100

    feat(tokens): Add granular component tokens (VIV-1059) (#1147)
    * Add connotation variable overrides for each component.
    
    * Add some examples to the docs.
    
    * Extract connotation variables into custom-element-manifest
    
    * Support showing variables from custom element manifest in docs
    
    * Restrict granular variables to accent connotation
    
    * Include generateMeta in build step
    
    * Add custom color examples to docs
    
    * Fix comment not always being on new line
    
    * Don't restrict variables to accent connotation
    
    * Group variables by connotation in docs
    
    * Fix docs serve not depending on generateMeta
    
    * Revert adding examples to docs
    
    * Add page with customization examples
    
    * Fix customization page
    
    * Remove underlying components from customization page
    
    * Sort connotation variables by shade
    
    * Remove tabs from certain components
    
    * Add explanation to customization page
    
    * Delete duplicate image
    
    * Improve colors table
    
    * Improve example for alert
    
    ---------
    
    Co-authored-by: Richard Helm <richard.helm@vonage.com>

--- libs/components/cem.config.mjs
+++ libs/components/cem.config.mjs
@@ -2,6 +2,8 @@ import { readonlyPlugin } from 'cem-plugin-readonly'
 import { asyncFunctionPlugin } from 'cem-plugin-async-function'
 import { jsdocFunctionPlugin } from 'cem-plugin-jsdoc-function'
 import { jsdocExamplePlugin } from 'cem-plugin-jsdoc-example'
+import sass from 'sass'
+import fs from 'fs'
 
 export default {
     /** Globs to analyze */
@@ -26,6 +28,75 @@ export default {
         jsdocFunctionPlugin(),
         jsdocExamplePlugin(),
         asyncFunctionPlugin(),
-        readonlyPlugin()
-    ]
-}
+        readonlyPlugin(),
+        {
+            name: 'stylePlugin',
+            analyzePhase({ ts, node, moduleDoc, context }) {
+                switch (node.kind) {
+                    case ts.SyntaxKind.ClassDeclaration:
+                        const className = node.name.getText();
+                        const classDeclaration = moduleDoc.declarations.find(
+                            (declaration) => declaration.name === className
+                        );
+
+                        // Try to find corresponding scss file
+                        const filePath = node.getSourceFile().fileName; // e.g 'libs/components/src/lib/tree-view/tree-view.ts'
+                        const withoutExtension = filePath.replace(
+                            /\.[^/.]+$/,
+                            ''
+                        );
+                        const stylePath = `${withoutExtension}.scss`;
+                        if (!fs.existsSync(stylePath)) {
+                            return;
+                        }
+
+                        // Then find comments beginning '/* @cssprop' in the compiled css
+                        const result = sass.compile(stylePath);
+                        const compiledCss = result.css.toString();
+                        for (const line of compiledCss.split('\n')) {
+                            const match = line.match(
+                                /^.*\/\*\s*@cssprop(erty)?\s+(.+)\s*\*\/\s*$/
+                            );
+                            if (!match) {
+                                continue;
+                            }
+
+                            let definition = match[2];
+
+                            let cssProperty = {};
+
+                            if (definition.startsWith('[')) {
+                                // Definition provides a default value, e.g. '[--vvd-button-cta-primary=var(--vvd-color-canvas-text)]'
+                                const insideBrackets = definition.substring(
+                                    1,
+                                    definition.indexOf(']')
+                                );
+                                definition = definition
+                                    .substring(definition.indexOf(']') + 1)
+                                    .trim();
+                                const parts = insideBrackets.split('='); // e.g. ['--vvd-button-cta-primary', 'var(--vvd-color-canvas-text)']
+                                cssProperty.name = parts[0]; // e.g. '--vvd-button-cta-primary'
+                                cssProperty.default = parts[1]; // e.g. 'var(--vvd-color-canvas-text)'
+                            } else {
+                                // Definition just contains the name, e.g. '--vvd-button-cta-primary'
+                                const [varName, ...rest] =
+                                    definition.split(' ');
+                                definition = rest.join(' ').trim();
+                                cssProperty.name = varName; // e.g. '--vvd-button-cta-primary'
+                            }
+
+                            // Now there may be a description left, e.g. '- Primary button color'
+                            const description = definition
+                                .replace('-', '')
+                                .trim();
+                            if (description) {
+                                cssProperty.description = description;
+                            }
+
+                            classDeclaration.cssProperties.push(cssProperty);
+                        }
+                }
+            },
+        },
+    ],
+};
