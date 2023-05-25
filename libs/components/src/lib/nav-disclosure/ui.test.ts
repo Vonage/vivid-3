41de222c36151053dcd7ba7f7f011317c548bf9f
Richard Helm <richard.helm@vonage.com>
2023-05-25 12:28:05 +0100

    Update UI snapshots

--- libs/components/src/lib/nav-disclosure/ui.test.ts
+++ libs/components/src/lib/nav-disclosure/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['nav-disclosure', 'nav-item'];
+const components = ['nav-disclosure', 'nav-item', 'badge'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(path.join(new URL('.', import.meta.url).pathname, 'README.md'))
