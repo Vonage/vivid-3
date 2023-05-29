62d8bb9a34efa8fee8b61399424d31e85f3915ba
Yonatan Kra <yonatan.kra@vonage.com>
2023-05-29 09:23:08 +0300

    Lighthouse-playwright

--- libs/components/src/lib/select/select.a11y.ts
+++ libs/components/src/lib/select/select.a11y.ts
@@ -1,6 +1,6 @@
 import { test } from '@playwright/test';
 import type { Page } from '@playwright/test';
-import { playAudit } from 'playwright-lighthouse'
+import { playAudit } from 'playwright-lighthouse';
 import {
 	loadComponents,
 	loadTemplate,
@@ -11,6 +11,7 @@ const components = ['select', 'option', 'badge'];
 test('a11y', async ({ page }: { page: Page }) => {
 
 	const template = `
+	<div style="height: 500px; width: 500px; display: flex; flex-wrap: nowrap;">
 		<vwc-select aria-label="test">
 			<vwc-option value="1" text="Option 1"></vwc-option>
 			<vwc-option value="2" text="Option 2"></vwc-option>
@@ -26,6 +27,7 @@ test('a11y', async ({ page }: { page: Page }) => {
 			<vwc-option value="3" text="Option 3"></vwc-option>
 			<span slot="meta" class="duration">00:00:00</span>
 		</vwc-select>
+		</div>
 	`;
 
 	await loadComponents({
@@ -37,18 +39,36 @@ test('a11y', async ({ page }: { page: Page }) => {
 		template,
 	});
 
-	await page.$('#wrapper');
+	const testWrapper = await page.$('#wrapper');
 
 	await page.waitForLoadState('networkidle');
 
 	const config = {
 		extends: 'lighthouse:default',
 		settings: {
+			maxWaitForFcp: 15 * 1000,
+			maxWaitForLoad: 35 * 1000,
+			formFactor: 'desktop',
+			screenEmulation: {
+				mobile: false,
+				width: 1350,
+				height: 940,
+				deviceScaleFactor: 1,
+				disabled: false,
+			},
+			// eslint-disable-next-line max-len
+			emulatedUserAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
 			skipAudits: [
-				'html-has-lang','document-title'
+				'html-has-lang',
+				'document-title'
 			],
 		},
 	};
+
+	await testWrapper?.screenshot({
+		path: './snapshots/select-a11y.png',
+	});
+
 	await playAudit({
 		page: page,
 		thresholds: {
@@ -57,7 +77,8 @@ test('a11y', async ({ page }: { page: Page }) => {
 		port: 9222,
 		reports: {
 			formats: {
-				html: true, //defaults to false
+				html: true,
+				json: true,
 			},
 			name: 'select-a11y', //defaults to `lighthouse-${new Date().getTime()}`
 		},
