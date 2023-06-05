9ecbce2933ac49bb99e049e372a973333be35f2f
Rina <rina.oksman@vonage.com>
2023-06-05 12:39:40 +0300

    extracted code to external file

---
+++ apps/docs/assets/scripts/icons-gallery.js
@@ -0,0 +1,76 @@
+const VERSION = "4.1.2";
+const ICONS_TO_SHOW = 16;
+let jsonData;
+let index = 0;
+let iconsShown = ICONS_TO_SHOW;
+
+fetchJSONData();
+
+async function fetchJSONData() {
+  const response = await fetch(`https://icon.resources.vonage.com/v${VERSION}/manifest.json`);
+  jsonData = await response.json();
+
+  showIcons(jsonData);
+} 
+
+function showIcons(data) {
+  index = 0;
+  while (last = iconsLayout.lastChild) iconsLayout.removeChild(last);
+  while(index < data.length){
+    addIcon(data[index].id);
+    if(++ index >= ICONS_TO_SHOW) break;
+  }
+  disableShowMoreButton(data);
+}
+
+function showMoreIcons(data){
+  while(index < data.length){
+    addIcon(data[index].id);
+    if(++ index >= iconsShown) break;
+  }
+  disableShowMoreButton(data);
+}
+
+function disableShowMoreButton(data){
+  showMoreButton.disabled = (iconsShown >= data.length);
+}
+
+function showMore(){
+  iconsShown += ICONS_TO_SHOW;
+  filterIcons();
+}
+
+function addIcon(id){
+    const emptyState = document.createElement('vwc-empty-state');
+    emptyState.headline = emptyState.icon = id;
+    iconsLayout.appendChild(emptyState);
+}
+
+function onClickFilter() {
+  iconsShown = ICONS_TO_SHOW;
+  filterIcons();
+}
+
+function filterIcons() {
+  let iconsArray = jsonData.filter(item => item.keyword.some(icon => icon.includes(iconsTextField.value)));
+
+  iconsArray = filterIconsByTags(iconsArray);
+  iconsShown > ICONS_TO_SHOW ? showMoreIcons(iconsArray) : showIcons(iconsArray);
+}
+
+ function filterIconsByTags(iconsArray) {
+  if(solidTag.selected){
+    iconsArray = iconsArray.filter(item => item.tag.some(icon => icon === "style_weight_solid"));
+  }
+  if(linearTag.selected){
+    iconsArray = iconsArray.filter(item => item.tag.some(icon => icon === "style_weight_regular"));
+  }
+  if(singleTag.selected){
+    iconsArray = iconsArray.filter(item => item.tag.some(icon => icon === "style_color_single"));
+  }
+  if(multiTag.selected){
+    iconsArray = iconsArray.filter(item => item.tag.some(icon => icon === "style_color_multi"));
+  }
+
+  return iconsArray;
+ }
