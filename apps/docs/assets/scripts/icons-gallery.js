e828f550a073d50173888b1f92530161372ed0f0
Rina <rina.oksman@vonage.com>
2023-06-06 15:02:32 +0300

    update design and copy to clipboard

--- apps/docs/assets/scripts/icons-gallery.js
+++ apps/docs/assets/scripts/icons-gallery.js
@@ -1,5 +1,5 @@
 const VERSION = "4.1.2";
-const ICONS_TO_SHOW = 14;
+const ICONS_TO_SHOW = 21;
 let jsonData;
 let index = 0;
 let iconsShown = ICONS_TO_SHOW;
@@ -41,10 +41,31 @@ function showMore(){
 }
 
 function addIcon(id){
-    const emptyState = document.createElement('vwc-empty-state');
-    emptyState.id = "emptyState";
-    emptyState.headline = emptyState.icon = id;
-    iconsLayout.appendChild(emptyState);
+    const iconDiv = document.createElement('div');
+    iconDiv.id = "iconDiv";
+
+    const iconSpan = document.createElement('span');
+    iconSpan.id = "iconSpan";
+
+    const icon = document.createElement('vwc-icon');
+    icon.name = id;
+    icon.size = "1";
+    iconSpan.appendChild(icon);
+
+    const nameSpan = document.createElement('span');
+    nameSpan.id = "nameSpan";
+    nameSpan.innerText = id;
+
+    iconDiv.appendChild(iconSpan);
+    iconDiv.appendChild(nameSpan);
+
+    iconDiv.onclick = () => onClickiconDiv(id);
+    iconsLayout.appendChild(iconDiv);
+}
+
+function onClickiconDiv(id) {
+  navigator.clipboard.writeText(id);
+  copyAlert.open = true;
 }
 
 function onClickFilter() {
@@ -55,21 +76,21 @@ function onClickFilter() {
 function filterIcons() {
   let iconsArray = jsonData.filter(item => item.keyword.some(icon => icon.includes(iconsTextField.value)));
 
-  iconsArray = filterIconsByRadio(iconsArray);
+  iconsArray = filterIconsByTag(iconsArray);
   iconsShown > ICONS_TO_SHOW ? showMoreIcons(iconsArray) : showIcons(iconsArray);
 }
 
- function filterIconsByRadio(iconsArray) {
-  if(solidRadio.checked){
+ function filterIconsByTag(iconsArray) {
+  if(solidTag.selected){
     iconsArray = iconsArray.filter(item => item.tag.some(icon => icon === "style_weight_solid"));
   }
-  if(linearRadio.checked){
+  if(linearTag.selected){
     iconsArray = iconsArray.filter(item => item.tag.some(icon => icon === "style_weight_regular"));
   }
-  if(singleRadio.checked){
+  if(singleTag.selected){
     iconsArray = iconsArray.filter(item => item.tag.some(icon => icon === "style_color_single"));
   }
-  if(multiRadio.checked){
+  if(multiTag.selected){
     iconsArray = iconsArray.filter(item => item.tag.some(icon => icon === "style_color_multi"));
   }
 
--- docs/design/icons-gallery.md
+++ docs/design/icons-gallery.md
@@ -1,25 +1,29 @@
 # Icons Gallery
 
-Select the icon you wish to use and copy its name. 
+Choose the icon you wish to use and click to copy its name. 
 As an example, you can use it as follows:
 
 ```html
 <vwc-icon name="icon-name-here"></vwc-icon>
 ```
 
+<link rel="stylesheet" href="../../assets/styles/icons-gallery.css">
+
 <vwc-text-field id="iconsTextField" icon="search-line" label="Search Icons" oninput="onClickFilter()"></vwc-text-field>
 
-<vwc-radio-group id="radioGroup" label="Filter by:" onclick="onClickFilter()">
-  <vwc-radio id="solidRadio" label="Solid" value="Solid"></vwc-radio>
-  <vwc-radio id="linearRadio" label="Linear" value="Linear"></vwc-radio>
-  <vwc-radio id="singleRadio" label="Single Color" value="Single"></vwc-radio>
-  <vwc-radio id="multiRadio" label="Multi Color" value="Multi"></vwc-radio>
-  <vwc-radio id="allStylesRadio" label="All" value="All" checked></vwc-radio>
-</vwc-radio-group>
+<vwc-tag-group onclick="onClickFilter()">
+  Filter By Style:
+  <vwc-tag id="solidTag" label="Solid" selectable></vwc-tag>
+  <vwc-tag id="linearTag" label="Linear" selectable></vwc-tag>
+  Filter By Color:
+  <vwc-tag id="singleTag" label="Single Color" selectable></vwc-tag>
+  <vwc-tag id="multiTag" label="Multi Color" selectable></vwc-tag>
+</vwc-tag-group>
+
+<vwc-alert id="copyAlert" text="Icon name copied to clipboard" connotation="success" timeoutms="2000"></vwc-alert>
 
 <vwc-layout id="iconsLayout" gutters="small" column-basis="small"></vwc-layout>
 
 <vwc-button id="showMoreButton" label="Show More" appearance='filled' onclick="showMore()"></vwc-button>
 
 <script src="../../assets/scripts/icons-gallery.js" async></script>
-<link rel="stylesheet" href="../../assets/styles/icons-gallery.css">
