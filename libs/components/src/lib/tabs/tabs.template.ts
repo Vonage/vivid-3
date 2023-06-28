966f85366278d1f2db8c90ab716d15700189c4c2
RTannenbaum <rachel.tannenbaum@vonage.com>
2023-06-28 09:59:42 +0300

    tab scroll

--- libs/components/src/lib/tabs/tabs.template.ts
+++ libs/components/src/lib/tabs/tabs.template.ts
@@ -20,11 +20,13 @@ export function TabsTemplate<T extends Tabs>() {
 	return html<T>`
 	<template role="tablist">
 		<div class="${getClasses}">
-			<div class="tablist" ${ref('tablist')}>
+			<div class="tablist-wrapper">
+					<div class="tablist" ${ref('tablist')}>
 				<slot name="tab" ${slotted('tabs')}></slot>
 				${when(x => x.showActiveIndicator, html<T>`
 					<div ${ref('activeIndicatorRef')} class="active-indicator"></div>
 				`)}
+			</div>
 			</div>
 			<div class="tabpanel">
 				<slot name="tabpanel" ${slotted('tabpanels')}></slot>
