7181483792498528b3ea8ef6165bc197ba31c260
Yonatan Kra <yonatan.kra@vonage.com>
2023-07-10 11:35:15 +0300

    fix(tabs): tabs now scroll only inside the tabs (VIV-1191) (#1253)

--- libs/components/src/lib/tabs/tabs.ts
+++ libs/components/src/lib/tabs/tabs.ts
@@ -22,7 +22,6 @@ export type TabsConnotation = Extract<Connotation,
 export class Tabs extends FoundationTabs {
 
 	@observable tablist?: HTMLElement;
-
 	/**
 	 * The connotation the tabs should have.
 	 *
@@ -43,28 +42,24 @@ export class Tabs extends FoundationTabs {
 			this.activeIndicatorRef.style.removeProperty(ACTIVE_TAB_WIDTH);
 		}
 		this.#patchActiveID();
-
 	}
 
 	override activeidChanged(oldValue: string, newValue: string): void {
 		super.activeidChanged(oldValue, newValue);
 		this.patchIndicatorStyleTransition();
 		this.#patchActiveID();
-
 	}
 
 	override tabsChanged(): 	void {
 		super.tabsChanged();
 		this.patchIndicatorStyleTransition();
 		this.#patchActiveID();
-
 	}
 
 	override tabpanelsChanged(): void {
 		super.tabpanelsChanged();
 		this.patchIndicatorStyleTransition();
 		this.#patchActiveID();
-
 	}
 
 	patchIndicatorStyleTransition() {
@@ -86,6 +81,33 @@ export class Tabs extends FoundationTabs {
 		}
 	}
 
+	get #tabListWrapper() {
+		return this.shadowRoot!.querySelector('.tablist-wrapper') as HTMLElement;
+	}
+
+	#scrollToIndex(index: number) {
+		const tab = this.tabs[index];
+		if (!tab) return;
+		let left = 0;
+		let top = 0;
+		if (this.orientation === TabsOrientation.vertical) {
+			if (index === this.tabs.length - 1) {
+				top = this.#tabListWrapper.scrollHeight;
+			}
+			if (index > 0 && index < this.tabs.length - 1) {
+				top = tab.offsetTop - this.#tabListWrapper.offsetHeight / 2 + (tab.offsetHeight / 2);
+			}
+		} else {
+			if (index === this.tabs.length - 1) {
+				left = this.#tabListWrapper.scrollWidth;
+			}
+			if (index > 0 && index < this.tabs.length - 1) {
+				left = tab.offsetLeft - this.#tabListWrapper.offsetWidth / 2 + (tab.offsetWidth / 2);
+			}
+		}
+
+		this.#tabListWrapper.scrollTo({top, left, behavior: 'smooth'});
+	}
 	// adapted FAST fix https://github.com/microsoft/fast/pull/6606
 	#patchActiveID() {
 		if (!this.activetab) return;
@@ -93,6 +115,7 @@ export class Tabs extends FoundationTabs {
 		const idx = this.tabs.indexOf(this.activetab);
 		this.activeid = this['tabIds'][idx];
 		this.#updateTabsConnotation();
-		this.activetab.scrollIntoView({ block: 'nearest', behavior: 'smooth'});
+
+		this.#scrollToIndex(idx);
 	}
 }
