5283ce3b8d72034f79510997d019a01656988e82
Yonatan Kra <yonatan.kra@vonage.com>
2023-07-20 15:29:06 +0300

    Fix merge mistake

--- libs/components/src/lib/data-grid/data-grid.options.ts
+++ libs/components/src/lib/data-grid/data-grid.options.ts
@@ -1,12 +1,5 @@
 export type ValuesOf<T> = T[keyof T];
 
-export const DataGridCellSortStates = {
-	none: 'none',
-	ascending: 'ascending',
-	descending: 'descending',
-	other: 'other'
-} as const;
-
 /**
  * Enumerates the data grid auto generated header options
  * default option generates a non-sticky header row
