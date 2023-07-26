d6449b70aaefd5fcae202dc9999ae5ae40696d90
Rina <rina.oksman@vonage.com>
2023-07-26 18:33:32 +0300

    update snapshots

--- libs/components/src/lib/nav/ui.test.ts
+++ libs/components/src/lib/nav/ui.test.ts
@@ -1,17 +1,25 @@
-import * as path from 'path';
 import { expect, test } from '@playwright/test';
 import type { Page } from '@playwright/test';
 import {
-	extractHTMLBlocksFromReadme,
 	loadComponents,
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['nav', 'nav-item'];
+const components = ['nav', 'nav-item', 'badge', 'icon'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
-	const template = extractHTMLBlocksFromReadme(path.join(new URL('.', import.meta.url).pathname, 'README.md'))
-		.reduce((htmlString: string, block: string) => `${htmlString} <div style="margin: 5px;">${block}</div>`, '');
+	const template = `<vwc-nav>
+	<vwc-nav-item href="#" icon="profile" text="Account" onclick="onClick(this)" aria-current="page" ></vwc-nav-item>
+	<vwc-nav-item href="#" icon="inbox-line" text="Inbox" onclick="onClick(this)">
+		  <vwc-badge slot="meta" text="21" connotation="alert" appearance="subtle" shape="pill"></vwc-badge>
+	  </vwc-nav-item>
+	<vwc-nav-item href="#" icon="ai" text="AI Studio" onclick="onClick(this)">
+		  <vwc-badge slot="meta" text="new" connotation="success" appearance="subtle" shape="pill"></vwc-badge>
+	  </vwc-nav-item>
+	  <vwc-nav-item href="#" icon="books-line" text="Documentation" onclick="onClick(this)">
+		  <vwc-icon slot="meta" name="open-solid"></vwc-icon>
+	  </vwc-nav-item>
+  </vwc-nav>`;
 
 	page.setViewportSize({ width: 200, height: 720 });
 
