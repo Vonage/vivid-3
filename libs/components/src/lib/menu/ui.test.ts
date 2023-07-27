79ff73a16775a2190cc96c7657152de45f319c61
Rina Oksman <rinaok@gmail.com>
2023-07-27 12:59:31 +0300

    .

--- libs/components/src/lib/menu/ui.test.ts
+++ libs/components/src/lib/menu/ui.test.ts
@@ -8,7 +8,32 @@ import {
 const components = ['menu', 'menu-item', 'button', 'text-field'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
-	c
+	const template =`
+		<style>
+			.wrapper {
+				width: 300px;
+				height: 700px;
+				position: relative;
+			}
+		</style>
+
+		<div class="wrapper">
+			<vwc-button id="button" label="Toggle Menu" onclick="menu.open = !menu.open" appearance="outlined"></vwc-button>
+
+			<vwc-menu id="menu" anchor="button" placement="right-start" open>
+				<vwc-text-field slot="header" placeholder="Search" icon="search"></vwc-text-field>
+				<vwc-menu-item text="Lorem ipsum dolor sit amet, consectetur adipisicing elit"></vwc-menu-item>
+				<vwc-menu-item text="Menu item 1"></vwc-menu-item>
+				<vwc-menu-item text="Menu item 2"></vwc-menu-item>
+				<vwc-menu-item text="Menu item 3"></vwc-menu-item>
+				<vwc-menu-item text="Menu item 4"></vwc-menu-item>
+				<vwc-button slot="action-items" appearance="outlined" label="Close"></vwc-button>
+				<vwc-button slot="action-items" appearance="filled" label="Select"></vwc-button>
+			</vwc-menu>
+		</div>`;
+
+	page.setViewportSize({ width: 720, height: 720 });
+
 	await loadComponents({
 		page,
 		components,
