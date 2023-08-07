37861e8718e42d8d1231afe454503ca8458c4749
Yonatan Kra <yonatan.kra@vonage.com>
2023-08-07 13:00:36 +0300

    Vite is working
    TODO::take care of the definition imports (the component.ts entry)
    TODO::See that the api-meta is correctly generated
    TODO::use the publish command

---
+++ libs/components/vite.config.ts
@@ -0,0 +1,48 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
+
+export default defineConfig({
+	cacheDir: '../../../node_modules/.vite/components',
+
+	plugins: [
+		dts({
+			entryRoot: 'src',
+			tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+			skipDiagnostics: true,
+		}),
+
+		viteTsConfigPaths({
+			root: '../../',
+		}),
+	],
+
+	worker: {
+	 plugins: [
+	   viteTsConfigPaths({
+	     root: '../../',
+	   }),
+	 ],
+	},
+
+	// Configuration for building your library.
+	// See: https://vitejs.dev/guide/build.html#library-mode
+	build: {
+		lib: {
+			// Could also be a dictionary or array of multiple entry points.
+			entry: 'src/index.ts',
+			name: 'components',
+			fileName: 'index',
+			// Change this to the formats you want to support.
+			// Don't forget to update your package.json as well.
+			formats: ['es', 'cjs'],
+		},
+		rollupOptions: {
+			// External packages that should not be bundled into your library.
+			external: [],
+		},
+	},
+});
