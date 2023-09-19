1b3954fa804fc0fae49b1dee1d53231faab9021d
Rina Oksman <rina.oksman@vonage.com>
2023-09-19 15:44:29 +0300

    feat(accordion-item and tree-item): add icon slot (VIV-1323) (#1383)
    * add icon slot to accordion-item
    
    * add icon slot to tree-item
    
    * fix tests
    
    * update snapshots
    
    * fix ui tests
    
    * fix test
    
    * fix

--- libs/components/src/lib/accordion-item/ui.test.ts
+++ libs/components/src/lib/accordion-item/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['accordion-item'];
+const components = ['accordion', 'accordion-item', 'icon'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
