94dba91670ee10f7cfe1db5d70a9fb3fadb304e4
Rina <rina.oksman@vonage.com>
2023-10-10 14:46:01 +0300

    progress

--- libs/components/src/lib/audio-player/audio-player.template.ts
+++ libs/components/src/lib/audio-player/audio-player.template.ts
@@ -1,42 +1,20 @@
-import { html } from '@microsoft/fast-element';
+/* eslint-disable max-len */
+import { html, ref } from '@microsoft/fast-element';
 import type { ViewTemplate } from '@microsoft/fast-element';
 import type {
 	ElementDefinitionContext,
 	FoundationElementDefinition,
 } from '@microsoft/fast-foundation';
-import { classNames } from '@microsoft/fast-web-utilities';
-import { Button } from '../button/button';
-import type { AudioPlayer } from './audio-player';
 import { Elevation } from '../elevation/elevation';
+import type { AudioPlayer } from './audio-player';
 
-const getClasses = ({ connotation, disabled }: AudioPlayer) =>
-	classNames(
-		'base',
-		[`connotation-${connotation}`, Boolean(connotation)],
-		['disabled', Boolean(disabled)],
-	);
+// const getClasses = ({ connotation, disabled }: AudioPlayer) =>
+// 	classNames(
+// 		'base',
+// 		[`connotation-${connotation}`, Boolean(connotation)],
+// 		['disabled', Boolean(disabled)],
+// 	);
 
-const SECOND = 1;
-const MINUTE = 60 * SECOND;
-const HOUR = 60 * MINUTE;
-
-const formatTime = (seconds: number) => {
-	const outputTime: Array<number> = [];
-	[HOUR, MINUTE, SECOND].reduce((ac: number, divider: number) => {
-		outputTime.push(~~(ac / divider));
-		return ac % divider;
-	}, seconds);
-	return outputTime
-		.filter((segment: number, index: number, arr: number[]) => segment > 0 || index > arr.length - 3)
-		.map((segment: number, index: number) => segment.toString().padStart(index === 0 ? 1 : 2, '0'))
-		.join(':');
-};
-
-
-function getTimeStampTemplate(_playheadPosition: number, _duration: number) {
-	const timeString = _duration === Infinity ? '__ / __' : `${formatTime(_playheadPosition)} / ${formatTime(_duration)}`;
-	return html`<div class="playhead-position">${timeString}</div>`;
-}
 
 /**
  * The template for the AudioPlayer component.
@@ -48,21 +26,32 @@ export const AudioPlayerTemplate: (
 	context: ElementDefinitionContext,
 	definition: FoundationElementDefinition
 ) => ViewTemplate<AudioPlayer> = (context: ElementDefinitionContext) => {
-	const buttonTag = context.tagFor(Button);
+	// const buttonTag = context.tagFor(Button);
 	const elevationTag = context.tagFor(Elevation);
 
 	return html<AudioPlayer>`
-	<audio class='audio' src='${x => x.src}'></audio>
 	<${elevationTag} dp="2">
-		<div class="${getClasses}">
-			<${buttonTag}
-				aria-label="Play/Pause"
-				class="control-button"
-				?disabled=${x => x.disabled}
-				icon="play-solid"
-				connotation=${x => x.connotation}
-			></${buttonTag}>
-			${x => x.timestamp ? getTimeStampTemplate(x._playheadPosition, x._duration) : ''}
-		</div>
-	</${elevationTag}>`;
+    <div ${ref('audioPlayer')} class="audio green-audio-player">
+      <div class="play-pause-btn" ${ref('playpauseBtn')}>  
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 18 24">
+          <path fill="#566574" fill-rule="evenodd" d="M18 12L0 24V0" class="play-pause-icon" id="playPause" ${ref('playPause')}/>
+        </svg>
+      </div>
+
+      <div class="controls">
+        <span class="current-time" ${ref('currentTime')}>0:00</span>
+        <span> / </span>
+        <span class="total-time" ${ref('totalTime')}>0:00</span>
+        <div class="slider" ${ref('sliders')} data-direction="horizontal">
+          <div class="progress" ${ref('progress')} >
+            <div class="pin" id="progress-pin" data-method="${x => x.rewind}"></div>
+          </div>
+        </div>
+      </div>
+
+      <audio ${ref('player')} src="${x => x.src}" type="audio/mpeg"></audio>
+    </div>
+  </${elevationTag}>`;
 };
+
+
--- libs/components/src/lib/audio-player/audio-player.ts
+++ libs/components/src/lib/audio-player/audio-player.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { FoundationElement } from '@microsoft/fast-foundation';
 import { attr } from '@microsoft/fast-element';
 import type { Connotation } from '../enums';
@@ -59,11 +60,148 @@ export class AudioPlayer extends FoundationElement {
 	 */
 	@attr({ mode: 'boolean' }) noseek = false;
 
-	_duration = 0;
+	draggableClasses = ['pin'];
+	currentlyDragged: any = null;
 
-	_isPlaying = false;
+	audioPlayer!: any;
+	playPause!: any;
+	playpauseBtn!: HTMLElement;
+	progress!: any;
+	sliders!: HTMLElement[];
+	player!: any;
+	currentTime!: any;
+	totalTime!: any;
+	speaker!: any;
 
-	_loading = true;
+	override connectedCallback() {
+		super.connectedCallback();
 
-	_playheadPosition = 0;
+		window.addEventListener('mousedown', this.onMouseDown);
+
+		this.playpauseBtn.addEventListener('click', this.togglePlay);
+		this.player.addEventListener('timeupdate', this.updateProgress);
+		this.player.addEventListener('loadedmetadata', () => {
+			this.totalTime.textContent = this.formatTime(this.player.duration);
+		});
+		this.player.addEventListener('ended', () => {
+			this.playPause.setAttribute('d', 'M18 12L0 24V0');
+			this.player.currentTime = 0;
+		});
+
+		this.sliders.forEach((slider: HTMLElement) => {
+			const pin: any = slider.querySelector('.pin');
+			slider.addEventListener('click', window[pin!.dataset.method] as any);
+		});
+	}
+
+	onMouseDown = (event: any): any => {
+		if (!this.isDraggable(event.target)) return false;
+
+		this.currentlyDragged = event.target;
+		const handleMethod: any = this.currentlyDragged!.dataset.method;
+
+		this.addEventListener('mousemove', window[handleMethod] as any, false);
+
+		window.addEventListener('mouseup', this.onMouseUp, handleMethod);
+	};
+
+
+	onMouseUp = (handleMethod: any) => {
+		this.currentlyDragged = false;
+		window.removeEventListener('mousemove', window[handleMethod] as any, false);
+	};
+
+	isDraggable = (el: HTMLElement) => {
+		let canDrag = false;
+		const classes = Array.from(el.classList);
+		this.draggableClasses.forEach(draggable => {
+			if (classes.indexOf(draggable) !== -1)
+				canDrag = true;
+		});
+		return canDrag;
+	};
+
+	inRange = (event: any) => {
+		const rangeBox = this.getRangeBox(event);
+		const rect = rangeBox.getBoundingClientRect();
+		const direction = rangeBox.dataset.direction;
+		if (direction == 'horizontal') {
+			const min = rangeBox.offsetLeft;
+			const max = min + rangeBox.offsetWidth;
+			if (event.clientX < min || event.clientX > max) return false;
+		} else {
+			const min = rect.top;
+			const max = min + rangeBox.offsetHeight;
+			if (event.clientY < min || event.clientY > max) return false;
+		}
+		return true;
+	};
+
+	updateProgress = () => {
+		const current: number = this.player!.currentTime;
+		const percent = (current / this.player!.duration) * 100;
+		(this.progress as HTMLElement).style.width = percent + '%';
+
+		this.currentTime!.textContent = this.formatTime(current);
+	};
+
+	getRangeBox = (event: any) => {
+		let rangeBox = event.target;
+		const el = this.currentlyDragged;
+		if (event.type == 'click' && this.isDraggable(event.target)) {
+			rangeBox = event.target.parentElement.parentElement;
+		}
+		if (event.type == 'mousemove') {
+			rangeBox = el!.parentElement!.parentElement;
+		}
+		return rangeBox;
+	};
+
+	getCoefficient = (event: any) => {
+		const slider = this.getRangeBox(event);
+		const rect = slider.getBoundingClientRect();
+		let K = 0;
+		if (slider.dataset.direction == 'horizontal') {
+
+			const offsetX = event.clientX - slider.offsetLeft;
+			const width = slider.clientWidth;
+			K = offsetX / width;
+
+		} else if (slider.dataset.direction == 'vertical') {
+
+			const height = slider.clientHeight;
+			const offsetY = event.clientY - rect.top;
+			K = 1 - offsetY / height;
+
+		}
+		return K;
+	};
+
+	rewind = (event: any) => {
+		if (this.inRange(event)) {
+			this.player!.currentTime = this.player!.duration * this.getCoefficient(event);
+		}
+	};
+
+	changeVolume = (event: any) => {
+		if (this.inRange(event)) {
+			this.player!.volume = this.getCoefficient(event);
+		}
+	};
+
+	formatTime = (time: number) => {
+		const min = Math.floor(time / 60);
+		const sec = Math.floor(time % 60);
+		return min + ':' + ((sec < 10) ? ('0' + sec) : sec);
+	};
+
+	togglePlay = () => {
+		if (this.player.paused) {
+			(this.playPause as HTMLElement).setAttribute('d', 'M0 0h6v24H0zM12 0h6v24h-6z');
+			this.player.play();
+		} else {
+			(this.playPause as HTMLElement).setAttribute('d', 'M18 12L0 24V0');
+			this.player.pause();
+		}
+	};
 }
