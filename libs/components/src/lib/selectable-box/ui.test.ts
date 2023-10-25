4711e8ae96a35ac7291365d0913508add13d9c3a
James Taylor <jstaylor@vondlq035x47v.home>
2023-10-25 08:30:58 +0100

    chore: updates ui test

--- libs/components/src/lib/selectable-box/ui.test.ts
+++ libs/components/src/lib/selectable-box/ui.test.ts
@@ -7,7 +7,7 @@ import {
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
 
-const components = ['selectable-box'];
+const components = ['selectable-box', 'checkbox', 'radio', 'layout'];
 
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
