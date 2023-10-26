200e4d2ca5ec0904e82aeab32d3152e71937b476
RTannenbaum <rachel.tannenbaum@vonage.com>
2023-10-26 13:18:42 +0300

    remove use case from docs

--- libs/components/src/lib/checkbox/ui.test.ts
+++ libs/components/src/lib/checkbox/ui.test.ts
@@ -14,9 +14,13 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		path.join(new URL('.', import.meta.url).pathname, 'README.md')
 	).reduce(
 		(htmlString: string, block: string) =>
-			`${htmlString} <div style="margin: 5px;">${block}</div>`,
-		''
-	);
+			`${htmlString} <div style="margin: 5px;">${block}</div>`, ''
+	) + `<form method="post" action="">
+				<vwc-checkbox error-text="You need to accept the Terms of service">
+				<a href="https://www.vonage.com/legal/" target="_blank">Vonage Terms of Service</a>
+				</vwc-checkbox>
+				<vwc-button label="Submit" appearance="filled" type="submit"></vwc-button>
+			</form>`;
 
 	page.setViewportSize({ width: 400, height: 800 });
 
