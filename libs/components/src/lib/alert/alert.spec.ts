b4e516466bba5ec2ac770de9745df44e7704d435
RTannenbaum <rachel.tannenbaum@vonage.com>
2024-02-06 21:59:28 +0200

    code coverage

--- libs/components/src/lib/alert/alert.spec.ts
+++ libs/components/src/lib/alert/alert.spec.ts
@@ -195,8 +195,14 @@ describe('vwc-alert', () => {
 			expect(element.style.display).toEqual('none');
 		});
 		it('should be display contents when is open', async function () {
-			element.open = true;
+			element.open = false;
 			await elementUpdated(element);
+			expect(element.style.display).toEqual('none');
+
+			element.open = true;
+			getControlElement(element).dispatchEvent(new Event('transitionend'));
+			await elementUpdated(element);
+
 			expect(element.style.display).toEqual('contents');
 		});
 	});
