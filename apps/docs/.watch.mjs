a23ead2f0ac6e1cbfdbbd02493951016f5b7057b
Yonatan Kra <me@yonatankra.com>
2024-05-02 21:22:09 +0300

    Add the serial steps watcher

---
+++ apps/docs/.watch.mjs
@@ -0,0 +1,27 @@
+import { watchDependencies } from './multi-dependent-watch.mjs';
+
+function proceduralDepsWatch() {
+	const projects = {
+		"styles": {
+			script: "npx nx run styles:build",
+			dependencies: ["build"],
+			watchPaths: ["./libs/styles/**/*.scss"]
+		},
+		"build": {
+			script: "npx nx run components:build",
+			dependencies: ["bundle"],
+			watchPaths: ["libs/components/src/**/*.(ts|scss)"]
+		},
+		"bundle": {
+			script: "npx esbuild ./dist/libs/components/index.js --bundle --outfile=./dist/libs/components-bundle/index.js --format=esm",
+			dependencies: [],
+			watchPaths: [
+				"./apps/docs/**/*.(html|md|njk|ts)",
+				"libs/components/src/lib/**/README.md"
+			]
+		}
+	};
+	watchDependencies(projects);
+}
+
+proceduralDepsWatch();
