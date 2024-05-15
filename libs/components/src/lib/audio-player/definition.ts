e37cc1a17a389d18e3cba53e8e4b5bff934f02aa
Rachel Bratt Tannenbaum <rachelbt@users.noreply.github.com>
2024-05-15 10:11:24 +0300

    Update libs/components/src/lib/audio-player/definition.ts

--- libs/components/src/lib/audio-player/definition.ts
+++ libs/components/src/lib/audio-player/definition.ts
@@ -2,7 +2,6 @@ import type { FoundationElementDefinition } from '@microsoft/fast-foundation';
 import { registerFactory } from '../../shared/design-system';
 import { buttonRegistries } from '../button/definition';
 import { sliderRegistries } from '../slider/definition';
-import { menuRegistries } from '../menu/definition';
 import styles from './audio-player.scss?inline';
 
 import { AudioPlayer } from './audio-player';
