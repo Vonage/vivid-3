45eb8a6dd98b246a15a3989e20275a1686fa6958
James Taylor <jstaylor@vonht2fg32k90.home>
2024-06-07 11:42:06 +0100

    chore; formatting

--- apps/docs/assets/scripts/skip-top/skip-top.ts
+++ apps/docs/assets/scripts/skip-top/skip-top.ts
@@ -1,24 +1,27 @@
 import './skip-top.style.scss';
 
 function setupSkipTop() {
-  const skipTopBtn = document.getElementById('skip-top');
-  if (!skipTopBtn) return;
-  
-  function handleScroll() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      skipTopBtn!.style.display = 'inline-block';
-    } else {
-      skipTopBtn!.style.display = 'none'
-    }
-  }
+	const skipTopBtn = document.getElementById('skip-top');
+	if (!skipTopBtn) return;
 
-  function skipToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
+	function handleScroll() {
+		if (
+			document.body.scrollTop > 20 ||
+			document.documentElement.scrollTop > 20
+		) {
+			skipTopBtn!.style.display = 'inline-block';
+		} else {
+			skipTopBtn!.style.display = 'none';
+		}
+	}
 
-  skipTopBtn!.addEventListener('click', skipToTop);
-  window.addEventListener('scroll', handleScroll);
+	function skipToTop() {
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+
+	skipTopBtn!.addEventListener('click', skipToTop);
+	window.addEventListener('scroll', handleScroll);
 }
 
 window.addEventListener('DOMContentLoaded', setupSkipTop);
