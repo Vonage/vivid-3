ecbdb2e4813e63c5e206d15419d59b51e57dfda5
James Taylor <146064280+TaylorJ76@users.noreply.github.com>
2024-06-07 16:10:25 +0100

    chore(docs): adds skip button (VIV-1732) (#1753)
    * chore: adds skip button to docs
    
    * chore; formatting
    
    ---------
    
    Co-authored-by: James Taylor <jstaylor@vonht2fg32k90.home>

---
+++ apps/docs/assets/scripts/skip-top/skip-top.ts
@@ -0,0 +1,28 @@
+import './skip-top.style.scss';
+
+function setupSkipTop() {
+	const skipTopBtn = document.getElementById('skip-top');
+	if (!skipTopBtn) return;
+
+	function handleScroll() {
+		if (
+			document.body.scrollTop > 20 ||
+			document.documentElement.scrollTop > 20
+		) {
+			skipTopBtn!.style.display = 'inline-block';
+		} else {
+			skipTopBtn!.style.display = 'none';
+		}
+	}
+
+	function skipToTop() {
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+
+	skipTopBtn!.addEventListener('click', skipToTop);
+	window.addEventListener('scroll', handleScroll);
+}
+
+window.addEventListener('DOMContentLoaded', setupSkipTop);
+window.addEventListener('htmx:afterSwap', () => setTimeout(setupSkipTop, 100));
