6a3e495896c91a079c1f524bea258a7d6fc12ec1
Richard Helm <86777660+RichardHelm@users.noreply.github.com>
2024-06-27 18:36:48 +0100

    feat(eslint-plugin): add no value attribute rules (VIV-1827) (#1785)
    * feat(eslint-plugin): add no value attribute rules (VIV-1827)
    
    * Add to docs
    
    ---------
    
    Co-authored-by: James Taylor <146064280+TaylorJ76@users.noreply.github.com>

---
+++ libs/eslint-plugin/src/rules/no-current-value-attribute.ts
@@ -0,0 +1,155 @@
+import * as utils from 'eslint-plugin-vue/lib/utils';
+import type { Rule } from 'eslint';
+import { normalizeTag } from '../utils/components';
+import { ComponentMetadata } from '../utils/ComponentMetadata';
+import { camelToKebab } from '../utils/casing';
+import { getAttributes } from '../utils/attributes';
+
+const componentsWithCurrentValue = new ComponentMetadata<
+	{
+		currentValueAttr: string;
+		modelValueAttr: string;
+	}[]
+>();
+componentsWithCurrentValue.add('VTextField', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VTextArea', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VNumberField', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VSelect', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VCombobox', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VSlider', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VRangeSlider', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+	{
+		currentValueAttr: 'currentStart',
+		modelValueAttr: 'start',
+	},
+	{
+		currentValueAttr: 'currentEnd',
+		modelValueAttr: 'end',
+	},
+]);
+componentsWithCurrentValue.add('VDatePicker', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VDateRangePicker', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+	{
+		currentValueAttr: 'currentStart',
+		modelValueAttr: 'start',
+	},
+	{
+		currentValueAttr: 'currentEnd',
+		modelValueAttr: 'end',
+	},
+]);
+componentsWithCurrentValue.add('VTimePicker', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VFilePicker', [
+	{
+		currentValueAttr: 'currentValue',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VCheckbox', [
+	{
+		currentValueAttr: 'currentChecked',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VSwitch', [
+	{
+		currentValueAttr: 'currentChecked',
+		modelValueAttr: 'modelValue',
+	},
+]);
+componentsWithCurrentValue.add('VRadio', [
+	{
+		currentValueAttr: 'currentChecked',
+		modelValueAttr: 'modelValue',
+	},
+]);
+
+export const noCurrentValueAttribute: Rule.RuleModule = {
+	meta: {
+		type: 'problem',
+		docs: {
+			description: 'Do not use current value attributes.',
+		},
+		fixable: 'code',
+		schema: [],
+	},
+	create(context) {
+		return utils.defineTemplateBodyVisitor(context, {
+			VElement(node) {
+				componentsWithCurrentValue.forTag(
+					normalizeTag(node.name),
+					(_, currentValueAttrs) => {
+						const attrs = getAttributes(node.startTag);
+
+						for (const currentValueAttr of currentValueAttrs) {
+							const currentValueUsage = attrs.find(
+								(attr) => attr.name === currentValueAttr.currentValueAttr
+							);
+
+							if (currentValueUsage) {
+								context.report({
+									loc: currentValueUsage.node.loc,
+									message: `Use \`${currentValueAttr.modelValueAttr}\` instead of \`${currentValueAttr.currentValueAttr}\` to set the current value.`,
+									fix(fixer) {
+										return fixer.replaceText(
+											currentValueUsage.node,
+											camelToKebab(currentValueAttr.modelValueAttr)
+										);
+									},
+								});
+							}
+						}
+					}
+				);
+			},
+		});
+	},
+};
---
+++ libs/eslint-plugin/src/utils/attributes.ts
@@ -0,0 +1,32 @@
+import { Node } from '../types/vue-eslint-parser';
+import { kebabToCamel } from './casing';
+
+const normalizeAttributeName = (name: string) => kebabToCamel(name);
+
+/**
+ * Returns list of attributes, normalizing the name and handling directives
+ */
+export const getAttributes = (tag: Node): { name: string; node: Node }[] =>
+	tag.attributes.flatMap((attr: Node) => {
+		if (
+			attr.directive &&
+			attr.key.name.name === 'bind' &&
+			attr.key.argument &&
+			attr.key.argument.type === 'VIdentifier'
+		) {
+			return [
+				{
+					name: normalizeAttributeName(attr.key.argument.rawName),
+					node: attr.key.argument,
+				},
+			];
+		} else if (!attr.directive) {
+			return [
+				{
+					name: normalizeAttributeName(attr.key.rawName),
+					node: attr.key,
+				},
+			];
+		}
+		return [];
+	});
