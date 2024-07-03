6dbc84c55fc170e8fd2dc79a7bdfb71142e28e4f
Richard Helm <richard.helm@vonage.com>
2024-07-03 11:41:15 +0100

    Start fleshing out range-slider-thumb

--- libs/components/src/lib/range-slider/range-slider-thumb.ts
+++ libs/components/src/lib/range-slider/range-slider-thumb.ts
@@ -1,8 +1,17 @@
-import { FASTElement, html, when, ViewTemplate } from '@microsoft/fast-element';
-import { classNames, Orientation } from '@microsoft/fast-web-utilities';
+import {
+	FASTElement,
+	html,
+	observable,
+	ref,
+	ViewTemplate,
+	when,
+} from '@microsoft/fast-element';
+import { classNames } from '@microsoft/fast-web-utilities';
 import type { ElementDefinitionContext } from '@microsoft/fast-foundation';
 import { PlacementStrategy, Popup } from '../popup/popup';
+import type { ThumbId } from './range-slider';
 
+// TODO: take care of visibilyFocusedThumb
 const getThumbClassesFor =
 	(thumb: ThumbId) =>
 	({ _visiblyFocusedThumb }: RangeSliderThumb) =>
@@ -14,45 +23,44 @@ const getThumbClassesFor =
 export const RangeSliderThumbTemplate: (
 	context: ElementDefinitionContext
 ) => ViewTemplate<RangeSliderThumb> = (context) => {
-  const popupTag = context.tagFor(Popup);
-  return html`
-  <div
-      id="start-thumb"
-      class="${getThumbClassesFor('start')}"
-      style="${(x) => x._startThumbCss}"
-      role="slider"
-      tabindex="${(x) => (x.disabled ? null : 0)}"
-      aria-label="${(x) =>
-        x.ariaStartLabel || x.locale.rangeSlider.startThumbLabel}"
-      aria-valuetext="${(x) => x.valueTextFormatter(x.start)}"
-      aria-valuenow="${(x) => x.start}"
-      aria-valuemin="${(x) => x.min}"
-      aria-valuemax="${(x) => x.end}"
-      aria-disabled="${(x) => x.disabled}"
-      aria-orientation="${(x) => x.orientation}"
-    ></div>
-    ${when(
-      (x) => x.pin,
-      html<RangeSliderThumb>`<${popupTag}
-      class="popup"
-      id="start-popup"
+	const popupTag = context.tagFor(Popup);
+	return html`
+		<div
+			${ref('thumb')}
+			class="${getThumbClassesFor('start')}"
+			style="${(x) => x._startThumbCss}"
+			role="slider"
+			tabindex="${(x) => (x.disabled ? null : 0)}"
+			aria-label="${(x) =>
+				x.ariaStartLabel || x.locale.rangeSlider.startThumbLabel}"
+			aria-valuetext="${(x) => x.valueTextFormatter(x.start)}"
+			aria-valuenow="${(x) => x.start}"
+			aria-valuemin="${(x) => x.min}"
+			aria-valuemax="${(x) => x.end}"
+			aria-disabled="${(x) => x.disabled}"
+			aria-orientation="${(x) => x.orientation}"
+		></div>
+		${when(
+			(x) => x.pin,
+			html<RangeSliderThumb>`<${popupTag}
+      class='popup'
+      ${ref('popup')}
       arrow
       alternate
       :open=${(x) => x._isThumbPopupOpen('start')}
-      :placementStrategy=${(x) =>
-        x.orientation === Orientation.horizontal
-          ? PlacementStrategy.AutoPlacementHorizontal
-          : PlacementStrategy.AutoPlacementVertical}
+      :placementStrategy=${(x) => x.placementStrategy}
       animation-frame
       exportparts="vvd-theme-alternate"
       aria-hidden="true"
     >
       <div class="tooltip">${(x) => x.valueTextFormatter(x.start)}</div>
     </${popupTag}>`
-    )}
-  `;
-
+		)}
+	`;
+};
 
 export class RangeSliderThumb extends FASTElement {
-
+	thumb: HTMLDivElement | null = null;
+	popup: HTMLDivElement | null = null;
+	@observable placementStrategy: PlacementStrategy;
 }
