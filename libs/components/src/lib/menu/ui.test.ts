b645dc027f73b8263f0323f8c4b0939187de3827
RTannenbaum <rachel.tannenbaum@vonage.com>
2024-09-25 11:39:12 +0300

    split the ui tests

--- libs/components/src/lib/menu/ui.test.ts
+++ libs/components/src/lib/menu/ui.test.ts
@@ -7,18 +7,59 @@ import {
 
 const components = ['menu', 'menu-item', 'button', 'text-field'];
 
+async function testAbsolutStrategy ({ page }: { page: Page }) {
+	const template = `
+<style>
+			.wrapper {
+				width: 100%;
+				height: 700px;
+				position: relative;
+			}
+		</style>
+		<div class="wrapper"><div style="container-type: inline-size">
+ <vwc-menu id="menu-1" placement="right-start" open strategy-absolute>
+  <vwc-button slot="anchor" label="Toggle Menu" appearance="outlined"></vwc-button>
+  <vwc-menu-item text="Menu item 1"></vwc-menu-item>
+  <vwc-menu-item text="Menu item 2"></vwc-menu-item>
+  <vwc-menu-item text="Menu item 3"></vwc-menu-item>
+ </vwc-menu>
+ </div></div>`;
+
+	page.setViewportSize({ width: 500, height: 400 });
+
+	await loadComponents({
+		page,
+		components,
+	});
+	await loadTemplate({
+		page,
+		template,
+	});
+
+	const testWrapper = await page.$('#wrapper');
+
+	await page.waitForLoadState('networkidle');
+
+	expect(await testWrapper?.screenshot()).toMatchSnapshot(
+		'./snapshots/absolute-menu.png',
+		{ maxDiffPixelRatio: 0.01 }
+	);
+
+}
+
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = `
 		<style>
 			.wrapper {
 				width: 100%;
-				height: 400px;
+				height: 700px;
+				position: relative;
 			}
 		</style>
 
 		<div class="wrapper">
 			<vwc-menu id="menu" placement="right-start" open>
-				<vwc-button slot="anchor" label="Toggle Menu" appearance="outlined"></vwc-button>
+				<vwc-button slot="anchor" label="Toggle Menu" onclick="menu.open = !menu.open" appearance="outlined"></vwc-button>
 				<vwc-text-field slot="header" placeholder="Search" icon="search"></vwc-text-field>
 				<vwc-menu-item text="Lorem ipsum dolor sit amet, consectetur adipisicing elit"></vwc-menu-item>
 				<vwc-menu-item text="Menu item 1"></vwc-menu-item>
@@ -28,19 +69,9 @@ test('should show the component', async ({ page }: { page: Page }) => {
 				<vwc-button slot="action-items" appearance="outlined" label="Close"></vwc-button>
 				<vwc-button slot="action-items" appearance="filled" label="Select"></vwc-button>
 			</vwc-menu>
-		</div>
-		<div class="wrapper">
-		<div style="container-type: inline-size">
-			<vwc-menu id="menu-1" placement="right-start" open strategy-absolute>
-				<vwc-button slot="anchor" label="Toggle Menu" appearance="outlined"></vwc-button>
-				<vwc-menu-item text="Menu item 1"></vwc-menu-item>
-				<vwc-menu-item text="Menu item 2"></vwc-menu-item>
-				<vwc-menu-item text="Menu item 3"></vwc-menu-item>
-			</vwc-menu>
-			</div>
 		</div>`;
 
-	page.setViewportSize({ width: 720, height: 800 });
+	page.setViewportSize({ width: 720, height: 720 });
 
 	await loadComponents({
 		page,
@@ -59,3 +90,5 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		'./snapshots/menu.png'
 	);
 });
+
+test('menu with absolute strategy', testAbsolutStrategy);
