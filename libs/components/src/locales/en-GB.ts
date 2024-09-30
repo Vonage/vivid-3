91fae26245fcce100b1e9fd434dc41ea20a61cb2
James Taylor <jstaylor@vonht2fg32k90.home>
2024-09-30 10:36:56 +0100

    chore: resolves conflicts

--- libs/components/src/locales/en-GB.ts
+++ libs/components/src/locales/en-GB.ts
@@ -138,17 +138,15 @@ const enGB: Locale = {
 		digitZeroLabel: '0',
 		digitHashtagLabel: '#',
 	},
-<<<<<<< HEAD
 	tab: {
 		dismissButtonLabel: 'Press the DELETE key to close this tab',
-=======
+	},
 	searchableSelect: {
 		clearButtonLabel: 'Clear',
 		noOptionsMessage: 'No options',
 		noMatchesMessage: 'No options found',
 		removeTagButtonLabel: /* istanbul ignore next */ (label: string) =>
 			`Remove ${label}`,
->>>>>>> main
 	},
 };
 
