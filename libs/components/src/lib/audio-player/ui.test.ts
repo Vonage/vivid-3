9936433c1a2974204baa4227019fff5c13b0f9c7
RTannenbaum <rachel.tannenbaum@vonage.com>
2024-10-14 15:50:35 +0300

    change menu position-strategy

--- libs/components/src/lib/audio-player/ui.test.ts
+++ libs/components/src/lib/audio-player/ui.test.ts
@@ -9,6 +9,47 @@ import {
 
 const components = ['audio-player'];
 
+async function testMenuOpen({ page }: { page: Page }) {
+	const template = `
+<style>
+			.wrapper {
+				width: 100%;
+				height: 700px;
+				position: relative;
+			}
+		</style>
+<vwc-audio-player
+src="https://download.samplelib.com/mp3/sample-6s.mp3"
+playback-rates="0.5, 1, 1.5, 2"
+>
+</vwc-audio-player>
+ </div></div>`;
+
+	page.setViewportSize({ width: 600, height: 400 });
+
+	await loadComponents({
+		page,
+		components,
+	});
+	await loadTemplate({
+		page,
+		template,
+	});
+
+	const testWrapper = await page.$('#wrapper');
+
+	await page.waitForLoadState('networkidle');
+	await page.waitForTimeout(500);
+
+	const pressedPlayback = await page.locator('#playback-open-button');
+	await pressedPlayback.click();
+
+	expect(await testWrapper?.screenshot()).toMatchSnapshot(
+		'./snapshots/open-playback-menu.png',
+		{ maxDiffPixelRatio: 0.01 }
+	);
+}
+
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = extractHTMLBlocksFromReadme(
 		path.join(new URL('.', import.meta.url).pathname, 'README.md')
@@ -37,3 +78,5 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		'./snapshots/audio-player.png'
 	);
 });
+
+test('audio with open menu for playback rates', testMenuOpen);
