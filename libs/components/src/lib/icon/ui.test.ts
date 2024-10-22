3342ab118807bb79b91c5ccd14ca0c661ab77272
Richard Helm <86777660+RichardHelm@users.noreply.github.com>
2024-10-22 10:14:44 +0100

    chore(docs): update icon docs to new format (VIV-2099) (#1952)
    * Update Icon docs to new format
    
    * Move style to end
    
    * Move custom color & size to code section
    
    * Remove irrelevant events
    
    * Update UI tests
    
    ---------
    
    Co-authored-by: James Taylor <146064280+TaylorJ76@users.noreply.github.com>

--- libs/components/src/lib/icon/ui.test.ts
+++ libs/components/src/lib/icon/ui.test.ts
@@ -9,9 +9,14 @@ import {
 
 const components = ['icon'];
 test('should show the component', async ({ page }: { page: Page }) => {
-	const template = extractHTMLBlocksFromReadme(
-		path.join(new URL('.', import.meta.url).pathname, 'README.md')
-	).reduce(
+	const template = [
+		...extractHTMLBlocksFromReadme(
+			path.join(new URL('.', import.meta.url).pathname, 'VARIATIONS.md')
+		),
+		...extractHTMLBlocksFromReadme(
+			path.join(new URL('.', import.meta.url).pathname, 'README.md')
+		),
+	].reduce(
 		(htmlString: string, block: string) =>
 			`${htmlString} <div style="margin: 5px;">${block}</div>`,
 		''
