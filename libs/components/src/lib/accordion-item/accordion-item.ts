6031323a6de35284bca31f0952cec4a13b297c1a
TaylorJ76 <james.taylor4@vonage.com>
2024-11-08 14:21:34 +0000

    chore: wip

--- libs/components/src/lib/accordion-item/accordion-item.ts
+++ libs/components/src/lib/accordion-item/accordion-item.ts
@@ -1,10 +1,24 @@
-import { attr } from '@microsoft/fast-element';
+import { attr, nullableNumberConverter } from '@microsoft/fast-element';
+import type { SyntheticViewTemplate } from '@microsoft/fast-element';
 import {
 	applyMixins,
-	AccordionItem as FASTAccordionItem,
+	FoundationElement,
 } from '@microsoft/fast-foundation';
+import type { FoundationElementDefinition } from '@microsoft/fast-foundation';
+import { StartEnd } from "../../shared/foundation/patterns/start-end";
+import type { StartEndOptions } from "../../shared/foundation/patterns/start-end";
 import { AffixIconWithTrailing } from '../../shared/patterns/affix';
 import type { Size } from '../enums.js';
+
+/**
+ * Accordion Item configuration options
+ * @public
+ */
+export type AccordionItemOptions = FoundationElementDefinition &
+    StartEndOptions & {
+        expandedIcon?: string | SyntheticViewTemplate;
+        collapsedIcon?: string | SyntheticViewTemplate;
+    };
 
 /**
  * Types of accordion size.
@@ -20,7 +34,44 @@ export type AccordionItemSize = Extract<Size, Size.Condensed | Size.Normal>;
  * @slot icon - Add an icon to the component.
  * @event {CustomEvent<undefined>} change - Fires a custom 'change' event when the button is invoked
  */
-export class AccordionItem extends FASTAccordionItem {
+export class AccordionItem extends FoundationElement {
+	/**
+	 * Configures the {@link https://www.w3.org/TR/wai-aria-1.1/#aria-level | level} of the
+	 * heading element.
+	 *
+	 * @defaultValue 2
+	 * @public
+	 * @remarks
+	 * HTML attribute: heading-level
+	 */
+	@attr({
+		attribute: "heading-level",
+		mode: "fromView",
+		converter: nullableNumberConverter,
+	})
+	headinglevel: 1 | 2 | 3 | 4 | 5 | 6 = 2;
+
+	/**
+	 * Expands or collapses the item.
+	 *
+	 * @public
+	 * @remarks
+	 * HTML attribute: expanded
+	 */
+	@attr({ mode: "boolean" })
+	expanded = false;
+
+	/**
+	 * The item ID
+	 *
+	 * @public
+	 * @remarks
+	 * HTML Attribute: id
+	 */
+	@attr
+	// @ts-expect-error Type is incorrectly non-optional
+	id: string;
+
 	/**
 	 *
 	 *
@@ -54,7 +105,25 @@ export class AccordionItem extends FASTAccordionItem {
 	 * HTML Attribute: size
 	 */
 	@attr size?: AccordionItemSize;
+
+	/**
+	 * @internal
+	 */
+	// @ts-expect-error Type is incorrectly non-optional
+	expandbutton: HTMLElement;
+
+	/**
+	 * @internal
+	 */
+	clickHandler = (e: MouseEvent) => {
+		this.expanded = !this.expanded;
+		this.change();
+	};
+
+	private change = (): void => {
+			this.$emit("change");
+	};
 }
 
-export interface AccordionItem extends AffixIconWithTrailing {}
-applyMixins(AccordionItem, AffixIconWithTrailing);
+export interface AccordionItem extends AffixIconWithTrailing, StartEnd {}
+applyMixins(AccordionItem, AffixIconWithTrailing, StartEnd);
