db99adbbfebd7014124c2f7187eeab28da50a3a7
TaylorJ76 <james.taylor4@vonage.com>
2024-11-13 14:46:45 +0000

    chore: formating

--- libs/components/src/lib/switch/switch.form-associated.ts
+++ libs/components/src/lib/switch/switch.form-associated.ts
@@ -1,4 +1,4 @@
-import { FormAssociated, FoundationElement, } from '@microsoft/fast-foundation';
+import { FormAssociated, FoundationElement } from '@microsoft/fast-foundation';
 
 class _Switch extends FoundationElement {}
 /* eslint-disable-next-line @typescript-eslint/naming-convention */
