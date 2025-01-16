27811d8abfd72bc9af2ab1f31c16966a741ae4c6
TaylorJ76 <james.taylor4@vonage.com>
2025-01-16 10:27:36 +0000

    chore: updates ui tests

--- libs/components/src/lib/audio-player/ui.test.ts
+++ libs/components/src/lib/audio-player/ui.test.ts
@@ -1,8 +1,6 @@
-import * as path from 'path';
 import { expect, test } from '@playwright/test';
 import type { Page } from '@playwright/test';
 import {
-	extractHTMLBlocksFromReadme,
 	loadComponents,
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
@@ -52,15 +50,83 @@ playback-rates="0.5, 1, 1.5, 2"
 }
 
 test('should show the component', async ({ page }: { page: Page }) => {
-	const template = extractHTMLBlocksFromReadme(
-		path.join(new URL('.', import.meta.url).pathname, 'README.md')
-	).reduce(
-		(htmlString: string, block: string) =>
-			`${htmlString} <div style="margin: 5px;">${block}</div>`,
-		''
-	);
+	const template = `<div style="margin: 5px;">
+		<vwc-audio-player 
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-audio-player 
+			notime
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-audio-player 
+			skip-by="5"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+		<vwc-audio-player 
+			skip-by="10"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+		<vwc-audio-player 
+			skip-by="30"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>
+	<div style="margin: 5px; width: 500px">
+		<vwc-audio-player 
+			skip-by="5"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+		<vwc-audio-player 
+			skip-by="10"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+		<vwc-audio-player 
+			skip-by="30"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-audio-player 
+			playback-rates="0.5, 1, 1.5, 2"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>
+	<div style="margin: 5px; width: 500px">
+		<vwc-audio-player 
+			playback-rates="0.5, 1, 1.5, 2"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-audio-player 
+			connotation="cta"
+			playback-rates="0.75, 1, 1.5"
+			skip-by="5"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>
+	<div style="margin: 5px;">
+		<vwc-audio-player 
+			disabled
+			playback-rates="0.75, 1, 1.5"
+			skip-by="5"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>
+	<div style="margin: 5px; width: 350px;">
+		<vwc-audio-player
+			playback-rates="0.75, 1, 1.5"
+			skip-by="5"
+			class="audio-player"
+			src="https://download.samplelib.com/mp3/sample-6s.mp3"
+		></vwc-audio-player>
+	</div>`
 
-	page.setViewportSize({ width: 500, height: 1000 });
+	page.setViewportSize({ width: 800, height: 1000 });
 
 	await loadComponents({
 		page,
