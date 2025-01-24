1a08d64cebf741135f1bc38a04df8a8f81bee41f
TaylorJ76 <james.taylor4@vonage.com>
2025-01-24 14:35:27 +0000

    chore: roles back a11y fix

--- libs/components/src/lib/divider/divider.template.ts
+++ libs/components/src/lib/divider/divider.template.ts
@@ -1,15 +1,12 @@
 import { html } from '@microsoft/fast-element';
 import { classNames } from '@microsoft/fast-web-utilities';
-import { type Divider, DividerRole } from './divider';
+import type { Divider } from './divider';
 
 const getClasses = ({ orientation }: Divider) =>
 	classNames('base', [`${orientation}`, Boolean(orientation)]);
 
-const getAriaOrientation = ({ role, orientation }: Divider) =>
-	role === DividerRole.presentation ? null : orientation;
-
 export const DividerTemplate = html<Divider>` <span
 	class="${getClasses}"
-	aria-orientation="${getAriaOrientation}"
+	orientation="${(x) => x.orientation}"
 	role="${(x) => x.role}"
 ></span>`;
--- libs/components/src/lib/divider/divider.ts
+++ libs/components/src/lib/divider/divider.ts
@@ -6,7 +6,7 @@ import { VividElement } from '../../shared/foundation/vivid-element/vivid-elemen
  * Divider roles
  * @public
  */
-export const DividerRole = {
+const DividerRole = {
 	/**
 	 * The divider semantically separates content
 	 */
