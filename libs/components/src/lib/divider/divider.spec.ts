095ae8ea6c70e3b8e1d9f9b38c82c1ecdede9d19
James Taylor <146064280+TaylorJ76@users.noreply.github.com>
2025-01-27 15:45:28 +0000

    Update libs/components/src/lib/divider/divider.spec.ts
    Co-authored-by: Rachel Bratt Tannenbaum <rachelbt@users.noreply.github.com>

--- libs/components/src/lib/divider/divider.spec.ts
+++ libs/components/src/lib/divider/divider.spec.ts
@@ -29,7 +29,7 @@ describe('vwc-divider', () => {
 			expect(base?.classList.contains(`${orientation}`)).toBeTruthy();
 		});
 
-		it('should set the aria-orientation class if role is separator', async () => {
+		it('should set the aria-orientation attribute if role is separator', async () => {
 			const base = element.shadowRoot?.querySelector('.base');
 			const orientation = 'vertical';
 			element.orientation = orientation;
