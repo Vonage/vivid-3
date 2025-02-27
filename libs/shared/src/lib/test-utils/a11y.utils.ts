ade97e29db5f57eb698eb8b969997826ddf45418
Yonatan Kra <me@yonatankra.com>
2025-02-27 08:30:50 +0200

    Make the a11y util on demand to be used only in a11y tests

---
+++ libs/shared/src/lib/test-utils/a11y.utils.ts
@@ -0,0 +1,12 @@
+export const axe = async function(element: any) {
+    const { configureAxe } = await import('vitest-axe');
+    let axeInstance = configureAxe({
+        rules: {
+            // color contrast doesn't work in this env
+            'color-contrast': { enabled: false },
+            // stops the HTML provided from being treated as a whole page
+            region: { enabled: false },
+        },
+    });
+    return axeInstance(element);
+}
--- libs/shared/src/lib/test-utils/index.ts
+++ libs/shared/src/lib/test-utils/index.ts
@@ -1,4 +1,3 @@
-import { configureAxe } from 'vitest-axe';
 
 export const elementUpdated = async (element: Element | HTMLElement) => {
 	return new Promise((resolve) =>
@@ -41,14 +40,7 @@ export async function setProperty<T extends Element, P extends keyof T>(
 	await elementUpdated(element);
 }
 
-export const axe = configureAxe({
-	rules: {
-		// color contrast doesn't work in this env
-		'color-contrast': { enabled: false },
-		// stops the HTML provided from being treated as a whole page
-		region: { enabled: false },
-	},
-});
+
 
 export * from './form-association';
 export * from './delegatesFocusPolyfill';
