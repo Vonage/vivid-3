fccd57d470c97933aa5bb04a5f1a05a0acd959cb
Richard Helm <richard.helm@vonage.com>
2025-03-05 12:16:40 +0000

    Expose current version

--- libs/components/src/shared/foundation/vivid-element/vivid-element.ts
+++ libs/components/src/shared/foundation/vivid-element/vivid-element.ts
@@ -3,4 +3,10 @@ import { FASTElement } from '@microsoft/fast-element';
 /**
  * Base class for all Vivid elements.
  */
-export class VividElement extends FASTElement {}
+export class VividElement extends FASTElement {
+	/**
+	 * The current version of the Vivid library, which is useful for debugging.
+	 * It can be accessed from any Vivid element via `<el>.constructor.VIVID_VERSION`.
+	 */
+	static VIVID_VERSION = __PACKAGE_VERSION__;
+}
--- libs/components/src/types/vite.d.ts
+++ libs/components/src/types/vite.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+declare const __PACKAGE_VERSION__: string;
