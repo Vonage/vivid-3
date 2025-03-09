c922bbbeac16451f4729f3246c4bb866f2b437e7
Yonatan Kra <yonatan.kra@vonage.com>
2025-03-09 14:33:33 +0200

    feat(rich-text-editor): basic visual interface (VIV-2135) (#2196)
    * chore(rich-text-editor): use prose mirror in the RTE component
    * feat(rich-text-editor): perliminary styles (css)
    ---------
    
    Co-authored-by: Yonatan Kra <me@yonatankra.com>
    Co-authored-by: RTannenbaum <rachel.tannenbaum@vonage.com>

--- libs/components/src/lib/rich-text-editor/rich-text-editor.ts
+++ libs/components/src/lib/rich-text-editor/rich-text-editor.ts
@@ -1,11 +1,12 @@
-import { FoundationElement } from '@microsoft/fast-foundation';
 import { attr } from '@microsoft/fast-element';
+import { VividElement } from '../../shared/foundation/vivid-element/vivid-element';
+import { ProseMirrorFacade } from './facades/vivid-prose-mirror.facade';
 
 /**
  * @public
  * @component rich-text-editor
  */
-export class RichTextEditor extends FoundationElement {
+export class RichTextEditor extends VividElement {
 	/**
 	 * Indicates the rich text editor's value.
 	 *
@@ -15,8 +16,28 @@ export class RichTextEditor extends FoundationElement {
 	 */
 	@attr value?: string;
 
+	#editor?: ProseMirrorFacade;
+
+	valueChanged(_: string, newValue: string): void {
+		if (this.#editor) {
+			this.#editor.replaceContent(newValue);
+		}
+	}
+
+	get #editorWrapperElement(): HTMLElement {
+		return this.shadowRoot!.querySelector('#editor') as HTMLElement;
+	}
+
 	constructor() {
 		super();
 		this.value = '';
+	}
+
+	override connectedCallback(): void {
+		super.connectedCallback();
+		if (!this.#editor) {
+			this.#editor = new ProseMirrorFacade();
+			this.#editor.init(this.#editorWrapperElement);
+		}
 	}
 }
