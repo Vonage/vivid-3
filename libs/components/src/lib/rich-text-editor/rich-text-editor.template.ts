6ce221d2dfa3c8ed01a6ecc2905c8df85f59eee3
Yonatan Kra <me@yonatankra.com>
2025-03-25 15:24:06 +0200

    chore(rich-text-editor): editor menu text-size-selected handling

--- libs/components/src/lib/rich-text-editor/rich-text-editor.template.ts
+++ libs/components/src/lib/rich-text-editor/rich-text-editor.template.ts
@@ -1,11 +1,26 @@
 import { html } from '@microsoft/fast-element';
-import type { ViewTemplate } from '@microsoft/fast-element';
+import type { ExecutionContext, ViewTemplate } from '@microsoft/fast-element';
 import { classNames } from '@microsoft/fast-web-utilities';
 import type { VividElementDefinitionContext } from '../../shared/design-system/defineVividComponent';
-import { RichTextEditor } from './rich-text-editor';
+import { RichTextEditor, RichTextEditorTextSizes } from './rich-text-editor';
 
 const getClasses = (_: RichTextEditor) => classNames('control');
 
+const VALID_MENU_ELEMEMENT_SUFFIX = 'menubar';
+
+function textSizeSelectedHandler(this: RichTextEditor, event: CustomEvent<string>) {
+	this.setTextSize(event.detail as RichTextEditorTextSizes);
+}
+
+function handleMenuBarSlotChange(richTextEditor: RichTextEditor, { event }: ExecutionContext) {
+	const slot = event.target as HTMLSlotElement;
+	const assignedElements = slot.assignedElements({ flatten: true });
+	const menuBar = assignedElements.find((element) => element.tagName.toLowerCase().endsWith(VALID_MENU_ELEMEMENT_SUFFIX));
+	assignedElements.forEach((element) => {
+		(element as HTMLElement).style.display = element === menuBar ? 'block' : 'none';
+	});
+	menuBar?.addEventListener('text-size-selected', textSizeSelectedHandler.bind(richTextEditor));
+}
 /**
  * The template for the RichTextEditor component.
  *
@@ -16,6 +31,10 @@ export const RichTextEditorTemplate: (
 	context: VividElementDefinitionContext
 ) => ViewTemplate<RichTextEditor> = (_: VividElementDefinitionContext) => {
 	return html`<template class="${getClasses}">
-		<div id="editor"></div>
+		<div id="editor">
+
+			
+		</div>
+		<slot name="menu-bar" @slotchange="${handleMenuBarSlotChange}"></slot>
 	</template>`;
 };
