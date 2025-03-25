a097168bb224969442941b1d25a3e6b19b41a4ba
Richard Helm <86777660+RichardHelm@users.noreply.github.com>
2025-03-25 09:50:08 +0000

    chore(cem-plugins): move cem plugins into own package (VIV-2359) (#2219)
    * Move improved mixin support into new package
    
    * Move vivid component plugin into new package
    
    * Move css properties plugin into new package
    
    * Small improvements
    
    * Don't lint fixtures
    
    * Fix formatting
    
    * Use describe/it instead of test

---
+++ libs/cem-analyzer-plugins/src/css-properties/css-properties.spec.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as glob from 'glob';
+import * as ts from '@custom-elements-manifest/analyzer/node_modules/typescript';
+import { create } from '@custom-elements-manifest/analyzer/src/create.js';
+import { cssPropertiesPlugin } from './css-properties';
+
+const fixturesPath = path.join(__dirname, '__fixtures__');
+const testCases = fs.readdirSync(fixturesPath);
+
+describe('cssPropertiesPlugin', () => {
+	it.each(testCases)(
+		`should produce correct results for testcase %s`,
+		async (testCase) => {
+			const moduleFiles = glob.sync(
+				path.join(fixturesPath, testCase, '/**/*.{ts,js}')
+			);
+			const modules = moduleFiles.map((moduleFile) =>
+				ts.createSourceFile(
+					path.basename(moduleFile),
+					fs.readFileSync(moduleFile, 'utf-8'),
+					ts.ScriptTarget.ES2015,
+					true
+				)
+			);
+			process.chdir(path.join(fixturesPath, testCase));
+
+			expect(
+				create({ modules, plugins: [cssPropertiesPlugin()] })
+			).toMatchSnapshot();
+		}
+	);
+});
