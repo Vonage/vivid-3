41a338a3c16d89211a43fadc3df3ff3a248b08e1
Richard Helm <richard.helm@vonage.com>
2025-03-31 15:00:19 +0100

    Document utility

--- libs/components/src/shared/utils/enums.ts
+++ libs/components/src/shared/utils/enums.ts
@@ -1,1 +1,6 @@
+/**
+ * Extracts a subset of enum members from an enum and returns a union of their string literal values.
+ * Usage: type MyShape = ExtractFromEnum<Shape, Shape.Rounded | Shape.Pill>;
+ * MyShape will have the type 'rounded' | 'pill'
+ */
 export type ExtractFromEnum<T, U extends string> = `${Extract<T, U>}`;
