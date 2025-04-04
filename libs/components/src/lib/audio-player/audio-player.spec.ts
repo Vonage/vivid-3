460e399321350f30222cf6a4b19a9c7b0dfbcc00
Richard Helm <richard.helm@vonage.com>
2025-04-04 16:06:27 +0100

    Fix lint error

--- libs/components/src/lib/audio-player/audio-player.spec.ts
+++ libs/components/src/lib/audio-player/audio-player.spec.ts
@@ -4,9 +4,9 @@ import { Button } from '../button/button';
 import { Slider } from '../slider/slider';
 import { DEFAULT_PLAYBACK_RATES } from '../video-player/video-player';
 import { MenuItem } from '../menu-item/menu-item';
+import type { Menu } from '../menu/menu';
 import { AudioPlayer } from './audio-player';
 import '.';
-import type { Menu } from '../menu/menu';
 
 const COMPONENT_TAG = 'vwc-audio-player';
 
