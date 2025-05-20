9dafec6340e489d4a52f0b298835921e15a4ac65
Uladzislau Pakosenka <uladzislau.pakosenka@vonage.com>
2025-05-20 08:50:50 +0200

    VIV-2653 added focus method and autofocus property (dial-pad)

--- libs/components/src/lib/dial-pad/ui.test.ts
+++ libs/components/src/lib/dial-pad/ui.test.ts
@@ -10,80 +10,118 @@ import {
 
 const components = ['dial-pad'];
 
-test('should show the component', async ({ page }: { page: Page }) => {
-	const template = `
-		<div style="margin: 5px;">
-			<vwc-dial-pad></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad value="1234567890"></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad
-				helper-text="58 Meeting Room - Extension"
-				value="4734"
-			></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad placeholder="Enter a phone number"></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad disabled></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad call-active value="01146869483"></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad no-call></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad no-input></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad call-button-label="Dial"></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad call-active end-call-button-label="End"></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad pending></vwc-dial-pad>
-		</div>
-		<div style="margin: 5px;">
-			<vwc-dial-pad
-				placeholder="Only digits"
-				pattern="^[0-9]*$"
-				error-text="The input is invalid"
-			></vwc-dial-pad>
-		</div>
-	`;
+test.describe('dial-pad', () => {
+	test('should show the component', async ({ page }: { page: Page }) => {
+		const template = `
+			<div style="margin: 5px;">
+				<vwc-dial-pad></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad value="1234567890"></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad
+					helper-text="58 Meeting Room - Extension"
+					value="4734"
+				></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad placeholder="Enter a phone number"></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad disabled></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad call-active value="01146869483"></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad no-call></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad no-input></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad call-button-label="Dial"></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad call-active end-call-button-label="End"></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad pending></vwc-dial-pad>
+			</div>
+			<div style="margin: 5px;">
+				<vwc-dial-pad
+					placeholder="Only digits"
+					pattern="^[0-9]*$"
+					error-text="The input is invalid"
+				></vwc-dial-pad>
+			</div>
+		`;
 
-	await loadComponents({
-		page,
-		components,
+		await loadComponents({
+			page,
+			components,
+		});
+		await loadTemplate({
+			page,
+			template,
+		});
+
+		const testWrapper = await page.$('#wrapper');
+
+		await page.evaluate(() => {
+			const pendingDialPad = document.querySelector(
+				'vwc-dial-pad[pending]'
+			) as DialPad;
+			const pendingButton = pendingDialPad.shadowRoot?.querySelector(
+				'vwc-button[pending]'
+			) as Button;
+			const indicator = pendingButton.shadowRoot?.querySelector(
+				'vwc-progress-ring'
+			) as ProgressRing;
+			if (indicator) indicator.value = 50;
+		});
+
+		await page.waitForLoadState('networkidle');
+
+		expect(await testWrapper?.screenshot()).toMatchSnapshot(
+			'snapshots/dial-pad-01-default.png'
+		);
 	});
-	await loadTemplate({
-		page,
-		template,
+
+	[
+		['', false],
+		['and hidden text-field', true],
+	].forEach(([s, noInput], index) => {
+		test(`with autofocus ${s}`, async ({ page }: { page: Page }) => {
+			const template = `
+				<div style="margin: 5px;">
+					<vwc-dial-pad 
+						value="1195"
+						autofocus
+ 						${noInput ? 'no-input' : ''} >
+ 					</vwc-dial-pad>
+				</div>
+			`;
+
+			await loadComponents({
+				page,
+				components,
+			});
+			await loadTemplate({
+				page,
+				template,
+			});
+
+			const testWrapper = await page.$('#wrapper');
+
+			await page.waitForLoadState('networkidle');
+
+			expect(await testWrapper?.screenshot()).toMatchSnapshot(
+				`snapshots/dial-pad-02-0${index + 1}-with-autofocus${
+					s ? '-' + s : ''
+				}.png`
+			);
+		});
 	});
-
-	const testWrapper = await page.$('#wrapper');
-
-	await page.evaluate(() => {
-		const pendingDialPad = document.querySelector(
-			'vwc-dial-pad[pending]'
-		) as DialPad;
-		const pendingButton = pendingDialPad.shadowRoot?.querySelector(
-			'vwc-button[pending]'
-		) as Button;
-		const indicator = pendingButton.shadowRoot?.querySelector(
-			'vwc-progress-ring'
-		) as ProgressRing;
-		if (indicator) indicator.value = 50;
-	});
-
-	await page.waitForLoadState('networkidle');
-
-	expect(await testWrapper?.screenshot()).toMatchSnapshot(
-		'snapshots/dial-pad.png'
-	);
 });
