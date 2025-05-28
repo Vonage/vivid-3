8524663b6c5a989a2c267ff1afa11d9a152e19eb
Jakub Hajduk <jakub.hajduk@vonage.com>
2025-05-28 17:48:54 +0200

    fix(wrapper-gen): remove console.log

--- libs/wrapper-gen/src/vueWrappers/vuePropTypes.ts
+++ libs/wrapper-gen/src/vueWrappers/vuePropTypes.ts
@@ -52,7 +52,6 @@ const toVuePropType = (type: TypeRef) => {
 		case 'void':
 			return 'null as unknown'; // will pass any validation
 		default:
-			console.log(type);
 			throw new Error(`Unknown type ${type}`);
 	}
 };
