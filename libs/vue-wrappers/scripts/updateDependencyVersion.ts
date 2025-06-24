5970ac4b3f6ffbee945cb5bd82dff214188766df
Richard Helm <richard.helm@vonage.com>
2025-06-24 21:49:01 +0100

    Fix publish

--- libs/vue-wrappers/scripts/updateDependencyVersion.ts
+++ libs/vue-wrappers/scripts/updateDependencyVersion.ts
@@ -1,19 +1,18 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+const dirname = path.dirname(new URL(import.meta.url).pathname);
+
 /**
  * Update the version of @vonage/vivid dependency in the package.json to the current version.
  */
 const main = async () => {
 	const packageJson = JSON.parse(
-		fs.readFileSync(
-			path.join(__dirname, '../../../dist/libs/vue-wrappers/package.json'),
-			'utf-8'
-		)
+		fs.readFileSync(path.join(dirname, '../package.json'), 'utf-8')
 	);
 	packageJson.dependencies['@vonage/vivid'] = `^${packageJson.version}`;
 	fs.writeFileSync(
-		path.join(__dirname, '../../../dist/libs/vue-wrappers/package.json'),
+		path.join(dirname, '../package.json'),
 		JSON.stringify(packageJson, null, 2)
 	);
 };
