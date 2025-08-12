afa0e9f0765f9750d7d399d78b16db09246bc40f
Konrad Sieńkowski <53014647+konradsienkowski@users.noreply.github.com>
2025-08-12 15:39:42 +0200

    feat(split-button): add outlined-light appearance (VIV-2751) (#2493)
    * feat(split-button): add outlined-light apperance (VIV-2751)
    
    * Update metadata
    
    * Fix expanded state in the ui tests
    
    * Fix separator issue, update snapshots
    
    * Add expanded menu to the ui test
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- libs/components/src/lib/split-button/ui.test.ts
+++ libs/components/src/lib/split-button/ui.test.ts
@@ -4,8 +4,9 @@ import {
 	loadComponents,
 	loadTemplate,
 } from '../../visual-tests/visual-tests-utils.js';
+import type { VwcMenuElement, VwcSplitButtonElement } from '../components.js';
 
-const components = ['split-button'];
+const components = ['split-button', 'menu', 'menu-item'];
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = `
 	<div style="margin: 5px;">
@@ -15,11 +16,13 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		<vwc-split-button label='ghost' appearance='ghost'></vwc-split-button>
 		<vwc-split-button label='filled' appearance='filled'></vwc-split-button>
 		<vwc-split-button label='outlined' appearance='outlined'></vwc-split-button>
+		<vwc-split-button label='outlined-light' appearance='outlined-light'></vwc-split-button>
 	</div>
 	<div style="margin: 5px; background-color: var(--vvd-color-cta-400)">
 		<vwc-split-button label='ghost' appearance='ghost'></vwc-split-button>
 		<vwc-split-button label='filled' appearance='filled'></vwc-split-button>
 		<vwc-split-button label='outlined' appearance='outlined'></vwc-split-button>
+		<vwc-split-button label='outlined-light' appearance='outlined-light'></vwc-split-button>
 	</div>
 	<div style="margin: 5px;">
 		<vwc-split-button appearance="filled" icon='compose-line' aria-label="Send Message"></vwc-split-button>
@@ -72,9 +75,17 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		<vwc-split-button appearance="outlined" label='alert' connotation='alert'></vwc-split-button>
 	</div>
 	<div style="margin: 5px;">
+		<vwc-split-button appearance="outlined-light" label='accent' connotation='accent'></vwc-split-button>
+		<vwc-split-button appearance="outlined-light" label='cta' connotation='cta'></vwc-split-button>
+		<vwc-split-button appearance="outlined-light" label='announcement' connotation='announcement'></vwc-split-button>
+		<vwc-split-button appearance="outlined-light" label='success' connotation='success'></vwc-split-button>
+		<vwc-split-button appearance="outlined-light" label='alert' connotation='alert'></vwc-split-button>
+	</div>
+	<div style="margin: 5px;">
 		<vwc-split-button appearance='ghost' label='ghost' disabled></vwc-split-button>
 		<vwc-split-button appearance='filled' label='filled' disabled></vwc-split-button>
 		<vwc-split-button appearance='outlined' label='outlined' disabled></vwc-split-button>
+		<vwc-split-button appearance='outlined-light' label='outlined-light' disabled></vwc-split-button>
 	</div>
 	<div style="margin: 5px;">
 		<vwc-split-button style="inline-size: 150px" appearance='filled' shape="rounded" label='A very long text default button'>
@@ -92,12 +103,17 @@ test('should show the component', async ({ page }: { page: Page }) => {
 			<vwc-icon slot="icon" name="check-circle-solid" connotation="success"></vwc-icon>
 		</vwc-split-button>
 	</div>
-	<div style="margin: 5px;">
-		<vwc-split-button label="Expanded" aria-expanded="true"></vwc-split-button>
+	<div style="margin: 5px; height: 135px;">
+		<vwc-split-button id="splitButton" label="Expanded">
+			<vwc-menu id="menu" placement="bottom-end">
+				<vwc-menu-item text="Menu item 1"></vwc-menu-item>
+				<vwc-menu-item text="Menu item 2"></vwc-menu-item>
+			</vwc-menu>
+		</vwc-split-button>
 	</div>
 `;
 
-	page.setViewportSize({ width: 600, height: 720 });
+	page.setViewportSize({ width: 600, height: 1200 });
 
 	await loadComponents({
 		page,
@@ -108,7 +124,22 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		template,
 	});
 
+	await page.evaluate(() => {
+		const splitButton = document.querySelector(
+			'#splitButton'
+		) as VwcSplitButtonElement;
+		const menu = document.querySelector('#menu') as VwcMenuElement;
+
+		menu.anchor = splitButton.indicator;
+
+		splitButton.addEventListener('indicator-click', () => {
+			menu.open = !menu.open;
+		});
+	});
+
 	const testWrapper = await page.$('#wrapper');
+	const buttonIndicator = await page.$('#splitButton .indicator');
+	await buttonIndicator?.click();
 
 	await page.waitForLoadState('networkidle');
 
