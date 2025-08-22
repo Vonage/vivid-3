96fd53cc0753f9ef4e32cdfe53d52727d280cb39
TaylorJ76 <james.taylor4@vonage.com>
2025-08-22 09:42:28 +0100

    chore: update toggletip lint

--- libs/components/src/lib/toggletip/toggletip.ts
+++ libs/components/src/lib/toggletip/toggletip.ts
@@ -12,7 +12,8 @@ import { Localized } from '../../shared/patterns';
  * @slot action-items - The content to display in the toggletip action items.
  */
 export class Toggletip extends Localized(Anchored(VividElement)) {
-	originalAriaLabel: string | null;
+	#originalAriaLabel: string | null = null;
+
 	/**
 	 * the optional title of the toggletip
 	 *
@@ -81,7 +82,7 @@ export class Toggletip extends Localized(Anchored(VividElement)) {
 
 	#setupAnchor(a: HTMLElement) {
 		a.addEventListener('click', this.#openIfClosed, true);
-		this.originalAriaLabel = a.ariaLabel;
+		this.#originalAriaLabel = a.ariaLabel;
 		a.ariaLabel = `${this.locale.toggletip.anchorLabel(a.ariaLabel || '')}`;
 
 		this.#updateAnchor(a);
@@ -95,7 +96,7 @@ export class Toggletip extends Localized(Anchored(VividElement)) {
 
 	#cleanupAnchor(a: HTMLElement) {
 		a.removeEventListener('click', this.#openIfClosed, true);
-		if (a.ariaLabel) a.ariaLabel = this.originalAriaLabel;
+		if (a.ariaLabel) a.ariaLabel = this.#originalAriaLabel;
 		a.removeAttribute('aria-expanded');
 		a.removeAttribute('data-expanded');
 	}
