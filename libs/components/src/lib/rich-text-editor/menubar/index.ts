713ab34b754fe5a811ac52ac9643e55deb2c1d11
Richard Helm <86777660+RichardHelm@users.noreply.github.com>
2025-10-22 17:26:28 +0100

    chore(rich-text-editor): refactor (VIV-2842) (#2541)
    Refactor rich text editor

--- libs/components/src/lib/rich-text-editor/menubar/index.ts
+++
@@ -1,3 +0,0 @@
-import { registerMenubar } from './definition.js';
-
-registerMenubar();
--- libs/components/src/shared/patterns/localized.ts
+++ libs/components/src/shared/patterns/localized.ts
@@ -1,3 +1,8 @@
+import {
+	type BindingObserver,
+	defaultExecutionContext,
+	Observable,
+} from '@microsoft/fast-element';
 import { VividElement } from '../foundation/vivid-element/vivid-element';
 import type { Constructor } from '../utils/mixins';
 import type { Locale } from '../localization/Locale';
@@ -17,4 +22,48 @@ export const Localized = <T extends Constructor<VividElement>>(Base: T) => {
 	}
 
 	return LocalizedElement;
+};
+
+/**
+ * Mixin for elements that need to access the current locale.
+ */
+export const WithObservableLocale = <T extends Constructor<VividElement>>(
+	Base: T
+) => {
+	class WithObservableLocaleElement extends Localized(Base) {
+		/**
+		 * Called whenever the locale changes.
+		 * @internal
+		 */
+		localeChanged() {}
+
+		override connectedCallback() {
+			super.connectedCallback();
+			this.#startObservingLocaleChanges();
+		}
+
+		override disconnectedCallback() {
+			super.disconnectedCallback();
+			this.#stopObservingLocaleChanges();
+		}
+
+		#localeChangeHandler = {
+			handleChange: () => {
+				this.localeChanged();
+			},
+		};
+		#localeChangeObserver!: BindingObserver;
+		#startObservingLocaleChanges() {
+			this.#localeChangeObserver = Observable.binding(
+				() => this.locale,
+				this.#localeChangeHandler
+			);
+			this.#localeChangeObserver.observe(this, defaultExecutionContext);
+		}
+		#stopObservingLocaleChanges() {
+			this.#localeChangeObserver.disconnect();
+		}
+	}
+
+	return WithObservableLocaleElement;
 };
