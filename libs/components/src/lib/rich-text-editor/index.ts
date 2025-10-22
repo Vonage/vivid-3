0fbabff424d934f85f7d6005424c32a1bb731302
Richard Helm <86777660+RichardHelm@users.noreply.github.com>
2025-10-22 17:26:28 +0100

    chore(rich-text-editor): refactor (VIV-2842) (#2541)
    Refactor rich text editor

--- libs/components/src/lib/rich-text-editor/index.ts
+++ libs/components/src/lib/rich-text-editor/index.ts
@@ -1,3 +1,15 @@
 import { registerRichTextEditor } from './definition';
 
 registerRichTextEditor();
+
+export { RTEConfig } from './rte/config';
+export { RTECore } from './rte/features/core';
+export { RTETextBlockStructure } from './rte/features/text-block';
+export { RTEFreeformStructure } from './rte/features/freeform';
+export { RTEToolbarFeature } from './rte/features/toolbar';
+export { RTEFontSizeFeature } from './rte/features/font-size';
+export { RTEBoldFeature } from './rte/features/bold';
+export { RTEItalicFeature } from './rte/features/italic';
+export { RTEUnderlineFeature } from './rte/features/underline';
+export { RTEStrikethroughFeature } from './rte/features/strikethrough';
+export { RTEMonospaceFeature } from './rte/features/monospace';
