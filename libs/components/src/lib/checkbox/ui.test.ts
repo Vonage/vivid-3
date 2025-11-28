2ec4f3c144d7f610ed5ec898e10a55ab51dd820f
TaylorJ76 <james.taylor4@vonage.com>
2025-11-28 07:59:16 +0000

    chore: updates ui test

--- libs/components/src/lib/checkbox/ui.test.ts
+++ libs/components/src/lib/checkbox/ui.test.ts
@@ -11,66 +11,94 @@ const components = ['checkbox', 'button', 'divider', 'layout'];
 test('should show the component', async ({ page }: { page: Page }) => {
 	const template = `
 	<div style="margin: 5px;">
-		<vwc-checkbox label="Use signed Webhooks"></vwc-checkbox>
+		<vwc-layout gutters="small" column-basis="block">
+			<vwc-checkbox label="Use signed Webhooks"></vwc-checkbox>
+		</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+	<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox label="Use signed Webhooks" helper-text="Signed Webhooks are a way to verify that the request is coming from Vonage."></vwc-checkbox>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+	<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox label="Use signed Webhooks" checked></vwc-checkbox>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+	<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox label="Select all"></vwc-checkbox>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+	<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox label="Select all"></vwc-checkbox>
 		<script>
 			document.querySelector('vwc-checkbox[label="Select all"]').indeterminate = true;
 		</script>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+	<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox disabled></vwc-checkbox> <vwc-checkbox disabled checked></vwc-checkbox>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+	<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox readonly></vwc-checkbox> <vwc-checkbox readonly checked></vwc-checkbox>
-	</div>
+	</vwc-layout>
 	<div style="margin: 5px;">
+		<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox label="I agree to the terms and conditions" error-text="You must agree to the terms and conditions to proceed"></vwc-checkbox>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+		<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox label="A default checkbox" success-text="Success text" checked></vwc-checkbox>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+		<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox connotation="accent"></vwc-checkbox>
 		<vwc-checkbox connotation="accent" checked></vwc-checkbox>
 		<vwc-checkbox connotation="cta"></vwc-checkbox>
 		<vwc-checkbox connotation="cta" checked></vwc-checkbox>		
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
-		<vwc-checkbox label="Use signed Webhooks"></vwc-checkbox>
+		<vwc-layout gutters="small" column-basis="block">
+	<vwc-checkbox label="Use signed Webhooks"></vwc-checkbox>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+		<vwc-layout gutters="small" column-basis="block">
 		<vwc-checkbox aria-checked="true"></vwc-checkbox>
 		<vwc-checkbox aria-checked="false"></vwc-checkbox>
 		<vwc-checkbox aria-checked="mixed"></vwc-checkbox>
+	</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
-		<vwc-checkbox error-text="You need to accept the Terms of service">
+		<vwc-layout gutters="small" column-basis="block">
+			<vwc-checkbox error-text="You need to accept the Terms of service">
 			I agree to
 			<a href="https://www.vonage.com/legal/" target="_blank"> Vonage Terms of Service </a>
 		</vwc-checkbox>
+	</vwc-layout>	
 	</div>
 	<div style="margin: 5px;">
-		<style>
-			.checkbox {
-				width: 300px;
-			}
-		</style>
-		<vwc-checkbox class="checkbox" label="Use Signed Webhooks">
-			<span slot="helper-text"><a href="#">Signed Webhooks</a> are a way to verify that the request is coming from Vonage.</span>
-		</vwc-checkbox>
+		<vwc-layout gutters="small" column-basis="block">
+			<style>
+				.checkbox {
+					width: 300px;
+				}
+			</style>
+			<vwc-checkbox class="checkbox" label="Use Signed Webhooks">
+				<span slot="helper-text"><a href="#">Signed Webhooks</a> are a way to verify that the request is coming from Vonage.</span>
+			</vwc-checkbox>
+		</vwc-layout>
 	</div>
 	<div style="margin: 5px;">
+		<vwc-layout gutters="small" column-basis="block">
 		<form method="post" action="">
 			<vwc-layout column-spacing="small" column-basis="block">
 				<vwc-checkbox required label="I agree to the terms and conditions"></vwc-checkbox>
@@ -80,6 +108,7 @@ test('should show the component', async ({ page }: { page: Page }) => {
 				</div>
 			</vwc-layout>
 		</form>
+		</vwc-layout>
 
 		<style>
 			.buttons {
@@ -89,7 +118,7 @@ test('should show the component', async ({ page }: { page: Page }) => {
 		</style>
 	</div>
 	<div style="margin: 5px;">
-		<div class="container">
+		<vwc-layout gutters="small" column-basis="block">
 			<vwc-checkbox id="select-all" label="Select all"></vwc-checkbox>
 			<vwc-divider></vwc-divider>
 			<div class="options">
@@ -97,7 +126,7 @@ test('should show the component', async ({ page }: { page: Page }) => {
 				<vwc-checkbox label="Option 2"></vwc-checkbox>
 				<vwc-checkbox label="Option 3"></vwc-checkbox>
 			</div>
-		</div>
+		</vwc-layout>
 	</div>`;
 
 	await page.setViewportSize({ width: 400, height: 800 });
