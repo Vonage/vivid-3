eb2ba77fabe688dc7bdf428ad0c17432a27db767
Jakub Hajduk <jakub.hajduk@vonage.com>
2025-12-01 14:27:35 +0100

    chore: temporary release tokens for flutter

--- libs/design-tokens/scripts/flutter/flutter.config.ts
+++ libs/design-tokens/scripts/flutter/flutter.config.ts
@@ -1,5 +1,6 @@
-import type { Hooks, PlatformConfig } from 'style-dictionary/types';
+import type { Hooks, PlatformConfig, TransformedToken } from 'style-dictionary/types';
 import { type ColorValue, getHex } from '../utils/hexify.util';
+import { fileHeader } from 'style-dictionary/utils';
 
 function double(value: string | number) {
 	return String(parseFloat(String(value)).toFixed(2));
@@ -11,6 +12,27 @@ function color(token: ColorValue) {
 }
 
 export const flutterConfig: Hooks = {
+	actions: {
+		'vvd/flutter/createPackage': {
+			do(_dictionary, platform, file, volume) {
+				volume.cpSync('./files/', platform.buildPath, { recursive: true } as any);
+			},
+			undo(_dictionary, platform, _config, volume) {
+				volume.unlinkSync(platform.buildPath);
+			},
+		},
+	},
+	formats: {
+		'vvd/flutter/variables': async ({ dictionary, file, options }) => {
+			const header = await fileHeader({ file, options });
+
+			const variables = Array.from(dictionary.tokenMap.values()).map(
+				(token: TransformedToken) => `const ${token.name} = ${token.$value};`,
+			);
+
+			return `${header}import 'package:flutter/painting.dart';\n\n${variables.join('\n')}`;
+		},
+	},
 	transforms: {
 		'vvd/value/flutter/borderRadius': {
 			type: 'value',
@@ -46,7 +68,7 @@ export const flutterConfig: Hooks = {
 				const fontSize = double(token.$value.fontSize.value);
 				const lineHeight = double(
 					parseFloat(token.$value.lineHeight) /
-						parseFloat(token.$value.fontSize.value)
+						parseFloat(token.$value.fontSize.value),
 				);
 				const fontWeight = token.$value.fontWeight;
 				const fontWidth = token.$value.fontFamily.toLowerCase().includes('wide')
@@ -65,12 +87,11 @@ export const flutterConfig: Hooks = {
 		)`;
 			},
 		},
-
 		'vvd/value/flutter/shadow': {
 			type: 'value',
 			filter: (token) => token.$type === 'shadow',
 			transform(token) {
-				const stops = token.$value.map((stop) => {
+				const stops = token.$value.map((stop: any) => {
 					return `			BoxShadow(
 					color: ${color(stop.color)},
 					offset: Offset(${double(stop.offsetX)}, ${double(stop.offsetY)}),
@@ -85,7 +106,7 @@ export const flutterConfig: Hooks = {
 			type: 'name',
 			transform(token) {
 				return token.name.replace(/([-_\/][a-z0-9])/g, ($1) =>
-					$1.toUpperCase().replace('-', '').replace('_', '').replace('/', '')
+					$1.toUpperCase().replace('-', '').replace('_', '').replace('/', ''),
 				);
 			},
 		},
@@ -93,9 +114,6 @@ export const flutterConfig: Hooks = {
 };
 
 export const flutterPlatform: PlatformConfig = {
-	options: {
-		selector: '.vvd-root, ::part(vvd-root)',
-	},
 	basePxFontSize: 14,
 	transforms: [
 		'vvd/value/flutter/color',
@@ -105,14 +123,17 @@ export const flutterPlatform: PlatformConfig = {
 		'vvd/value/flutter/borderRadius',
 		'vvd/value/flutter/shadow',
 	],
-	buildPath: './flutter-dist/',
+	buildPath: './flutter-dist',
 	files: [
 		{
-			destination: 'vivid-design-tokens.dart',
-			format: 'flutter/class.dart',
+			destination: 'lib/vivid-design-tokens.dart',
+			format: 'vvd/flutter/variables',
+			// TODO: Remove this filter after font licensing is sorted out and typography tokens are rolled out.
+			filter: (token) => !token.filePath.includes('typography'),
 			options: {
 				className: 'VvdTokens',
 			},
 		},
 	],
+	actions: ['vvd/flutter/createPackage'],
 };
