27de9a5452b5c09366e96667fc388f390fb227d0
Jakub Hajduk <jakub.hajduk@vonage.com>
2026-01-27 12:43:43 +0100

    chore(icons): format files

--- libs/tools/src/fetch-icons/manifest-types.ts
+++ libs/tools/src/fetch-icons/manifest-types.ts
@@ -1,16 +1,16 @@
 import { IconCategory, IconStyle } from './types';
 
 export type IconTag =
-  | `style_weight_${IconStyle}`
-  | `style_color_${IconStyle}`
-  | `category_${IconCategory}`
-  // eslint-disable-next-line
-  | ({} & string);
+	| `style_weight_${IconStyle}`
+	| `style_color_${IconStyle}`
+	| `category_${IconCategory}`
+	// eslint-disable-next-line
+	| ({} & string);
 
 export interface IconDefinition {
-  id: string;
-  keyword: string[];
-  tag: IconTag[];
+	id: string;
+	keyword: string[];
+	tag: IconTag[];
 }
 
 export type IconsManifest = IconDefinition[];
