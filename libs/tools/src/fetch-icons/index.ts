d265f7567a2ce2d827971d58b64f9c50076548b6
Jakub Hajduk <jakub.hajduk@vonage.com>
2026-01-27 10:55:52 +0100

    chore(icons): refactor fetching mechanism for parallel fetch

--- libs/tools/src/fetch-icons/index.ts
+++ libs/tools/src/fetch-icons/index.ts
@@ -1,3 +1,4 @@
 export * from './fetch-icons';
 export * from './types';
 export * from './create-icon-entry';
+export * from './manifest-types';
--- libs/tools/src/fetch-icons/types.ts
+++ libs/tools/src/fetch-icons/types.ts
@@ -2,72 +2,80 @@ import type { Node } from '@figma/rest-api-spec';
 import { GetClientUserOptions } from './cached-client';
 import { FileResponse } from 'figma-js';
 
-export type IconStyle = 'solid' | 'line' | 'flag';
+export type IconStyle =
+  | 'solid'
+  | 'line'
+  | 'flag'
+  | 'color'
+  | 'mono'
+  | ({} & string);
 
 export type IconCategory =
-	| 'alert'
-	| 'arrows'
-	| 'audio'
-	| 'brand'
-	| 'calling'
-	| 'charts'
-	| 'check'
-	| 'chevrons'
-	| 'commerce'
-	| 'connectivity'
-	| 'delete'
-	| 'devices'
-	| 'emoji'
-	| 'file'
-	| 'flags'
-	| 'layout'
-	| 'location'
-	| 'messaging'
-	| 'objects'
-	| 'password'
-	| 'science'
-	| 'social'
-	| 'sort'
-	| 'time'
-	| 'tools'
-	| 'user'
-	| 'video'
-	| 'view';
+  | 'alert'
+  | 'arrows'
+  | 'audio'
+  | 'brand'
+  | 'calling'
+  | 'charts'
+  | 'check'
+  | 'chevrons'
+  | 'commerce'
+  | 'connectivity'
+  | 'delete'
+  | 'devices'
+  | 'emoji'
+  | 'file'
+  | 'flags'
+  | 'layout'
+  | 'location'
+  | 'messaging'
+  | 'objects'
+  | 'password'
+  | 'science'
+  | 'social'
+  | 'sort'
+  | 'time'
+  | 'tools'
+  | 'user'
+  | 'video'
+  | 'view'
+  | ({} & string);
 
 export interface IconEntry {
-	category: IconCategory;
-	figmaComponentName: string;
-	figmaNodeId: string;
-	imageUrl: string;
-	keywords: string[];
-	name: string;
-	style: IconStyle;
+  category: IconCategory;
+  figmaComponentName: string;
+  figmaNodeId: string;
+  imageUrl: string;
+  keywords: string[];
+  name: string;
+  style: IconStyle;
 }
 
 export type TemplateFunction = (
-	entry: IconEntry,
-	svg: string
+  entry: IconEntry,
+  svg: string
 ) => string | undefined;
 export type PathFunction = (entry: IconEntry) => string;
 
 export interface OutputFormat {
-	fileName: PathFunction;
-	template: TemplateFunction;
+  fileName: PathFunction;
+  template: TemplateFunction;
 }
 
 export type NodeFilterFunction = (node: Node, path: Node[]) => boolean;
+
 export type CreateIconEntryFunction = (
-	node: Node,
-	path: Node[],
-	file: FileResponse
+  node: Node,
+  path: Node[],
+  file: FileResponse
 ) => IconEntry;
 
 export interface FetchIconsOptions {
-	cacheOptions: GetClientUserOptions;
-	createEntry: CreateIconEntryFunction;
-	dir: string;
-	filter: NodeFilterFunction;
-	forceUpdate: boolean;
-	indexFileName: string;
-	outputs: OutputFormat[];
+  cacheOptions: GetClientUserOptions;
+  createEntry: CreateIconEntryFunction;
+  dir: string;
+  filter: NodeFilterFunction;
+  forceUpdate: boolean;
+  indexFileName: string;
+  outputs: OutputFormat[];
 }
