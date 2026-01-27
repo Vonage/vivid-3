0dcbed00c4c3cc66d4ef6ce6ee3ca4dad70f04e4
Jakub Hajduk <jakub.hajduk@vonage.com>
2026-01-27 11:02:55 +0100

    chore(icons): extract brand icons to separate package

---
+++ libs/icons-brands/scripts/fetch-brands.ts
@@ -0,0 +1,67 @@
+import {
+  createIconEntry,
+  type CreateIconEntryFunction,
+  fetchIcons,
+  type IconsManifest,
+  type NodeFilterFunction,
+  writeJson,
+} from '@repo/tools';
+import type { Node } from '@figma/rest-api-spec';
+import { rmSync } from 'node:fs';
+import 'dotenv/config';
+import { svg } from './svg.output';
+
+// Only brands icons
+const onlyBrands: NodeFilterFunction = (node, path) => {
+  if (!Array.isArray(path)) return false;
+
+  return (
+    node.type === 'COMPONENT' &&
+    path.length >= 3 &&
+    path.at(-4)?.name === 'Icons' &&
+    path.at(-3)?.name === 'brand'
+  );
+};
+
+const entryFunction: CreateIconEntryFunction = (
+  node: Node,
+  path: Node[],
+  file
+) => {
+  const entry = createIconEntry(node, path, file);
+
+  entry.name = path.at(-2)?.name || '[UNKNOWN]';
+  entry.figmaComponentName = path.at(-2)?.name || '[UNKNOWN]';
+  entry.category = 'brand';
+  entry.style = node.name.replace('style=', '') || '[UNKNOWN]';
+
+  return entry;
+};
+
+(async () => {
+  const clear = true;
+
+  if (clear) {
+    rmSync('./src/generated', { recursive: true, force: true });
+  }
+
+  const icons = await fetchIcons('isdKI406usLCxZ2U8ljDrn', {
+    dir: './src/generated/',
+    forceUpdate: clear,
+    filter: onlyBrands,
+    createEntry: entryFunction,
+    indexFileName: 'index.json',
+    outputs: [svg],
+  });
+
+  const manifest: IconsManifest = icons.map((icon) => ({
+    id: `${icon.name}-${icon.style}`,
+    keyword: icon.keywords,
+    tag: [
+      `style_color_${icon.style === 'color' ? 'multi' : 'single'}`,
+      'category_brand',
+    ],
+  }));
+
+  writeJson('./src/generated/manifest.json', manifest);
+})();
