b013bf3e2b4cdddc5d1f2b3cd7eb3ad358955278
Jakub Hajduk <jakub.hajduk@vonage.com>
2026-01-29 10:26:09 +0100

    chore(icons): add aliases to icons

--- libs/tools/src/fetch-icons/create-icon-entry.ts
+++ libs/tools/src/fetch-icons/create-icon-entry.ts
@@ -1,25 +1,38 @@
 import type {
-	CreateIconEntryFunction,
-	IconCategory,
-	IconEntry,
-	IconStyle,
+  CreateIconEntryFunction,
+  IconCategory,
+  IconEntry,
+  IconStyle,
 } from './types';
 
 export const createIconEntry: CreateIconEntryFunction = (node, path, file) => {
-	const keywords = (file.components[node.id]?.description || '')
-		.replace('keyword:', '')
-		.split(',')
-		.map((k) => k.trim());
+  const description = file.components[node.id]?.description || '';
 
-	const entry: IconEntry = {
-		figmaNodeId: node.id,
-		imageUrl: '',
-		figmaComponentName: path.at(-2)?.name || '[UNKNOWN]',
-		name: path.at(-2)?.name || node.id.replace(':', '-'),
-		style: node.name.replace('style=', '') as IconStyle,
-		category: path.at(-3)?.name as IconCategory,
-		keywords: keywords,
-	};
+  const keywordsString = description
+    .split(/[Kk]eyword:/)
+    .at(1)
+    ?.split(/[Aa]lias:/)
+    .at(0);
+  const aliasString = description.split(/[Aa]lias:/).at(1);
 
-	return entry;
+  const keywords =
+    keywordsString
+      ?.split(',')
+      .map((keyword) => keyword.trim().replace(/[:;]/g, '')) || [];
+  const aliases =
+    aliasString?.split(',').map((alias) => alias.trim().replace(/[:;]/g, '')) ||
+    [];
+
+  const entry: IconEntry = {
+    figmaNodeId: node.id,
+    imageUrl: '',
+    figmaComponentName: path.at(-2)?.name || '[UNKNOWN]',
+    name: path.at(-2)?.name || node.id.replace(':', '-'),
+    style: node.name.replace('style=', '') as IconStyle,
+    category: path.at(-3)?.name as IconCategory,
+    keywords,
+    aliases,
+  };
+
+  return entry;
 };
--- libs/tools/src/fetch-icons/types.ts
+++ libs/tools/src/fetch-icons/types.ts
@@ -1,83 +1,84 @@
 import type { Node } from '@figma/rest-api-spec';
-import { GetClientUserOptions } from './cached-client';
-import { FileResponse } from 'figma-js';
+import type { GetClientUserOptions } from './cached-client';
+import type { FileResponse } from 'figma-js';
 
 export type IconStyle =
-	| 'solid'
-	| 'line'
-	| 'flag'
-	| 'color'
-	| 'mono'
-	// eslint-disable-next-line
-	| ({} & string);
+  | 'solid'
+  | 'line'
+  | 'flag'
+  | 'color'
+  | 'mono'
+  // eslint-disable-next-line
+  | ({} & string);
 
 export type IconCategory =
-	| 'alert'
-	| 'arrows'
-	| 'audio'
-	| 'brand'
-	| 'calling'
-	| 'charts'
-	| 'check'
-	| 'chevrons'
-	| 'commerce'
-	| 'connectivity'
-	| 'delete'
-	| 'devices'
-	| 'emoji'
-	| 'file'
-	| 'flags'
-	| 'layout'
-	| 'location'
-	| 'messaging'
-	| 'objects'
-	| 'password'
-	| 'science'
-	| 'social'
-	| 'sort'
-	| 'time'
-	| 'tools'
-	| 'user'
-	| 'video'
-	| 'view'
-	// eslint-disable-next-line
-	| ({} & string);
+  | 'alert'
+  | 'arrows'
+  | 'audio'
+  | 'brand'
+  | 'calling'
+  | 'charts'
+  | 'check'
+  | 'chevrons'
+  | 'commerce'
+  | 'connectivity'
+  | 'delete'
+  | 'devices'
+  | 'emoji'
+  | 'file'
+  | 'flags'
+  | 'layout'
+  | 'location'
+  | 'messaging'
+  | 'objects'
+  | 'password'
+  | 'science'
+  | 'social'
+  | 'sort'
+  | 'time'
+  | 'tools'
+  | 'user'
+  | 'video'
+  | 'view'
+  // eslint-disable-next-line
+  | ({} & string);
 
 export interface IconEntry {
-	category: IconCategory;
-	figmaComponentName: string;
-	figmaNodeId: string;
-	imageUrl: string;
-	keywords: string[];
-	name: string;
-	style: IconStyle;
+  category: IconCategory;
+  figmaComponentName: string;
+  figmaNodeId: string;
+  imageUrl: string;
+  keywords: string[];
+  aliases: string[];
+  name: string;
+  style: IconStyle;
 }
 
 export type TemplateFunction = (
-	entry: IconEntry,
-	svg: string
+  entry: IconEntry,
+  svg: string
 ) => string | undefined;
 export type PathFunction = (entry: IconEntry) => string;
 
 export interface OutputFormat {
-	fileName: PathFunction;
-	template: TemplateFunction;
+  fileName: PathFunction;
+  template: TemplateFunction;
 }
 
 export type NodeFilterFunction = (node: Node, path: Node[]) => boolean;
 
 export type CreateIconEntryFunction = (
-	node: Node,
-	path: Node[],
-	file: FileResponse
+  node: Node,
+  path: Node[],
+  file: FileResponse
 ) => IconEntry;
 
 export interface FetchIconsOptions {
-	cacheOptions: GetClientUserOptions;
-	createEntry: CreateIconEntryFunction;
-	dir: string;
-	filter: NodeFilterFunction;
-	forceUpdate: boolean;
-	indexFileName: string;
-	outputs: OutputFormat[];
+  cacheOptions: GetClientUserOptions;
+  createEntry: CreateIconEntryFunction;
+  dir: string;
+  filter: NodeFilterFunction;
+  forceUpdate: boolean;
+  indexFileName: string;
+  outputs: OutputFormat[];
 }
