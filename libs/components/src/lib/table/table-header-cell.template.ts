ff912c27a6d648e57038b1f330768c7c6d098224
Uladzislau Pakosenka <uladzislau.pakosenka@vonage.com>
2026-02-20 11:41:44 +0100

    VIV-3096 remove composed-dom utility function

--- libs/components/src/lib/table/table-header-cell.template.ts
+++ libs/components/src/lib/table/table-header-cell.template.ts
@@ -1,26 +1,21 @@
 import { html } from '@microsoft/fast-element';
 import type { VividElementDefinitionContext } from '../../shared/design-system/defineVividComponent';
 import { applyHostSemantics } from '../../shared/aria/host-semantics';
-import { findComposedAncestor } from '../../shared/utils/composed-dom';
 import { DataGridCellRole } from '../data-grid/data-grid.options';
 import type { TableHeaderCell } from './table-header-cell';
-
-function isTableHead(el: Element): boolean {
-	return (
-		el.getAttribute?.('data-vvd-component') === 'table-head' ||
-		(el.tagName?.toLowerCase().endsWith('-table-head') ?? false)
-	);
-}
+import { tableHeadName } from './table-head';
 
 export const TableHeaderCellTemplate = (
 	context: VividElementDefinitionContext
 ) => {
+	const tableHeadTag = context.tagForNonDependency(tableHeadName);
+
 	return html<TableHeaderCell>`
 		<template
 			tabindex="-1"
 			${applyHostSemantics({
 				role: (x) =>
-					findComposedAncestor(x, isTableHead) !== null
+					x.closest(tableHeadTag) !== null
 						? DataGridCellRole.columnheader
 						: DataGridCellRole.rowheader,
 			})}
