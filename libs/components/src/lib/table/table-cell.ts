2c05546d6f585ffdb911fa1fed9e22032e5eab30
Uladzislau Pakosenka <uladzislau.pakosenka@vonage.com>
2026-02-23 10:42:06 +0100

    VIV-3096 update screenshots

--- libs/components/src/lib/table/table-cell.ts
+++ libs/components/src/lib/table/table-cell.ts
@@ -6,7 +6,7 @@ import { HostSemantics } from '../../shared/aria/host-semantics';
  * @public
  * @component table-cell
  * @slot - Default slot.
- * @event {CustomEvent<TableCell>} cell-focused - Fires when the cell or its contents receive focus
+ * @event {CustomEvent<HTMLElement>} cell-focused - Fires when the cell or its contents receive focus
  */
 export class TableCell extends HostSemantics(VividElement) {
 	override connectedCallback(): void {
