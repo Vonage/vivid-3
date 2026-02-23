b7cb3805fc55985aca7ada9abd8a180ac4aa3f09
Uladzislau Pakosenka <uladzislau.pakosenka@vonage.com>
2026-02-23 10:10:27 +0100

    VIV-3096  add arrow navigation (table)

--- libs/components/src/lib/table/table-cell.ts
+++ libs/components/src/lib/table/table-cell.ts
@@ -1,3 +1,4 @@
+import { eventFocusIn } from '@microsoft/fast-web-utilities';
 import { VividElement } from '../../shared/foundation/vivid-element/vivid-element';
 import { HostSemantics } from '../../shared/aria/host-semantics';
 
@@ -5,5 +6,20 @@ import { HostSemantics } from '../../shared/aria/host-semantics';
  * @public
  * @component table-cell
  * @slot - Default slot.
+ * @event {CustomEvent<TableCell>} cell-focused - Fires when the cell or its contents receive focus
  */
-export class TableCell extends HostSemantics(VividElement) {}
+export class TableCell extends HostSemantics(VividElement) {
+	override connectedCallback(): void {
+		super.connectedCallback();
+		this.addEventListener(eventFocusIn, this.handleFocusIn as EventListener);
+	}
+
+	override disconnectedCallback(): void {
+		super.disconnectedCallback();
+		this.removeEventListener(eventFocusIn, this.handleFocusIn as EventListener);
+	}
+
+	private handleFocusIn = (): void => {
+		this.$emit('cell-focused', this);
+	};
+}
