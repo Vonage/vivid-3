b02bfa60958b1412fe758411de66ea4413b2278e
Jakub Hajduk <jakub.hajduk@vonage.com>
2026-03-18 15:53:47 +0100

    chore(ci): Fix missing flag icons

--- .github/actions/upload-icons/src/get-manifest.js
+++ .github/actions/upload-icons/src/get-manifest.js
@@ -1,6 +1,6 @@
 export function getManifest(entries) {
 	return entries.map((icon) => ({
-		id: `${icon.name}-${icon.style}`,
+		id: icon.id,
 		keyword: icon.keywords,
 		tag: [
 			`style_color_${icon.style === 'color' ? 'multi' : 'single'}`,
