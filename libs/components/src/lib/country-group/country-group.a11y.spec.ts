e99d6ae26bdb1a0f148225a8be10af5537d70daf
SpawnAtis <upakosenka@vonage.net>
2026-04-21 11:05:46 +0200

    APIVIV-11 fix lint

--- libs/components/src/lib/country-group/country-group.a11y.spec.ts
+++ libs/components/src/lib/country-group/country-group.a11y.spec.ts
@@ -1,5 +1,5 @@
 import { axe, elementUpdated, fixture } from '@repo/shared';
-import { CountryGroup } from './country-group';
+import type { CountryGroup } from './country-group';
 import '.';
 
 const COMPONENT_TAG = 'vwc-country-group';
